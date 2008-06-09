65e5080ec82c4a3c64a203357e3edddd278c2d27
henry <Henry Weller h.weller@opencfd.co.uk>
2008-06-09 14:10:14 +0100

    Added WM_THIRD_PARTY environment variable.

--- etc/settings.sh
+++ etc/settings.sh
@@ -91,10 +91,6 @@ AddPath $FOAM_USER_APPBIN
 export FOAM_RUN=$WM_PROJECT_USER_DIR/run
 
 
-# Location of third-party software
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-thirdParty=$WM_PROJECT_INST_DIR/ThirdParty
-
 # Compiler settings
 # ~~~~~~~~~~~~~~~~~
 WM_COMPILER_BIN=
@@ -109,10 +105,10 @@ case "$WM_COMPILER_INST" in
 OpenFOAM)
     case "$WM_COMPILER" in
     Gcc43)
-        export WM_COMPILER_DIR=$thirdParty/gcc-4.3.0/platforms/$WM_ARCH$WM_COMPILER_ARCH
+        export WM_COMPILER_DIR=$WM_THIRD_PARTY/gcc-4.3.0/platforms/$WM_ARCH$WM_COMPILER_ARCH
         ;;
     Gcc)
-        export WM_COMPILER_DIR=$thirdParty/gcc-4.2.2/platforms/$WM_ARCH$WM_COMPILER_ARCH
+        export WM_COMPILER_DIR=$WM_THIRD_PARTY/gcc-4.2.2/platforms/$WM_ARCH$WM_COMPILER_ARCH
         ;;
     esac
 
@@ -146,7 +142,7 @@ unset MPI_ARCH_PATH
 case "$WM_MPLIB" in
 OPENMPI)
     mpi_version=openmpi-1.2.6
-    export MPI_HOME=$thirdParty/$mpi_version
+    export MPI_HOME=$WM_THIRD_PARTY/$mpi_version
     export MPI_ARCH_PATH=$MPI_HOME/platforms/$WM_OPTIONS
 
     # Tell OpenMPI where to find its install directory
@@ -161,9 +157,9 @@ OPENMPI)
 
 LAM)
     mpi_version=lam-7.1.4
-    export MPI_HOME=$thirdParty/$mpi_version
+    export MPI_HOME=$WM_THIRD_PARTY/$mpi_version
     export MPI_ARCH_PATH=$MPI_HOME/platforms/$WM_OPTIONS
-    export LAMHOME=$thirdParty/$mpi_version
+    export LAMHOME=$WM_THIRD_PARTY/$mpi_version
     # note: LAMHOME is deprecated, should probably point to MPI_ARCH_PATH too
 
     AddLib  $MPI_ARCH_PATH/lib
@@ -175,7 +171,7 @@ LAM)
 
 MPICH)
     mpi_version=mpich-1.2.4
-    export MPI_HOME=$thirdParty/$mpi_version
+    export MPI_HOME=$WM_THIRD_PARTY/$mpi_version
     export MPI_ARCH_PATH=$MPI_HOME/platforms/$WM_OPTIONS
     export MPICH_ROOT=$MPI_ARCH_PATH
 
