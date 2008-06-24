7639571b4b4e56252f3f60569cd7b72ef51ae6b0
henry <Henry Weller h.weller@opencfd.co.uk>
2008-06-24 17:01:01 +0100

    Fixed a bug introduced by maintaining the coupled patch fields in sliced fields.

--- src/finiteVolume/fvMatrices/solvers/MULES/MULESTemplates.C
+++ src/finiteVolume/fvMatrices/solvers/MULES/MULESTemplates.C
@@ -55,6 +55,7 @@ void Foam::MULES::explicitSolve
     Info<< "MULES: Solving for " << psi.name() << endl;
 
     const fvMesh& mesh = psi.mesh();
+    psi.correctBoundaryConditions();
 
     surfaceScalarField phiBD = upwind<scalar>(psi.mesh(), phi).flux(psi);
 
@@ -76,7 +77,8 @@ void Foam::MULES::explicitSolve
         ),
         mesh,
         dimless,
-        allLambda
+        allLambda,
+        false   // Use slices for the couples
     );
 
     limiter
@@ -183,7 +185,8 @@ void Foam::MULES::implicitSolve
             ),
             mesh,
             dimless,
-            allCoLambda
+            allCoLambda,
+            false   // Use slices for the couples
         );
 
         CoLambda == 1.0/max(CoCoeff*Cof, scalar(1));
@@ -205,7 +208,8 @@ void Foam::MULES::implicitSolve
         ),
         mesh,
         dimless,
-        allLambda
+        allLambda,
+        false   // Use slices for the couples
     );
 
     linear<scalar> CDs(mesh);
@@ -347,7 +351,8 @@ void Foam::MULES::limiter
         ),
         mesh,
         dimless,
-        allLambda
+        allLambda,
+        false   // Use slices for the couples
     );
 
     scalarField& lambdaIf = lambda;
@@ -405,6 +410,9 @@ void Foam::MULES::limiter
             forAll(phiCorrPf, pFacei)
             {
                 label pfCelli = pFaceCells[pFacei];
+
+                psiMaxn[pfCelli] = max(psiMaxn[pfCelli], psiPf[pFacei]);
+                psiMinn[pfCelli] = min(psiMinn[pfCelli], psiPf[pFacei]);
 
                 psiMaxn[pfCelli] = max(psiMaxn[pfCelli], psiPNf[pFacei]);
                 psiMinn[pfCelli] = min(psiMinn[pfCelli], psiPNf[pFacei]);
