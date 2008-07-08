32d46dc30c1f5509b859e061667f6e4ec2ced527
Mark Olesen <Mark.Olesen@Germany>
2008-07-08 14:45:31 +0200

    add in UEqn.H, pEqn.H for simpleFoam as well

--- applications/solvers/incompressible/simpleFoam/simpleFoam.C
+++ applications/solvers/incompressible/simpleFoam/simpleFoam.C
@@ -26,8 +26,7 @@ Application
     simpleFoam
 
 Description
-    Steady-state solver for incompressible, turbulent flow of non-Newtonian
-    fluids.
+    Steady-state solver for incompressible, turbulent flow
 
 \*---------------------------------------------------------------------------*/
 
@@ -41,13 +40,10 @@ int main(int argc, char *argv[])
 {
 
 #   include "setRootCase.H"
-
 #   include "createTime.H"
 #   include "createMesh.H"
 #   include "createFields.H"
 #   include "initContinuityErrs.H"
-
-    //mesh.clearPrimitives();
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -63,50 +59,8 @@ int main(int argc, char *argv[])
 
         // Pressure-velocity SIMPLE corrector
         {
-            // Momentum predictor
-
-            tmp<fvVectorMatrix> UEqn
-            (
-                fvm::div(phi, U)
-              + turbulence->divDevReff(U)
-            );
-
-            UEqn().relax();
-
-            solve(UEqn() == -fvc::grad(p));
-
-            p.boundaryField().updateCoeffs();
-            volScalarField AU = UEqn().A();
-            U = UEqn().H()/AU;
-            UEqn.clear();
-            phi = fvc::interpolate(U) & mesh.Sf();
-            adjustPhi(phi, U, p);
-
-            // Non-orthogonal pressure corrector loop
-            for (int nonOrth=0; nonOrth<=nNonOrthCorr; nonOrth++)
-            {
-                fvScalarMatrix pEqn
-                (
-                    fvm::laplacian(1.0/AU, p) == fvc::div(phi)
-                );
-
-                pEqn.setReference(pRefCell, pRefValue);
-                pEqn.solve();
-
-                if (nonOrth == nNonOrthCorr)
-                {
-                    phi -= pEqn.flux();
-                }
-            }
-
-#           include "continuityErrs.H"
-
-            // Explicitly relax pressure for momentum corrector
-            p.relax();
-
-            // Momentum corrector
-            U -= fvc::grad(p)/AU;
-            U.correctBoundaryConditions();
+#           include "UEqn.H"
+#           include "pEqn.H"
         }
 
         turbulence->correct();
