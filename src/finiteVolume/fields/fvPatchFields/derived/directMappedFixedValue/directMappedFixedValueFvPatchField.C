571d39c321e12c91279ebcecc735a3f01567b009
andy <a.heather@opencfd.co.uk>
2008-09-16 11:25:08 +0100

    further updates to mapping

--- src/finiteVolume/fields/fvPatchFields/derived/directMappedFixedValue/directMappedFixedValueFvPatchField.C
+++ src/finiteVolume/fields/fvPatchFields/derived/directMappedFixedValue/directMappedFixedValueFvPatchField.C
@@ -144,58 +144,17 @@ directMappedFixedValueFvPatchField<Type>::directMappedFixedValueFvPatchField
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
 template<class Type>
-void directMappedFixedValueFvPatchField<Type>::updateCoeffs()
+void directMappedFixedValueFvPatchField<Type>::getNewValues
+(
+    const directMappedPolyPatch& mpp,
+    const Field<Type>& sendValues,
+    Field<Type>& newValues
+) const
 {
-    if (this->updated())
-    {
-        return;
-    }
-
-    // Get the directMappedPolyPatch
-    const directMappedPolyPatch& mpp = refCast<const directMappedPolyPatch>
-    (
-        directMappedFixedValueFvPatchField<Type>::patch().patch()
-    );
-
     // Get the scheduling information
     const List<labelPair>& schedule = mpp.schedule();
     const labelListList& sendLabels = mpp.sendLabels();
     const labelListList& receiveFaceLabels = mpp.receiveFaceLabels();
-
-
-    Field<Type> newValues(this->size());
-    Field<Type> sendValues(this->size());
-
-    switch(mpp.mode())
-    {
-        case directMappedPolyPatch::NEARESTCELL:
-        {
-            sendValues = this->internalField();
-            break;
-        }
-        case directMappedPolyPatch::NEARESTPATCHFACE:
-        {
-            const label patchID =
-                 this->patch().patch().boundaryMesh().findPatchID
-                 (
-                    mpp.samplePatch()
-                 );
-            typedef GeometricField<Type, fvPatchField, volMesh> fieldType;
-            const word& fieldName = this->dimensionedInternalField().name();
-            const fieldType& sendField =
-                this->db().objectRegistry::lookupObject<fieldType>(fieldName);
-            sendValues = sendField.boundaryField()[patchID];
-            break;
-        }
-        default:
-        {
-            FatalErrorIn
-            (
-                "directMappedFixedValueFvPatchField<Type>::updateCoeffs()"
-            )<< "patch can only be used in NEARESTCELL or NEARESTPATCHFACE "
-             << "mode" << nl << abort(FatalError);
-        }
-    }
 
     forAll(schedule, i)
     {
@@ -205,13 +164,13 @@ void directMappedFixedValueFvPatchField<Type>::updateCoeffs()
 
         if (Pstream::myProcNo() == sendProc)
         {
-            OPstream toProc(Pstream::blocking, recvProc);
+            OPstream toProc(Pstream::scheduled, recvProc);
             toProc<< IndirectList<Type>(sendValues, sendLabels[recvProc])();
         }
         else
         {
             // I am receiver. Receive from sendProc.
-            IPstream fromProc(Pstream::blocking, sendProc);
+            IPstream fromProc(Pstream::scheduled, sendProc);
 
             Field<Type> fromFld(fromProc);
 
@@ -239,6 +198,89 @@ void directMappedFixedValueFvPatchField<Type>::updateCoeffs()
             label patchFaceI = faceLabels[i];
 
             newValues[patchFaceI] = fromFld[i];
+        }
+    }
+}
+
+
+template<class Type>
+void directMappedFixedValueFvPatchField<Type>::updateCoeffs()
+{
+    if (this->updated())
+    {
+        return;
+    }
+
+    // Get the directMappedPolyPatch
+    const directMappedPolyPatch& mpp = refCast<const directMappedPolyPatch>
+    (
+        directMappedFixedValueFvPatchField<Type>::patch().patch()
+    );
+
+    Field<Type> newValues(this->size());
+
+    switch (mpp.mode())
+    {
+        case directMappedPolyPatch::NEARESTCELL:
+        {
+            getNewValues(mpp, this->internalField(), newValues);
+
+            break;
+        }
+        case directMappedPolyPatch::NEARESTPATCHFACE:
+        {
+            const label patchID =
+                 this->patch().patch().boundaryMesh().findPatchID
+                 (
+                    mpp.samplePatch()
+                 );
+            typedef GeometricField<Type, fvPatchField, volMesh> fieldType;
+            const word& fieldName = this->dimensionedInternalField().name();
+            const fieldType& sendField =
+                this->db().objectRegistry::lookupObject<fieldType>(fieldName);
+
+            getNewValues(mpp, sendField.boundaryField()[patchID], newValues);
+
+            break;
+        }
+        case directMappedPolyPatch::NEARESTFACE:
+        {
+            typedef GeometricField<Type, fvPatchField, volMesh> fieldType;
+            const word& fieldName = this->dimensionedInternalField().name();
+            const fieldType& sendField =
+                this->db().objectRegistry::lookupObject<fieldType>(fieldName);
+
+            Field<Type> allValues
+            (
+                this->patch().patch().boundaryMesh().mesh().nFaces(),
+                pTraits<Type>::zero
+            );
+
+            forAll(sendField.boundaryField(), patchI)
+            {
+                const fvPatchField<Type>& pf =
+                    sendField.boundaryField()[patchI];
+                label faceStart = pf.patch().patch().start();
+
+                forAll(pf, faceI)
+                {
+                    allValues[faceStart++] = pf[faceI];
+                }
+            }
+
+            getNewValues(mpp, allValues, newValues);
+
+            newValues = this->patch().patchSlice(newValues);
+
+            break;
+        }
+        default:
+        {
+            FatalErrorIn
+            (
+                "directMappedFixedValueFvPatchField<Type>::updateCoeffs()"
+            )<< "Unknown sampling mode: " << mpp.mode()
+             << nl << abort(FatalError);
         }
     }
 
