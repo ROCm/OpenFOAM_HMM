eba9bbf0d82f6bf2fe9bf963b13556c85ebfa1f7
Mark Olesen <Mark.Olesen@Germany>
2008-10-28 01:59:42 +0100

    cosmetics

--- applications/utilities/mesh/conversion/ideasUnvToFoam/ideasUnvToFoam.C
+++ applications/utilities/mesh/conversion/ideasUnvToFoam/ideasUnvToFoam.C
@@ -572,6 +572,7 @@ int main(int argc, char *argv[])
 {
     argList::noParallel();
     argList::validArgs.append(".unv file");
+    argList::validOptions.insert("dump", "");
 
 #   include "setRootCase.H"
 #   include "createTime.H"
@@ -806,7 +807,7 @@ int main(int argc, char *argv[])
 
         forAll(dynPatchFaces, patchI)
         {
-            patchFaceVerts[patchI].transfer(dynPatchFaces[patchI].shrink());
+            patchFaceVerts[patchI].transfer(dynPatchFaces[patchI]);
         }
     }
     else
@@ -845,13 +846,13 @@ int main(int argc, char *argv[])
 
     pointField polyPoints;
     polyPoints.transfer(points);
-    points.clear();
 
     // Length scaling factor
     polyPoints /= lengthScale;
 
 
     // For debugging: dump boundary faces as triSurface
+    if (args.options().found("dump"))
     {
         DynamicList<labelledTri> triangles(boundaryFaces.size());
 
@@ -885,7 +886,6 @@ int main(int argc, char *argv[])
 
         surface.write(runTime.path()/"boundaryFaces.stl");
     }
-
 
 
     Info<< "Constructing mesh with non-default patches of size:" << nl;
