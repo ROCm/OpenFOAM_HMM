80c19974f1e2d12221f9dd28c14b02a9c8fbf60a
mattijs <mattijs@hunt.opencfd.co.uk>
2008-11-14 13:42:28 +0000

    ignore status

--- src/Pstream/mpi/IPread.C
+++ src/Pstream/mpi/IPread.C
@@ -193,15 +193,13 @@ void Foam::IPstream::waitRequests()
 {
     if (IPstream_outstandingRequests_.size() > 0)
     {
-        List<MPI_Status> status(IPstream_outstandingRequests_.size());
-
         if
         (
             MPI_Waitall
             (
                 IPstream_outstandingRequests_.size(),
                 IPstream_outstandingRequests_.begin(),
-                status.begin()
+                MPI_STATUSES_IGNORE
             )
         )
         {
@@ -231,9 +229,7 @@ bool Foam::IPstream::finishedRequest(const label i)
     }
 
     int flag;
-    MPI_Status status;
-
-    MPI_Test(&IPstream_outstandingRequests_[i], &flag, &status);
+    MPI_Test(&IPstream_outstandingRequests_[i], &flag, MPI_STATUS_IGNORE);
 
     return flag != 0;
 }
