1f7747776c7d35567c87b46974e846ec04968eaa
graham <graham.macpherson@strath.ac.uk>
2008-11-14 17:12:40 +0000

    Changed placeholder functions for electrostatic interactions.

--- src/lagrangian/molecularDynamics/molecule/moleculeCloud/moleculeCloud.C
+++ src/lagrangian/molecularDynamics/molecule/moleculeCloud/moleculeCloud.C
@@ -209,20 +209,7 @@ void Foam::moleculeCloud::calculateElectrostaticForce()
 {
     Info<< "Electrostatic forces" << endl;
 
-    // Particle-Particle part.
-    // Specialised version of the normal pair force calculation
 
-    // Particle Mesh part.
-
-    // Assign charges from the molecules to the charge density field of the
-    // mesh.
-    assignChargesToMesh();
-
-    // Solve Poisson's equation for the electrostatic field.  Take the gradient
-    // of it to calculate the force field.
-    calculateElectrostaticForceField();
-
-    // Interpolate forces from the force field to
 }
 
 
