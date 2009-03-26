d504063ce1aa4f083a3b9264e8aca36c09f99e68
andy <a.heather@opencfd.co.uk>
2009-03-26 11:43:25 +0000

    tidying

--- src/thermophysicalModels/radiation/radiationModel/fvDOM/fvDOM/fvDOM.C
+++ src/thermophysicalModels/radiation/radiationModel/fvDOM/fvDOM/fvDOM.C
@@ -132,8 +132,8 @@ Foam::radiation::fvDOM::fvDOM(const volScalarField& T)
 {
     if (mesh_.nSolutionD() == 3)    //3D
     {
-        IRay_.setSize(4*nPhi_*nTheta_);
         nRay_ = 4*nPhi_*nTheta_;
+        IRay_.setSize(nRay_);
         scalar deltaPhi = mathematicalConstant::pi/(2.0*nPhi_);
         scalar deltaTheta = mathematicalConstant::pi/nTheta_;
         label i = 0;
@@ -169,8 +169,8 @@ Foam::radiation::fvDOM::fvDOM(const volScalarField& T)
         {
             scalar thetai = mathematicalConstant::pi/2.0;
             scalar deltaTheta = mathematicalConstant::pi;
-            IRay_.setSize(4*nPhi_);
             nRay_ = 4*nPhi_;
+            IRay_.setSize(nRay_);
             scalar deltaPhi = mathematicalConstant::pi/(2.0*nPhi_);
             label i = 0;
             for (label m = 1; m <= 4*nPhi_; m++)
@@ -199,8 +199,8 @@ Foam::radiation::fvDOM::fvDOM(const volScalarField& T)
         {
             scalar thetai = mathematicalConstant::pi/2.0;
             scalar deltaTheta = mathematicalConstant::pi;
-            IRay_.setSize(2);
             nRay_ = 2;
+            IRay_.setSize(nRay_);
             scalar deltaPhi = mathematicalConstant::pi;
             label i = 0;
             for (label m = 1; m <= 2; m++)
@@ -296,7 +296,7 @@ void Foam::radiation::fvDOM::calculate()
             maxResidual = max(maxBandResidual, maxResidual);
         }
 
-        Info << "Radiation solver iter: " <<  radIter << endl;
+        Info << "Radiation solver iter: " << radIter << endl;
 
     } while(maxResidual > convergence_);
 
