0e0476362f0398024828a9fce3e23175c75f5a82
mattijs <mattijs@hunt.opencfd.co.uk>
2009-06-10 11:31:17 +0100

    disable extended stencils on coupled patches with transformation since incorrect

--- src/finiteVolume/fvMesh/extendedStencil/faceToCell/extendedFaceToCellStencil.C
+++ src/finiteVolume/fvMesh/extendedStencil/faceToCell/extendedFaceToCellStencil.C
@@ -33,7 +33,30 @@ License
 Foam::extendedFaceToCellStencil::extendedFaceToCellStencil(const polyMesh& mesh)
 :
     mesh_(mesh)
-{}
+{
+    // Check for transformation - not supported.
+    const polyBoundaryMesh& patches = mesh.boundaryMesh();
+
+    forAll(patches, patchI)
+    {
+        if (isA<coupledPolyPatch>(patches[patchI]))
+        {
+            const coupledPolyPatch& cpp =
+                refCast<const coupledPolyPatch>(patches[patchI]);
+
+            if (!cpp.parallel() || cpp.separated())
+            {
+                FatalErrorIn
+                (
+                    "extendedFaceToCellStencil::extendedFaceToCellStencil"
+                    "(const polyMesh&)"
+                )   << "Coupled patches with transformations not supported."
+                    << endl
+                    << "Problematic patch " << cpp.name() << exit(FatalError);
+            }
+        }
+    }
+}
 
 
 // ************************************************************************* //
