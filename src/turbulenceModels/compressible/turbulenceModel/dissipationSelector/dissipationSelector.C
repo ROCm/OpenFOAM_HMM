043098fb7c1b3a35192bed54486b800e49fc85fa
Mark Olesen <Mark.Olesen@Germany>
2009-07-10 22:29:42 +0200

    added dissipationSelector
    - first stab at a simple runtime selection for extra turbulence-related
      features
    
    - the code duplication seems to indicate that a common ancestor
      for compressible and incompressible turbulence models could be good

---
+++ src/turbulenceModels/compressible/turbulenceModel/dissipationSelector/dissipationSelector.C
@@ -0,0 +1,111 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2009 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+\*---------------------------------------------------------------------------*/
+
+#include "dissipationSelector.H"
+#include "dictionary.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace compressible
+{
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+template<>
+const char* NamedEnum<dissipationSelector::treatment, 3>::names[] =
+{
+    "none",
+    "cascade",
+    "equilibrium"
+};
+
+const NamedEnum<dissipationSelector::treatment, 3>
+    dissipationSelector::typeNames;
+
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+
+dissipationSelector::dissipationSelector
+(
+    const turbulenceModel& turbModel,
+    const treatment treatmentType
+)
+:
+    turbModel_(turbModel),
+    treatment_(treatmentType)
+{}
+
+
+dissipationSelector::dissipationSelector
+(
+    const turbulenceModel& turbModel,
+    const dictionary& dict
+)
+:
+    turbModel_(turbModel),
+    treatment_(none)
+{
+
+    word modelType;
+
+    if (dict.readIfPresent<word>("dissipation", modelType))
+    {
+        treatment_ = typeNames[modelType];
+    }
+}
+
+
+tmp<volScalarField>
+dissipationSelector::dissipation() const
+{
+    if (treatment_ == cascade)
+    {
+        return turbModel_.thermalDissipation();
+    }
+    else if (treatment_ == equilibrium)
+    {
+        return turbModel_.thermalDissipationEff();
+    }
+    else
+    {
+        // a bit wasteful, but we'll avoid it with 'enabled' query anyhow
+        tmp<volScalarField> tField = turbModel_.thermalDissipation();
+        tField() = 0.0;
+
+        return tField;
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace compressible
+} // End namespace Foam
+
+// ************************************************************************* //
