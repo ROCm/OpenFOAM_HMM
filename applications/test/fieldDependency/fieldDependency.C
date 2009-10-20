dfd82248e26c567f198fcb306b47bd6b7d26f41b
henry <Henry Weller h.weller@opencfd.co.uk>
2009-10-20 18:30:55 +0100

    First level of support for cached temporary fields.

---
+++ applications/test/fieldDependency/fieldDependency.C
@@ -0,0 +1,102 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2009 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Description
+    Test field dependencies.
+
+\*---------------------------------------------------------------------------*/
+
+#include "argList.H"
+#include "Time.H"
+#include "volFields.H"
+
+using namespace Foam;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+// Main program:
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    Info<< "Creating field T\n" << endl;
+    volScalarField T
+    (
+        IOobject
+        (
+            "T",
+            runTime.timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimensionedScalar("zero", dimless, 0)
+    );
+
+    Info<< "Creating field p\n" << endl;
+    volScalarField p
+    (
+        IOobject
+        (
+            "p",
+            runTime.timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimensionedScalar("zero", dimless, 0)
+    );
+
+
+    Info<< "p.eventNo:" << p.eventNo() << endl;
+    Info<< "p.uptodate:" << p.upToDate("T")<< endl;
+
+    // Change T and mark as uptodate.
+    Info<< "Changing T" << endl;
+    T = 0.0;
+    T.setUpToDate();
+    Info<< "T.eventNo:" << T.eventNo() << endl;
+
+    // Check p dependency:
+    Info<< "p.uptodate:" << p.upToDate("T")<< endl;
+
+    // Change p and mark as uptodate.
+    Info<< "Changing p." << endl;
+    p.setUpToDate();
+    Info<< "p.uptodate:" << p.upToDate("T")<< endl;
+    Info<< "p.eventNo:" << p.eventNo() << endl;
+
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
