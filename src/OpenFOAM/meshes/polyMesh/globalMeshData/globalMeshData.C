69215358f617b95c152a9e4fad2dfd45bfe89d9e
andy <a.heather@opencfd.co.uk>
2010-02-08 15:51:20 +0000

    STYLE: Minor code style corrections

--- src/OpenFOAM/meshes/polyMesh/globalMeshData/globalMeshData.C
+++ src/OpenFOAM/meshes/polyMesh/globalMeshData/globalMeshData.C
@@ -703,6 +703,7 @@ void Foam::globalMeshData::calcPointBoundaryFaces
     }
 }
 
+
 void Foam::globalMeshData::calcGlobalPointBoundaryFaces() const
 {
     if (debug)
@@ -1743,12 +1744,12 @@ Foam::autoPtr<Foam::globalIndex> Foam::globalMeshData::mergePoints
 
     label myUniquePoints = meshPoints.size() + nCoupledMaster - nCoupledSlave;
 
-Pout<< "Points :" << nl
-    << "    patch            : " << meshPoints.size() << nl
-    << "    of which coupled : " << nCoupledMaster+nCoupledSlave << nl
-    << "    of which master  : " << nCoupledMaster << nl
-    << "    of which slave   : " << nCoupledSlave << nl
-    << endl;
+    Pout<< "Points :" << nl
+        << "    patch            : " << meshPoints.size() << nl
+        << "    of which coupled : " << nCoupledMaster+nCoupledSlave << nl
+        << "    of which master  : " << nCoupledMaster << nl
+        << "    of which slave   : " << nCoupledSlave << nl
+        << endl;
 
 
     // 2. Create global indexing for unique points.
