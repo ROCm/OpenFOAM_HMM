182b368bf6354224f28b02a1cc881a3c3c12c410
Mark Olesen <Mark.Olesen@Germany>
2010-05-17 16:06:53 +0200

    ENH: change return type of Time::stopAt to report if the setting changed

--- src/OpenFOAM/db/Time/Time.C
+++ src/OpenFOAM/db/Time/Time.C
@@ -552,8 +552,9 @@ bool Foam::Time::end() const
 }
 
 
-void Foam::Time::stopAt(const stopAtControls sa) const
+bool Foam::Time::stopAt(const stopAtControls sa) const
 {
+    const bool changed = (stopAt_ != sa);
     stopAt_ = sa;
 
     // adjust endTime
@@ -565,6 +566,7 @@ void Foam::Time::stopAt(const stopAtControls sa) const
     {
         endTime_ = GREAT;
     }
+    return changed;
 }
 
 
