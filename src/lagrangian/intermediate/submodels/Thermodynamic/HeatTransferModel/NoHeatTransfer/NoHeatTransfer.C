bbd4e103880f819bb3855038c90f806013324bbf
andy <a.heather@opencfd.co.uk>
2010-10-28 15:44:47 +0100

    ENH: Added active() function to NoHeatTransfer model

--- src/lagrangian/intermediate/submodels/Thermodynamic/HeatTransferModel/NoHeatTransfer/NoHeatTransfer.C
+++ src/lagrangian/intermediate/submodels/Thermodynamic/HeatTransferModel/NoHeatTransfer/NoHeatTransfer.C
@@ -58,6 +58,13 @@ Foam::NoHeatTransfer<CloudType>::~NoHeatTransfer()
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
 template<class CloudType>
+bool Foam::NoHeatTransfer<CloudType>::active() const
+{
+    return false;
+}
+
+
+template<class CloudType>
 Foam::scalar Foam::NoHeatTransfer<CloudType>::Nu
 (
     const scalar,
