e338bd79ca0a2e3c6d79473d2355e659a93b5e03
andy <a.heather@opencfd.co.uk>
2010-11-05 12:52:30 +0000

    BUG: Corrected missing sqrt(2) factors in incompressible kOmegaSST model

--- src/turbulenceModels/incompressible/RAS/kOmegaSST/kOmegaSST.C
+++ src/turbulenceModels/incompressible/RAS/kOmegaSST/kOmegaSST.C
@@ -246,7 +246,7 @@ kOmegaSST::kOmegaSST
       / max
         (
             a1_*omega_,
-            F2()*mag(symm(fvc::grad(U_)))
+            F2()*sqrt(2.0)*mag(symm(fvc::grad(U_)))
         )
     );
     nut_.correctBoundaryConditions();
@@ -400,7 +400,7 @@ void kOmegaSST::correct()
 
 
     // Re-calculate viscosity
-    nut_ = a1_*k_/max(a1_*omega_, F2()*sqrt(S2));
+    nut_ = a1_*k_/max(a1_*omega_, F2()*sqrt(2*S2));
     nut_.correctBoundaryConditions();
 }
 
