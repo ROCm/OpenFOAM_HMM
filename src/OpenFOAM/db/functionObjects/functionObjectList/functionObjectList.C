57a443a1834d46f8f8d1fdc07e1cef20b2d6a17c
mattijs <mattijs>
2010-11-16 12:38:07 +0000

    STYLE: functionObjectList : indentation

--- src/OpenFOAM/db/functionObjects/functionObjectList/functionObjectList.C
+++ src/OpenFOAM/db/functionObjects/functionObjectList/functionObjectList.C
@@ -198,7 +198,13 @@ bool Foam::functionObjectList::read()
     }
 
     // Update existing and add new functionObjects
-    const entry* entryPtr = parentDict_.lookupEntryPtr("functions",false,false);
+    const entry* entryPtr = parentDict_.lookupEntryPtr
+    (
+        "functions",
+        false,
+        false
+    );
+
     if (entryPtr)
     {
         PtrList<functionObject> newPtrs;
