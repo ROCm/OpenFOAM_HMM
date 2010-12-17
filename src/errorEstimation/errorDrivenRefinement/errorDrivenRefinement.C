bd7d27e6224d39835f9dd554a4e3706660db3033
Mark Olesen <Mark.Olesen@Germany>
2010-12-17 16:43:37 +0100

    COMP: avoid ambiguous construct from tmp - engine

--- src/errorEstimation/errorDrivenRefinement/errorDrivenRefinement.C
+++ src/errorEstimation/errorDrivenRefinement/errorDrivenRefinement.C
@@ -130,7 +130,7 @@ void Foam::errorDrivenRefinement::setRefinement(polyTopoChange& ref) const
     const volVectorField& resError =
         runTime.lookupObject<volVectorField>(errorField_);
 
-    const volScalarField magResError = Foam::mag(resError);
+    const volScalarField magResError(Foam::mag(resError));
 
     scalar min = Foam::min(magResError).value();
     scalar max = Foam::max(magResError).value();
