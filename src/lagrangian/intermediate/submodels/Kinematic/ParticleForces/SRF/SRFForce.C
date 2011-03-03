467eb90a7189df973234adbb053334379457582e
graham <g.macpherson@opencfd.co.uk>
2011-03-03 11:19:29 +0000

    BUG: mass multiples both terms in SRF.

--- src/lagrangian/intermediate/submodels/Kinematic/ParticleForces/SRF/SRFForce.C
+++ src/lagrangian/intermediate/submodels/Kinematic/ParticleForces/SRF/SRFForce.C
@@ -97,7 +97,7 @@ Foam::forceSuSp Foam::SRFForce<CloudType>::calcNonCoupled
     const vector r = p.position() - axis*(axis & p.position());
 
     // Coriolis and centrifugal acceleration terms
-    value.Su() = mass*2.0*(p.U() ^ omega) + (omega ^ (r ^ omega));
+    value.Su() = mass*(2.0*(p.U() ^ omega) + (omega ^ (r ^ omega)));
 
     return value;
 }
