d6e24bb6684afcd29ea6554bb205c5c6c066dee7
Sergio Ferraris <sergio@tom.opencfd.co.uk>
2011-06-09 15:48:04 +0100

    BUG: Missing complete re-start for mixed BC

--- applications/solvers/compressible/rhoCentralFoam/BCs/U/maxwellSlipUFvPatchVectorField.C
+++ applications/solvers/compressible/rhoCentralFoam/BCs/U/maxwellSlipUFvPatchVectorField.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2004-2010 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2004-2011 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -103,6 +103,9 @@ maxwellSlipUFvPatchVectorField::maxwellSlipUFvPatchVectorField
         (
             vectorField("value", dict, p.size())
         );
+
+        this->refValue() = vectorField("refValue", dict, p.size());
+        this->valueFraction() = scalarField("valueFraction", dict, p.size());
     }
     else
     {
