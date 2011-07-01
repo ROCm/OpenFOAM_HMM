0e91b291a469c2488b834ad65e26c1dd884c23e8
graham <g.macpherson@opencfd.co.uk>
2011-07-01 10:49:15 +0100

    BUG: Uninitialised point.

--- src/OpenFOAM/algorithms/indexedOctree/indexedOctree.C
+++ src/OpenFOAM/algorithms/indexedOctree/indexedOctree.C
@@ -2568,7 +2568,7 @@ Foam::pointIndexHit Foam::indexedOctree<Type>::findNearest
 {
     scalar nearestDistSqr = startDistSqr;
     label nearestShapeI = -1;
-    point nearestPoint;
+    point nearestPoint = vector::zero;
 
     if (nodes_.size())
     {
@@ -2581,10 +2581,6 @@ Foam::pointIndexHit Foam::indexedOctree<Type>::findNearest
             nearestShapeI,
             nearestPoint
         );
-    }
-    else
-    {
-        nearestPoint = vector::zero;
     }
 
     return pointIndexHit(nearestShapeI != -1, nearestPoint, nearestShapeI);
