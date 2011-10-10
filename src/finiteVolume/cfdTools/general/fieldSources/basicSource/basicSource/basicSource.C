b69ebd9e77c6289bd6d1224acb07a3acb3452016
Sergio Ferraris <sergio@tom.emea.sgi.com>
2011-10-10 12:01:49 +0100

    ENH: setExplicitValue source

--- src/finiteVolume/cfdTools/general/fieldSources/basicSource/basicSource/basicSource.C
+++ src/finiteVolume/cfdTools/general/fieldSources/basicSource/basicSource/basicSource.C
@@ -116,10 +116,15 @@ void Foam::basicSource::setCellSet()
                 label globalCellI = returnReduce(cellI, maxOp<label>());
                 if (globalCellI < 0)
                 {
-                    WarningIn("TimeActivatedExplicitSource<Type>::setCellIds()")
-                        << "Unable to find owner cell for point " << points_[i]
-                        << endl;
+                    WarningIn
+                    (
+                        "TimeActivatedExplicitSource<Type>::setCellIds()"
+                    )
+                    << "Unable to find owner cell for point " << points_[i]
+                    << endl;
+
                 }
+
             }
 
             cells_ = selectedCells.toc();
@@ -269,5 +274,31 @@ bool Foam::basicSource::isActive()
     }
 }
 
+
+void Foam::basicSource::addSu(Foam::fvMatrix<vector>& Eqn)
+{
+    notImplemented
+    (
+        "Foam::basicSource addSu(Foam::fvMatrix<vector>& Eqn)"
+    );
+}
+
+
+void Foam::basicSource::addSu(Foam::fvMatrix<scalar>& Eqn)
+{
+    notImplemented
+    (
+        "Foam::basicSource addSu(Foam::fvMatrix<scalar>& Eqn)"
+    );
+}
+
+
+void Foam::basicSource::setValue(Foam::fvMatrix<scalar>& Eqn)
+{
+    notImplemented
+    (
+        "Foam::basicSource setValue(Foam::fvMatrix<scalar>& Eqn)"
+    );
+}
 
 // ************************************************************************* //
