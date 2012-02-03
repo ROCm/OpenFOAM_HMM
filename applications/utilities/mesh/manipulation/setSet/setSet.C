cd01c8f1a26f6ec45920590861b5e1e4a6b3fa55
mattijs <mattijs>
2012-02-03 15:53:37 +0000

    BUG: mergePolyMesh: merging zones

--- applications/utilities/mesh/manipulation/setSet/setSet.C
+++ applications/utilities/mesh/manipulation/setSet/setSet.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -853,7 +853,7 @@ int main(int argc, char *argv[])
 
     // Read history if interactive
 #   ifdef HAS_READLINE
-    if (!batch && !read_history(historyFile))
+    if (!batch && !read_history(runTime.path()/historyFile))
     {
         Info<< "Successfully read history from " << historyFile << endl;
     }
