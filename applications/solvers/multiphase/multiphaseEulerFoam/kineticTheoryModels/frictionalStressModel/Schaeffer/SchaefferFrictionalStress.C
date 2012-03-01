c2041f9dba21f47e50e39f975abf6332f37f42af
Henry <Henry>
2012-03-01 18:22:59 +0000

    multiphase: completed phase renaming

--- applications/solvers/multiphase/multiphaseEulerFoam/kineticTheoryModels/frictionalStressModel/Schaeffer/SchaefferFrictionalStress.C
+++ applications/solvers/multiphase/multiphaseEulerFoam/kineticTheoryModels/frictionalStressModel/Schaeffer/SchaefferFrictionalStress.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -71,8 +71,8 @@ Foam::kineticTheoryModels::frictionalStressModels::Schaeffer::
 frictionalPressure
 (
     const volScalarField& alpha1,
-    const dimensionedScalar& alpha1MinFriction,
-    const dimensionedScalar& alpha1Max,
+    const dimensionedScalar& alphaMinFriction,
+    const dimensionedScalar& alphaMax,
     const dimensionedScalar& Fr,
     const dimensionedScalar& eta,
     const dimensionedScalar& p
@@ -80,7 +80,7 @@ frictionalPressure
 {
     return
         dimensionedScalar("1e24", dimensionSet(1, -1, -2, 0, 0), 1e24)
-       *pow(Foam::max(alpha1 - alpha1MinFriction, scalar(0)), 10.0);
+       *pow(Foam::max(alpha1 - alphaMinFriction, scalar(0)), 10.0);
 }
 
 
@@ -89,8 +89,8 @@ Foam::kineticTheoryModels::frictionalStressModels::Schaeffer::
 frictionalPressurePrime
 (
     const volScalarField& alpha1,
-    const dimensionedScalar& alpha1MinFriction,
-    const dimensionedScalar& alpha1Max,
+    const dimensionedScalar& alphaMinFriction,
+    const dimensionedScalar& alphaMax,
     const dimensionedScalar& Fr,
     const dimensionedScalar& eta,
     const dimensionedScalar& p
@@ -98,7 +98,7 @@ frictionalPressurePrime
 {
     return
         dimensionedScalar("1e25", dimensionSet(1, -1, -2, 0, 0), 1e25)
-       *pow(Foam::max(alpha1 - alpha1MinFriction, scalar(0)), 9.0);
+       *pow(Foam::max(alpha1 - alphaMinFriction, scalar(0)), 9.0);
 }
 
 
@@ -106,7 +106,7 @@ Foam::tmp<Foam::volScalarField>
 Foam::kineticTheoryModels::frictionalStressModels::Schaeffer::muf
 (
     const volScalarField& alpha1,
-    const dimensionedScalar& alpha1Max,
+    const dimensionedScalar& alphaMax,
     const volScalarField& pf,
     const volSymmTensorField& D,
     const dimensionedScalar& phi
@@ -135,7 +135,7 @@ Foam::kineticTheoryModels::frictionalStressModels::Schaeffer::muf
 
     forAll (D, celli)
     {
-        if (alpha1[celli] > alpha1Max.value() - 5e-2)
+        if (alpha1[celli] > alphaMax.value() - 5e-2)
         {
             muff[celli] =
                 0.5*pf[celli]*sin(phi.value())
