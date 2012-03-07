62e3b2963bfeb7a55a0903eb77049016fe288a0c
andy <andy>
2012-03-07 15:43:34 +0000

    ENH: Improvements to the turbulenceFields function object

--- src/postProcessing/functionObjects/field/turbulenceFields/turbulenceFieldsTemplates.C
+++ src/postProcessing/functionObjects/field/turbulenceFields/turbulenceFieldsTemplates.C
@@ -31,19 +31,49 @@ template<class Type>
 void Foam::turbulenceFields::processField
 (
     const word& fieldName,
-    const GeometricField<Type, fvPatchField, volMesh>& value
+    const tmp<GeometricField<Type, fvPatchField, volMesh> >& tvalue
 )
 {
     typedef GeometricField<Type, fvPatchField, volMesh> FieldType;
-    if (obr_.foundObject<FieldType>(fieldName))
+
+    const word scopedName = modelName + "::" + fieldName;
+
+    if (obr_.foundObject<FieldType>(scopedName))
     {
         FieldType& fld =
             const_cast<FieldType&>(obr_.lookupObject<FieldType>(fieldName));
-        fld == value;
+        fld == tvalue();
+    }
+    else if (obr_.found(scopedName))
+    {
+        WarningIn
+        (
+            "void Foam::turbulenceFields::processField"
+            "("
+                "const word&, "
+                "const tmp<GeometricField<Type, fvPatchField, volMesh> >&"
+            ")"
+        )   << "Cannot store turbulence field " << scopedName
+            << " since an object with that name already exists"
+            << nl << endl;
     }
     else
     {
-        obr_.store(new FieldType(io(fieldName), value));
+        obr_.store
+        (
+            new FieldType
+            (
+                IOobject
+                (
+                    scopedName,
+                    obr_.time().timeName(),
+                    obr_,
+                    IOobject::READ_IF_PRESENT,
+                    IOobject::NO_WRITE
+                ),
+                tvalue
+            )
+        );
     }
 }
 
