3fd792b2aee39e79d426691f700b16a44be0f7e6
andy <andy>
2012-03-14 15:12:42 +0000

    ENH: Removed unnecessary #include

--- src/combustionModels/psiCombustionModel/psiCombustionModels.C
+++ src/combustionModels/psiCombustionModel/psiCombustionModels.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -24,9 +24,7 @@ License
 \*---------------------------------------------------------------------------*/
 
 #include "makeCombustionTypes.H"
-
 #include "psiCombustionModel.H"
-#include "PaSR.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
