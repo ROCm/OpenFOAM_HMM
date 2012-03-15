2ce9d80e90e27ae02f4529675f3a35440535658a
mattijs <mattijs>
2012-03-15 15:43:09 +0000

    ENH: globalMeshData: allow call with empty transformed elements list

--- src/OpenFOAM/meshes/polyMesh/globalMeshData/globalMeshData.C
+++ src/OpenFOAM/meshes/polyMesh/globalMeshData/globalMeshData.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -2403,7 +2403,7 @@ Foam::autoPtr<Foam::globalIndex> Foam::globalMeshData::mergePoints
     (
         masterGlobalPoint,
         pointSlaves,
-        labelListList(cpp.nPoints()),   // no transforms
+        labelListList(0),   // no transforms
         pointSlavesMap,
         maxEqOp<label>()
     );
@@ -2684,7 +2684,7 @@ Foam::autoPtr<Foam::globalIndex> Foam::globalMeshData::mergePoints
     (
         globalMaster,
         pointSlaves,
-        labelListList(cpp.nPoints()),   // no transforms
+        labelListList(0),   // no transforms
         pointSlavesMap,
         maxEqOp<label>()
     );
