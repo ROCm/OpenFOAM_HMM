ec257834101f4d35f338a12fc9a3a8cb62c1b348
mattijs <mattijs>
2012-03-26 12:17:46 +0100

    ENH: splitMeshRegions, autoHexMesh: allocate face-decomp structure before doing findCell

--- applications/utilities/mesh/manipulation/splitMeshRegions/splitMeshRegions.C
+++ applications/utilities/mesh/manipulation/splitMeshRegions/splitMeshRegions.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -2193,6 +2193,8 @@ int main(int argc, char *argv[])
             const point insidePoint = args.optionRead<point>("insidePoint");
 
             label regionI = -1;
+
+            (void)mesh.tetBasePtIs();
 
             label cellI = mesh.findCell(insidePoint);
 
