154d7fafaba32e3328ef03a1216d26e46a04f527
Henry <Henry>
2012-04-25 18:19:18 +0100

    LduMatrix: Added missing file

---
+++ src/OpenFOAM/matrices/LduMatrix/LduMatrix/LduInterfaceField/LduInterfaceFields.C
@@ -0,0 +1,41 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "LduInterfaceField.H"
+#include "fieldTypes.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+defineTemplateTypeNameAndDebug(Foam::LduInterfaceField<Foam::scalar>, 0);
+defineTemplateTypeNameAndDebug(Foam::LduInterfaceField<Foam::vector>, 0);
+defineTemplateTypeNameAndDebug
+(
+    Foam::LduInterfaceField<Foam::sphericalTensor>,
+    0
+);
+defineTemplateTypeNameAndDebug(Foam::LduInterfaceField<Foam::symmTensor>, 0);
+defineTemplateTypeNameAndDebug(Foam::LduInterfaceField<Foam::tensor>, 0);
+
+// ************************************************************************* //
