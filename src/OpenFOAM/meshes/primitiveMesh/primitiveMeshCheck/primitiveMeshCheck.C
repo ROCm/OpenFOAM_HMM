60559ff3c72add4d936de9381b8ba479a2c58373
mattijs <mattijs>
2012-06-06 14:03:28 +0100

    STYLE: primitiveMeshCheck: typo

--- src/OpenFOAM/meshes/primitiveMesh/primitiveMeshCheck/primitiveMeshCheck.C
+++ src/OpenFOAM/meshes/primitiveMesh/primitiveMeshCheck/primitiveMeshCheck.C
@@ -337,7 +337,7 @@ bool Foam::primitiveMesh::checkFaceAreas
     {
         if (debug || report)
         {
-            Info<< "    Minumum face area = " << minArea
+            Info<< "    Minimum face area = " << minArea
                 << ". Maximum face area = " << maxArea
                 << ".  Face area magnitudes OK." << endl;
         }
