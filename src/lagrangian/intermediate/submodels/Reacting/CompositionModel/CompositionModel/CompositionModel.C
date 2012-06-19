847045f781de8544ec0339cc35ae9ca632c3d1bc
andy <andy>
2012-06-19 15:12:25 +0100

    BUG: Cloud composition model - use Ha, not Hs

--- src/lagrangian/intermediate/submodels/Reacting/CompositionModel/CompositionModel/CompositionModel.C
+++ src/lagrangian/intermediate/submodels/Reacting/CompositionModel/CompositionModel/CompositionModel.C
@@ -396,7 +396,7 @@ Foam::scalar Foam::CompositionModel<CloudType>::H
             forAll(Y, i)
             {
                 label gid = props.globalIds()[i];
-                HMixture += Y[i]*thermo_.carrier().Hs(gid, p, T);
+                HMixture += Y[i]*thermo_.carrier().Ha(gid, p, T);
             }
             break;
         }
