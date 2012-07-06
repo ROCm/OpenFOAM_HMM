50965fe65e01e24c7e0c0b10842d87e2b3b4eaa0
andy <andy>
2012-07-06 14:44:59 +0100

    ENH: Enabled both compressible and incompressible usage for the fixedShearStress BC

--- src/turbulenceModels/derivedFvPatchFields/fixedShearStress/fixedShearStressFvPatchVectorField.C
+++ src/turbulenceModels/derivedFvPatchFields/fixedShearStress/fixedShearStressFvPatchVectorField.C
@@ -28,28 +28,25 @@ License
 #include "addToRunTimeSelectionTable.H"
 #include "fvPatchFieldMapper.H"
 #include "volFields.H"
-#include "RASModel.H"
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
-namespace Foam
-{
-namespace incompressible
-{
+#include "incompressible/RAS/RASModel/RASModel.H"
+#include "compressible/RAS/RASModel/RASModel.H"
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
-fixedShearStressFvPatchVectorField::fixedShearStressFvPatchVectorField
+Foam::fixedShearStressFvPatchVectorField::fixedShearStressFvPatchVectorField
 (
     const fvPatch& p,
     const DimensionedField<vector, volMesh>& iF
 )
 :
     fixedValueFvPatchVectorField(p, iF),
+    phiName_("phi"),
+    rhoName_("rho"),
     tau0_(vector::zero)
 {}
 
 
-fixedShearStressFvPatchVectorField::fixedShearStressFvPatchVectorField
+Foam::fixedShearStressFvPatchVectorField::fixedShearStressFvPatchVectorField
 (
     const fvPatch& p,
     const DimensionedField<vector, volMesh>& iF,
@@ -57,13 +54,15 @@ fixedShearStressFvPatchVectorField::fixedShearStressFvPatchVectorField
 )
 :
     fixedValueFvPatchVectorField(p, iF),
+    phiName_(dict.lookupOrDefault<word>("phi", "phi")),
+    rhoName_(dict.lookupOrDefault<word>("rho", "rho")),
     tau0_(dict.lookupOrDefault<vector>("tau", vector::zero))
 {
     fvPatchField<vector>::operator=(patchInternalField());
 }
 
 
-fixedShearStressFvPatchVectorField::fixedShearStressFvPatchVectorField
+Foam::fixedShearStressFvPatchVectorField::fixedShearStressFvPatchVectorField
 (
     const fixedShearStressFvPatchVectorField& ptf,
     const fvPatch& p,
@@ -72,76 +71,102 @@ fixedShearStressFvPatchVectorField::fixedShearStressFvPatchVectorField
 )
 :
     fixedValueFvPatchVectorField(ptf, p, iF, mapper),
+    phiName_(ptf.phiName_),
+    rhoName_(ptf.rhoName_),
     tau0_(ptf.tau0_)
 {}
 
 
-fixedShearStressFvPatchVectorField::fixedShearStressFvPatchVectorField
+Foam::fixedShearStressFvPatchVectorField::fixedShearStressFvPatchVectorField
 (
     const fixedShearStressFvPatchVectorField& ptf
 )
 :
     fixedValueFvPatchVectorField(ptf),
+    phiName_(ptf.phiName_),
+    rhoName_(ptf.rhoName_),
     tau0_(ptf.tau0_)
 {}
 
 
-fixedShearStressFvPatchVectorField::fixedShearStressFvPatchVectorField
+Foam::fixedShearStressFvPatchVectorField::fixedShearStressFvPatchVectorField
 (
     const fixedShearStressFvPatchVectorField& ptf,
     const DimensionedField<vector, volMesh>& iF
 )
 :
     fixedValueFvPatchVectorField(ptf, iF),
+    phiName_(ptf.phiName_),
+    rhoName_(ptf.rhoName_),
     tau0_(ptf.tau0_)
 {}
 
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
-void fixedShearStressFvPatchVectorField::updateCoeffs()
+void Foam::fixedShearStressFvPatchVectorField::updateCoeffs()
 {
     if (updated())
     {
         return;
     }
 
-    const label patchi = patch().index();
+    const label patchI = patch().index();
 
-    const turbulenceModel& turbModel =
-        db().lookupObject<turbulenceModel>("turbulenceModel");
+    const surfaceScalarField& phi =
+        db().lookupObject<surfaceScalarField>(phiName_);
 
-    const fvPatchVectorField& Uw = turbModel.U().boundaryField()[patchi];
+    scalarField nuEff;
+    if (phi.dimensions() == dimVelocity*dimArea)
+    {
+        const incompressible::turbulenceModel& turbModel =
+            db().lookupObject<incompressible::turbulenceModel>
+            (
+                "turbulenceModel"
+            );
 
-    const vectorField Ui(Uw.patchInternalField());
+        nuEff = turbModel.nuEff()()[patchI];
+    }
+    else if (phi.dimensions() == dimDensity*dimVelocity*dimArea)
+    {
+        const compressible::turbulenceModel& turbModel =
+            db().lookupObject<compressible::turbulenceModel>
+            (
+                "turbulenceModel"
+            );
+
+        const fvPatchField<scalar>& rhop =
+            patch().lookupPatchField<volScalarField, scalar>(rhoName_);
+
+        nuEff = turbModel.muEff()()[patchI]/rhop;
+    }
+    else
+    {
+        FatalErrorIn("fixedShearStressFvPatchVectorField::updateCoeffs()")
+            << "dimensions of phi are not correct"
+            << "\n    on patch " << this->patch().name()
+            << " of field " << this->dimensionedInternalField().name()
+            << " in file " << this->dimensionedInternalField().objectPath()
+            << exit(FatalError);
+    }
+
+    const vectorField Uc(patchInternalField());
 
     vector tauHat = tau0_/(mag(tau0_) + ROOTVSMALL);
 
     const scalarField& ry = patch().deltaCoeffs();
 
-    tmp<volScalarField> tnuEff = turbModel.nuEff();
-    const volScalarField& nuEff = tnuEff();
-    const scalarField& nuEffw = nuEff.boundaryField()[patchi];
-
-    tmp<vectorField> UwUpdated =
-        tauHat*(tauHat & (tau0_*(1.0/(ry*nuEffw)) + Ui));
-
-    operator==(UwUpdated);
-
-    if (debug)
-    {
-        tmp<vectorField> nHat = this->patch().nf();
-        volSymmTensorField Reff(turbModel.devReff());
-        Info << "tau : " << (nHat & Reff.boundaryField()[patchi])() << endl;
-    }
+    operator==(tauHat*(tauHat & (tau0_*(1.0/(ry*nuEff)) + Uc)));
 
     fixedValueFvPatchVectorField::updateCoeffs();
 }
 
 
-void fixedShearStressFvPatchVectorField::write(Ostream& os) const
+void Foam::fixedShearStressFvPatchVectorField::write(Ostream& os) const
 {
     fixedValueFvPatchVectorField::write(os);
+    writeEntryIfDifferent<word>(os, "phi", "phi", phiName_);
+    writeEntryIfDifferent<word>(os, "rho", "rho", rhoName_);
     os.writeKeyword("tau") << tau0_ << token::END_STATEMENT << nl;
     writeEntry("value", os);
 }
@@ -149,15 +174,13 @@ void fixedShearStressFvPatchVectorField::write(Ostream& os) const
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-makePatchTypeField
-(
-    fvPatchVectorField,
-    fixedShearStressFvPatchVectorField
-);
-
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
-} // End namespace incompressible
-} // End namespace Foam
+namespace Foam
+{
+    makePatchTypeField
+    (
+        fvPatchVectorField,
+        fixedShearStressFvPatchVectorField
+    );
+}
 
 // ************************************************************************* //
