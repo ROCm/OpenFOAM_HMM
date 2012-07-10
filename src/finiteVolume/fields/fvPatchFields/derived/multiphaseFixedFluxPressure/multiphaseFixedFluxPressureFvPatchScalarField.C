04f044c14c95dded8ae4410a8414128cecad8ace
andy <andy>
2012-07-10 17:09:42 +0100

    ENH: Boundary condition doxygen documentation updates

--- src/finiteVolume/fields/fvPatchFields/derived/multiphaseFixedFluxPressure/multiphaseFixedFluxPressureFvPatchScalarField.C
+++ src/finiteVolume/fields/fvPatchFields/derived/multiphaseFixedFluxPressure/multiphaseFixedFluxPressureFvPatchScalarField.C
@@ -41,7 +41,8 @@ multiphaseFixedFluxPressureFvPatchScalarField
     fixedGradientFvPatchScalarField(p, iF),
     phiHbyAName_("phiHbyA"),
     phiName_("phi"),
-    rhoName_("rho")
+    rhoName_("rho"),
+    DpName_("Dp")
 {}
 
 
@@ -57,7 +58,8 @@ multiphaseFixedFluxPressureFvPatchScalarField
     fixedGradientFvPatchScalarField(ptf, p, iF, mapper),
     phiHbyAName_(ptf.phiHbyAName_),
     phiName_(ptf.phiName_),
-    rhoName_(ptf.rhoName_)
+    rhoName_(ptf.rhoName_),
+    DpName_(ptf.DpName_)
 {}
 
 
@@ -72,7 +74,8 @@ multiphaseFixedFluxPressureFvPatchScalarField
     fixedGradientFvPatchScalarField(p, iF),
     phiHbyAName_(dict.lookupOrDefault<word>("phiHbyA", "phiHbyA")),
     phiName_(dict.lookupOrDefault<word>("phi", "phi")),
-    rhoName_(dict.lookupOrDefault<word>("rho", "rho"))
+    rhoName_(dict.lookupOrDefault<word>("rho", "rho")),
+    DpName_(dict.lookupOrDefault<word>("Dp", "Dp"))
 {
     if (dict.found("gradient"))
     {
@@ -91,27 +94,29 @@ multiphaseFixedFluxPressureFvPatchScalarField
 Foam::multiphaseFixedFluxPressureFvPatchScalarField::
 multiphaseFixedFluxPressureFvPatchScalarField
 (
-    const multiphaseFixedFluxPressureFvPatchScalarField& wbppsf
+    const multiphaseFixedFluxPressureFvPatchScalarField& mfppsf
 )
 :
-    fixedGradientFvPatchScalarField(wbppsf),
-    phiHbyAName_(wbppsf.phiHbyAName_),
-    phiName_(wbppsf.phiName_),
-    rhoName_(wbppsf.rhoName_)
+    fixedGradientFvPatchScalarField(mfppsf),
+    phiHbyAName_(mfppsf.phiHbyAName_),
+    phiName_(mfppsf.phiName_),
+    rhoName_(mfppsf.rhoName_),
+    DpName_(mfppsf.DpName_)
 {}
 
 
 Foam::multiphaseFixedFluxPressureFvPatchScalarField::
 multiphaseFixedFluxPressureFvPatchScalarField
 (
-    const multiphaseFixedFluxPressureFvPatchScalarField& wbppsf,
+    const multiphaseFixedFluxPressureFvPatchScalarField& mfppsf,
     const DimensionedField<scalar, volMesh>& iF
 )
 :
-    fixedGradientFvPatchScalarField(wbppsf, iF),
-    phiHbyAName_(wbppsf.phiHbyAName_),
-    phiName_(wbppsf.phiName_),
-    rhoName_(wbppsf.rhoName_)
+    fixedGradientFvPatchScalarField(mfppsf, iF),
+    phiHbyAName_(mfppsf.phiHbyAName_),
+    phiName_(mfppsf.phiName_),
+    rhoName_(mfppsf.rhoName_),
+    DpName_(mfppsf.DpName_)
 {}
 
 
@@ -155,7 +160,7 @@ void Foam::multiphaseFixedFluxPressureFvPatchScalarField::updateCoeffs()
     */
 
     const fvsPatchField<scalar>& Dpp =
-        patch().lookupPatchField<surfaceScalarField, scalar>("Dp");
+        patch().lookupPatchField<surfaceScalarField, scalar>(DpName_);
 
     gradient() = (phiHbyAp - phip)/patch().magSf()/Dpp;
 
@@ -172,6 +177,7 @@ void Foam::multiphaseFixedFluxPressureFvPatchScalarField::write
     writeEntryIfDifferent<word>(os, "phiHbyA", "phiHbyA", phiHbyAName_);
     writeEntryIfDifferent<word>(os, "phi", "phi", phiName_);
     writeEntryIfDifferent<word>(os, "rho", "rho", rhoName_);
+    writeEntryIfDifferent<word>(os, "Dp", "Dp", DpName_);
     gradient().writeEntry("gradient", os);
     writeEntry("value", os);
 }
