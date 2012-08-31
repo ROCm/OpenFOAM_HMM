c1ef233e5d848eb7fe0c0575d5f09f7c726641ec
mattijs <mattijs>
2012-08-31 12:57:17 +0100

    ENH: pointMesh: use MeshObject form

--- src/fvMotionSolver/fvMotionSolvers/displacement/SBRStress/displacementSBRStressFvMotionSolver.C
+++ src/fvMotionSolver/fvMotionSolvers/displacement/SBRStress/displacementSBRStressFvMotionSolver.C
@@ -42,7 +42,7 @@ namespace Foam
 
     addToRunTimeSelectionTable
     (
-        fvMotionSolver,
+        motionSolver,
         displacementSBRStressFvMotionSolver,
         dictionary
     );
@@ -54,22 +54,11 @@ namespace Foam
 Foam::displacementSBRStressFvMotionSolver::displacementSBRStressFvMotionSolver
 (
     const polyMesh& mesh,
-    Istream& is
+    const IOdictionary& dict
 )
 :
-    displacementFvMotionSolver(mesh, is),
-    pointDisplacement_
-    (
-        IOobject
-        (
-            "pointDisplacement",
-            fvMesh_.time().timeName(),
-            fvMesh_,
-            IOobject::MUST_READ,
-            IOobject::AUTO_WRITE
-        ),
-        pointMesh::New(fvMesh_)
-    ),
+    displacementMotionSolver(mesh, dict, typeName),
+    fvMotionSolverCore(mesh),
     cellDisplacement_
     (
         IOobject
@@ -84,14 +73,14 @@ Foam::displacementSBRStressFvMotionSolver::displacementSBRStressFvMotionSolver
         dimensionedVector
         (
             "cellDisplacement",
-            pointDisplacement_.dimensions(),
+            pointDisplacement().dimensions(),
             vector::zero
         ),
-        cellMotionBoundaryTypes<vector>(pointDisplacement_.boundaryField())
+        cellMotionBoundaryTypes<vector>(pointDisplacement().boundaryField())
     ),
     diffusivityPtr_
     (
-        motionDiffusivity::New(fvMesh_, lookup("diffusivity"))
+        motionDiffusivity::New(fvMesh_, coeffDict().lookup("diffusivity"))
     )
 {}
 
@@ -116,7 +105,7 @@ Foam::displacementSBRStressFvMotionSolver::curPoints() const
 
     tmp<pointField> tcurPoints
     (
-        points0() + pointDisplacement_.internalField()
+        points0() + pointDisplacement().internalField()
     );
 
     twoDCorrectPoints(tcurPoints());
@@ -128,7 +117,7 @@ Foam::displacementSBRStressFvMotionSolver::curPoints() const
 void Foam::displacementSBRStressFvMotionSolver::solve()
 {
     // The points have moved so before interpolation update
-    // the motionSolver accordingly
+    // the mtionSolver accordingly
     movePoints(fvMesh_.points());
 
     diffusivityPtr_->correct();
@@ -192,12 +181,16 @@ void Foam::displacementSBRStressFvMotionSolver::updateMesh
     const mapPolyMesh& mpm
 )
 {
-    displacementFvMotionSolver::updateMesh(mpm);
+    displacementMotionSolver::updateMesh(mpm);
 
     // Update diffusivity. Note two stage to make sure old one is de-registered
     // before creating/registering new one.
     diffusivityPtr_.reset(NULL);
-    diffusivityPtr_ = motionDiffusivity::New(fvMesh_, lookup("diffusivity"));
+    diffusivityPtr_ = motionDiffusivity::New
+    (
+        fvMesh_,
+        coeffDict().lookup("diffusivity")
+    );
 }
 
 
