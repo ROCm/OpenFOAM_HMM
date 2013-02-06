792efbfc9252fd6ae015f94a68dc830be7bfe313
Henry <Henry>
2013-02-06 10:53:37 +0000

    perfectFluid: Added R as an independent parameter

--- src/thermophysicalModels/specie/equationOfState/perfectFluid/perfectFluid.C
+++ src/thermophysicalModels/specie/equationOfState/perfectFluid/perfectFluid.C
@@ -32,6 +32,7 @@ template<class Specie>
 Foam::perfectFluid<Specie>::perfectFluid(Istream& is)
 :
     Specie(is),
+    R_(readScalar(is)),
     rho0_(readScalar(is))
 {
     is.check("perfectFluid<Specie>::perfectFluid(Istream& is)");
@@ -42,6 +43,7 @@ template<class Specie>
 Foam::perfectFluid<Specie>::perfectFluid(const dictionary& dict)
 :
     Specie(dict),
+    R_(readScalar(dict.subDict("equationOfState").lookup("R"))),
     rho0_(readScalar(dict.subDict("equationOfState").lookup("rho0")))
 {}
 
@@ -54,6 +56,7 @@ void Foam::perfectFluid<Specie>::write(Ostream& os) const
     Specie::write(os);
 
     dictionary dict("equationOfState");
+    dict.add("R", R_);
     dict.add("rho0", rho0_);
 
     os  << indent << dict.dictName() << dict;
@@ -66,6 +69,7 @@ template<class Specie>
 Foam::Ostream& Foam::operator<<(Ostream& os, const perfectFluid<Specie>& pf)
 {
     os  << static_cast<const Specie&>(pf)
+        << token::SPACE << pf.R_
         << token::SPACE << pf.rho0_;
 
     os.check("Ostream& operator<<(Ostream&, const perfectFluid<Specie>&)");
