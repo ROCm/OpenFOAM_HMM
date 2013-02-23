14ace46518b75654094efad424731f43747e5686
Henry <Henry>
2013-02-23 15:50:42 +0000

    LeastSquaresGrad: rewritten to use compact storage of the mapped field

--- src/finiteVolume/finiteVolume/gradSchemes/LeastSquaresGrad/LeastSquaresGrad.C
+++ src/finiteVolume/finiteVolume/gradSchemes/LeastSquaresGrad/LeastSquaresGrad.C
@@ -84,32 +84,49 @@ Foam::fv::LeastSquaresGrad<Type, Stencil>::calcGrad
     Field<GradType>& lsGradIf = lsGrad;
 
     const extendedCentredCellToCellStencil& stencil = lsv.stencil();
+    const List<List<label> >& stencilAddr = stencil.stencil();
     const List<List<vector> >& lsvs = lsv.vectors();
 
-    // lsGrad = stencil.weightedSum(vtf, lsv.vectors());
+    // Construct flat version of vtf
+    // including all values referred to by the stencil
+    List<Type> flatVtf(stencil.map().constructSize(), pTraits<Type>::zero);
 
-    List<List<Type> > stencilVtf;
-    extendedCellToFaceStencil::collectData
-    (
-        stencil.map(),
-        stencil.stencil(),
-        vtf,
-        stencilVtf
-    );
-
-    forAll(lsGradIf, celli)
+    // Insert internal values
+    forAll(vtf, celli)
     {
-        const List<Type>& stfc = stencilVtf[celli];
-        const List<vector>& lsvc = lsvs[celli];
+        flatVtf[celli] = vtf[celli];
+    }
 
-        forAll(stfc, i)
+    // Insert boundary values
+    forAll(vtf.boundaryField(), patchi)
+    {
+        const fvPatchField<Type>& ptf = vtf.boundaryField()[patchi];
+        label nCompact = ptf.patch().start();
+
+        forAll(ptf, i)
         {
-            lsGradIf[celli] += lsvc[i]*stfc[i];
+            flatVtf[nCompact++] = ptf[i];
         }
     }
 
-    lsGrad.correctBoundaryConditions();
+    // Do all swapping to complete flatVtf
+    stencil.map().distribute(flatVtf);
 
+    // Accumulate the cell-centred gradient from the
+    // weighted least-squares vectors and the flattened field values
+    forAll(stencilAddr, celli)
+    {
+        const labelList& compactCells = stencilAddr[celli];
+        const List<vector>& lsvc = lsvs[celli];
+
+        forAll(compactCells, i)
+        {
+            lsGradIf[celli] += lsvc[i]*flatVtf[compactCells[i]];
+        }
+    }
+
+    // Correct the boundary conditions
+    lsGrad.correctBoundaryConditions();
     gaussGrad<Type>::correctBoundaryConditions(vtf, lsGrad);
 
     return tlsGrad;
