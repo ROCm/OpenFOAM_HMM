d66401c0be614adc77f8b46797818d94d5a0c1aa
laurence <laurence>
2013-05-31 09:30:00 +0100

    ENH: Move pointEdge sorting to PatchTools

--- src/OpenFOAM/meshes/primitiveMesh/PatchTools/PatchTools.C
+++ src/OpenFOAM/meshes/primitiveMesh/PatchTools/PatchTools.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -30,6 +30,7 @@ License
 #include "PatchToolsGatherAndMerge.C"
 #include "PatchToolsSearch.C"
 #include "PatchToolsSortEdges.C"
+#include "PatchToolsSortPoints.C"
 #include "PatchToolsNormals.C"
 #include "PatchToolsMatch.C"
 
