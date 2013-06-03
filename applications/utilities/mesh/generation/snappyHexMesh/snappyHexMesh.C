5d26732b7e3eb3a197befa24a80362a737c859ea
laurence <laurence>
2013-06-03 14:59:05 +0100

    ENH: snappyHexMesh: update outfile location for surfaceSimplify

--- applications/utilities/mesh/generation/snappyHexMesh/snappyHexMesh.C
+++ applications/utilities/mesh/generation/snappyHexMesh/snappyHexMesh.C
@@ -137,8 +137,11 @@ autoPtr<refinementSurfaces> createRefinementSurfaces
                 const scalar surfaceCellSize =
                     readScalar(scsDict.lookup("surfaceCellSizeCoeff"));
 
-                globalMinLevel[surfI] = label(1.0/surfaceCellSize);
-                globalMaxLevel[surfI] = label(1.0/surfaceCellSize);
+                const label calculatedCellLevel =
+                    round(::log(1.0/surfaceCellSize)/::log(2));
+
+                globalMinLevel[surfI] = calculatedCellLevel;
+                globalMaxLevel[surfI] = calculatedCellLevel;
                 globalLevelIncr[surfI] = shapeDict.lookupOrDefault
                 (
                     "gapLevelIncrement",
@@ -319,8 +322,14 @@ autoPtr<refinementSurfaces> createRefinementSurfaces
                                     scsDict.lookup("surfaceCellSizeCoeff")
                                 );
 
-                            globalMinLevel[surfI] = label(1.0/surfaceCellSize);
-                            globalMaxLevel[surfI] = label(1.0/surfaceCellSize);
+                            const label calculatedCellLevel =
+                                round
+                                (
+                                    ::log(1.0/surfaceCellSize)/::log(2)
+                                );
+
+                            globalMinLevel[surfI] = calculatedCellLevel;
+                            globalMaxLevel[surfI] = calculatedCellLevel;
                             globalLevelIncr[surfI] =
                                 shapeControlRegionDict.lookupOrDefault
                                 (
@@ -818,7 +827,8 @@ int main(int argc, char *argv[])
         meshPtr().addFvPatches(patches);
 
         const scalar initialCellSize = ::pow(meshPtr().V()[0], 1.0/3.0);
-        const label initialRefLevels = ::sqrt(initialCellSize/defaultCellSize);
+        const label initialRefLevels =
+            ::log(initialCellSize/defaultCellSize)/::log(2);
 
         Info<< "Default cell size = " << defaultCellSize << endl;
         Info<< "Initial cell size = " << initialCellSize << endl;
@@ -1518,12 +1528,14 @@ int main(int argc, char *argv[])
             }
         }
 
-        fileName outFileName("simplifiedSurface.obj");
-
-        if (args.optionFound("outFile"))
-        {
-            outFileName = word(args.optionLookup("outFile")());
-        }
+        fileName outFileName
+        (
+            args.optionLookupOrDefault<fileName>
+            (
+                "outFile",
+                "constant/triSurface/simplifiedSurface.stl"
+            )
+        );
 
         extractSurface
         (
