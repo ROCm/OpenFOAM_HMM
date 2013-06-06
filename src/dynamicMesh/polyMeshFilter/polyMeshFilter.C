0004dc6d1289863c2a77d8f8c3348be55ace6b8a
laurence <laurence>
2013-06-06 11:53:46 +0100

    BUG: polyMeshFilter: Reduce newBadFaces

--- src/dynamicMesh/polyMeshFilter/polyMeshFilter.C
+++ src/dynamicMesh/polyMeshFilter/polyMeshFilter.C
@@ -926,6 +926,8 @@ Foam::label Foam::polyMeshFilter::filter(const label nOriginalBadFaces)
                     }
                 }
             }
+
+            reduce(newBadFaces, orOp<bool>());
         }
         else
         {
