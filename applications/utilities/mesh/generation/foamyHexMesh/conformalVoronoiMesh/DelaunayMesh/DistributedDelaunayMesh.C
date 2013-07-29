56421096d13b823b89dad121930d13e52248a50b
laurence <laurence>
2013-07-29 15:38:30 +0100

    ENH: foamyHexMesh: Shift calculateLoadBalance to DistributedDelaunayMesh

--- applications/utilities/mesh/generation/foamyHexMesh/conformalVoronoiMesh/DelaunayMesh/DistributedDelaunayMesh.C
+++ applications/utilities/mesh/generation/foamyHexMesh/conformalVoronoiMesh/DelaunayMesh/DistributedDelaunayMesh.C
@@ -748,6 +748,44 @@ void Foam::DistributedDelaunayMesh<Triangulation>::sync
 // * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
 
 template<class Triangulation>
+Foam::scalar
+Foam::DistributedDelaunayMesh<Triangulation>::calculateLoadUnbalance() const
+{
+    label nRealVertices = 0;
+
+    for
+    (
+        Finite_vertices_iterator vit = Triangulation::finite_vertices_begin();
+        vit != Triangulation::finite_vertices_end();
+        ++vit
+    )
+    {
+        // Only store real vertices that are not feature vertices
+        if (vit->real() && !vit->featurePoint())
+        {
+            nRealVertices++;
+        }
+    }
+
+    scalar globalNRealVertices = returnReduce
+    (
+        nRealVertices,
+        sumOp<label>()
+    );
+
+    scalar unbalance = returnReduce
+    (
+        mag(1.0 - nRealVertices/(globalNRealVertices/Pstream::nProcs())),
+        maxOp<scalar>()
+    );
+
+    Info<< "    Processor unbalance " << unbalance << endl;
+
+    return unbalance;
+}
+
+
+template<class Triangulation>
 bool Foam::DistributedDelaunayMesh<Triangulation>::distribute
 (
     const boundBox& bb
