60bd776b1a24a1df152279010f5870b757489c77
mattijs <mattijs>
2013-09-23 17:29:37 +0100

    ENH: blockMesh: additional output

--- src/mesh/blockMesh/blockMesh/blockMeshCreate.C
+++ src/mesh/blockMesh/blockMesh/blockMeshCreate.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -47,6 +47,40 @@ void Foam::blockMesh::createPoints() const
     forAll(blocks, blockI)
     {
         const pointField& blockPoints = blocks[blockI].points();
+
+        if (verboseOutput)
+        {
+            const Vector<label>& density = blocks[blockI].meshDensity();
+
+            label v0 = blocks[blockI].vtxLabel(0, 0, 0);
+            label vi1 = blocks[blockI].vtxLabel(1, 0, 0);
+            scalar diStart = mag(blockPoints[vi1]-blockPoints[v0]);
+
+            label vinM1 = blocks[blockI].vtxLabel(density.x()-1, 0, 0);
+            label vin = blocks[blockI].vtxLabel(density.x(), 0, 0);
+            scalar diFinal = mag(blockPoints[vin]-blockPoints[vinM1]);
+
+            label vj1 = blocks[blockI].vtxLabel(0, 1, 0);
+            scalar djStart = mag(blockPoints[vj1]-blockPoints[v0]);
+            label vjnM1 = blocks[blockI].vtxLabel(0, density.y()-1, 0);
+            label vjn = blocks[blockI].vtxLabel(0, density.y(), 0);
+            scalar djFinal = mag(blockPoints[vjn]-blockPoints[vjnM1]);
+
+            label vk1 = blocks[blockI].vtxLabel(0, 0, 1);
+            scalar dkStart = mag(blockPoints[vk1]-blockPoints[v0]);
+            label vknM1 = blocks[blockI].vtxLabel(0, 0, density.z()-1);
+            label vkn = blocks[blockI].vtxLabel(0, 0, density.z());
+            scalar dkFinal = mag(blockPoints[vkn]-blockPoints[vknM1]);
+
+            Info<< "    Block " << blockI << " cell size :" << nl
+                << "        i : " << scaleFactor_*diStart << " .. "
+                << scaleFactor_*diFinal << nl
+                << "        j : " << scaleFactor_*djStart << " .. "
+                << scaleFactor_*djFinal << nl
+                << "        k : " << scaleFactor_*dkStart << " .. "
+                << scaleFactor_*dkFinal << nl
+                << endl;
+        }
 
         forAll(blockPoints, blockPointI)
         {
