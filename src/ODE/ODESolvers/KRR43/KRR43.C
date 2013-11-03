734d88a875f184aa005feda551631978b4fabee6
Henry <Henry>
2013-11-03 23:21:32 +0000

    ODE solvers: Added Rosenbrock43 (equivalent to KRR43 but more consistent)

--- src/ODE/ODESolvers/KRR43/KRR43.C
+++ src/ODE/ODESolvers/KRR43/KRR43.C
@@ -23,41 +23,54 @@ License
 
 \*---------------------------------------------------------------------------*/
 
-#include "KRR4.H"
+#include "KRR43.H"
 #include "addToRunTimeSelectionTable.H"
 
 // * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
 
 namespace Foam
 {
-    defineTypeNameAndDebug(KRR4, 0);
-    addToRunTimeSelectionTable(ODESolver, KRR4, dictionary);
+    defineTypeNameAndDebug(KRR43, 0);
+    addToRunTimeSelectionTable(ODESolver, KRR43, dictionary);
 
 const scalar
-    KRR4::gamma = 1.0/2.0,
-    KRR4::a21 = 2.0, KRR4::a31 = 48.0/25.0, KRR4::a32 = 6.0/25.0,
-    KRR4::c21 = -8.0, KRR4::c31 = 372.0/25.0, KRR4::c32 = 12.0/5.0,
-    KRR4::c41 = -112.0/125.0, KRR4::c42 = -54.0/125.0, KRR4::c43 = -2.0/5.0,
-    KRR4::b1 = 19.0/9.0, KRR4::b2 = 1.0/2.0, KRR4::b3 = 25.0/108.0,
-    KRR4::b4 = 125.0/108.0,
-    KRR4::e1 = 17.0/54.0, KRR4::e2 = 7.0/36.0, KRR4::e3 = 0.0,
-    KRR4::e4 = 125.0/108.0,
-    KRR4::c1X = 1.0/2.0, KRR4::c2X = -3.0/2.0, KRR4::c3X = 121.0/50.0,
-    KRR4::c4X = 29.0/250.0,
-    KRR4::a2X = 1.0, KRR4::a3X = 3.0/5.0;
+    KRR43::gamma = 1.0/2.0,
+    KRR43::a21 = 2.0,
+    KRR43::a31 = 48.0/25.0,
+    KRR43::a32 = 6.0/25.0,
+    KRR43::c21 = -8.0,
+    KRR43::c31 = 372.0/25.0,
+    KRR43::c32 = 12.0/5.0,
+    KRR43::c41 = -112.0/125.0,
+    KRR43::c42 = -54.0/125.0,
+    KRR43::c43 = -2.0/5.0,
+    KRR43::b1 = 19.0/9.0,
+    KRR43::b2 = 1.0/2.0,
+    KRR43::b3 = 25.0/108.0,
+    KRR43::b4 = 125.0/108.0,
+    KRR43::e1 = 17.0/54.0,
+    KRR43::e2 = 7.0/36.0,
+    KRR43::e3 = 0.0,
+    KRR43::e4 = 125.0/108.0,
+    KRR43::c1X = 1.0/2.0,
+    KRR43::c2X = -3.0/2.0,
+    KRR43::c3X = 121.0/50.0,
+    KRR43::c4X = 29.0/250.0,
+    KRR43::a2X = 1.0,
+    KRR43::a3X = 3.0/5.0;
 }
 
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
-Foam::KRR4::KRR4(const ODESystem& ode, const dictionary& dict)
+Foam::KRR43::KRR43(const ODESystem& ode, const dictionary& dict)
 :
     ODESolver(ode, dict),
     adaptiveSolver(ode, dict),
-    g1_(n_),
-    g2_(n_),
-    g3_(n_),
-    g4_(n_),
+    k1_(n_),
+    k2_(n_),
+    k3_(n_),
+    k4_(n_),
     err_(n_),
     dydx_(n_),
     dfdx_(n_),
@@ -69,7 +82,7 @@ Foam::KRR4::KRR4(const ODESystem& ode, const dictionary& dict)
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
-Foam::scalar Foam::KRR4::solve
+Foam::scalar Foam::KRR43::solve
 (
     const ODESystem& ode,
     const scalar x0,
@@ -93,60 +106,60 @@ Foam::scalar Foam::KRR4::solve
 
     LUDecompose(a_, pivotIndices_);
 
-    forAll(g1_, i)
+    forAll(k1_, i)
     {
-        g1_[i] = dydx0[i] + dx*c1X*dfdx_[i];
+        k1_[i] = dydx0[i] + dx*c1X*dfdx_[i];
     }
 
-    LUBacksubstitute(a_, pivotIndices_, g1_);
+    LUBacksubstitute(a_, pivotIndices_, k1_);
 
     forAll(y, i)
     {
-        y[i] = y0[i] + a21*g1_[i];
+        y[i] = y0[i] + a21*k1_[i];
     }
 
     ode.derivatives(x0 + a2X*dx, y, dydx_);
 
-    forAll(g2_, i)
+    forAll(k2_, i)
     {
-        g2_[i] = dydx_[i] + dx*c2X*dfdx_[i] + c21*g1_[i]/dx;
+        k2_[i] = dydx_[i] + dx*c2X*dfdx_[i] + c21*k1_[i]/dx;
     }
 
-    LUBacksubstitute(a_, pivotIndices_, g2_);
+    LUBacksubstitute(a_, pivotIndices_, k2_);
 
     forAll(y, i)
     {
-        y[i] = y0[i] + a31*g1_[i] + a32*g2_[i];
+        y[i] = y0[i] + a31*k1_[i] + a32*k2_[i];
     }
 
     ode.derivatives(x0 + a3X*dx, y, dydx_);
 
-    forAll(g3_, i)
+    forAll(k3_, i)
     {
-        g3_[i] = dydx0[i] + dx*c3X*dfdx_[i] + (c31*g1_[i] + c32*g2_[i])/dx;
+        k3_[i] = dydx_[i] + dx*c3X*dfdx_[i] + (c31*k1_[i] + c32*k2_[i])/dx;
     }
 
-    LUBacksubstitute(a_, pivotIndices_, g3_);
+    LUBacksubstitute(a_, pivotIndices_, k3_);
 
-    forAll(g4_, i)
+    forAll(k4_, i)
     {
-        g4_[i] = dydx_[i] + dx*c4X*dfdx_[i]
-           + (c41*g1_[i] + c42*g2_[i] + c43*g3_[i])/dx;
+        k4_[i] = dydx_[i] + dx*c4X*dfdx_[i]
+           + (c41*k1_[i] + c42*k2_[i] + c43*k3_[i])/dx;
     }
 
-    LUBacksubstitute(a_, pivotIndices_, g4_);
+    LUBacksubstitute(a_, pivotIndices_, k4_);
 
     forAll(y, i)
     {
-        y[i] = y0[i] + b1*g1_[i] + b2*g2_[i] + b3*g3_[i] + b4*g4_[i];
-        err_[i] = e1*g1_[i] + e2*g2_[i] + e3*g3_[i] + e4*g4_[i];
+        y[i] = y0[i] + b1*k1_[i] + b2*k2_[i] + b3*k3_[i] + b4*k4_[i];
+        err_[i] = e1*k1_[i] + e2*k2_[i] + e3*k3_[i] + e4*k4_[i];
     }
 
     return normalizeError(y0, y, err_);
 }
 
 
-void Foam::KRR4::solve
+void Foam::KRR43::solve
 (
     const ODESystem& odes,
     scalar& x,
