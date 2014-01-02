c71610aa761251e30172037c3a2edd13b15f468d
andy <andy>
2014-01-02 18:20:42 +0000

    ENH: Code formatting improvements/corrections

--- src/lagrangian/intermediate/submodels/MPPIC/CorrectionLimitingMethods/CorrectionLimitingMethod/CorrectionLimitingMethod.C
+++ src/lagrangian/intermediate/submodels/MPPIC/CorrectionLimitingMethods/CorrectionLimitingMethod/CorrectionLimitingMethod.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2013 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2013-2014 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -69,7 +69,6 @@ Foam::CorrectionLimitingMethod::New
         (
             "CorrectionLimitingMethod::New"
             "("
-                "const word&, "
                 "const dictionary&"
             ")"
         )   << "Unknown correction limiter type " << modelType
