b3f7637bfec72ad1a9b4f9e310fe2fe8f40e6104
Andrew Heather <a.heather@opencfd.co.uk>
2015-10-06 12:06:11 +0100

    STYLE: Updated copyright notices

--- src/OpenFOAM/db/functionObjects/functionObjectList/functionObjectList.C
+++ src/OpenFOAM/db/functionObjects/functionObjectList/functionObjectList.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2011-2014 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2011-2014 OpenCFD Ltd
+     \\/     M anipulation  | Copyright (C) 2015 OpenCFD Ltd
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
