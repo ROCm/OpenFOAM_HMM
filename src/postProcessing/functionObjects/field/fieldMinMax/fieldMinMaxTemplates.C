751bc41650a14b7852b925b0471070b6fd8da805
Andrew Heather <a.heather@opencfd.co.uk>
2015-10-06 12:07:18 +0100

    ENH: fieldMinMax FO updated follwing changes to functionObjectFile

--- src/postProcessing/functionObjects/field/fieldMinMax/fieldMinMaxTemplates.C
+++ src/postProcessing/functionObjects/field/fieldMinMax/fieldMinMaxTemplates.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
-     \\/     M anipulation  |
+     \\/     M anipulation  | Copyright (C) 2015 OpenCFD Ltd
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -43,7 +43,7 @@ void Foam::fieldMinMax::output
 {
     OFstream& file = this->file();
 
-    if (location_)
+    if (writeLocation_)
     {
         file<< obr_.time().value();
 
