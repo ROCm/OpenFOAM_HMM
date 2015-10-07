bc00be2c9af95ff000e191fa888bd5a44e26a4a2
Andrew Heather <a.heather@opencfd.co.uk>
2015-10-07 13:02:44 +0100

    ENH: forces FO updated following update to use functionObjectState
    Properties stored in state dictionary:
    - normalForce
    - tangentialForce
    - porousForce
    - normalMoment
    - tangentialMoment
    - porousMoment

--- src/postProcessing/functionObjects/forces/forces/forces.C
+++ src/postProcessing/functionObjects/forces/forces/forces.C
@@ -743,10 +743,9 @@ Foam::forces::forces
     const bool readFields
 )
 :
+    functionObjectState(obr, name),
     functionObjectFile(obr, name),
-    name_(name),
     obr_(obr),
-    active_(true),
     log_(true),
     force_(3),
     moment_(3),
@@ -779,28 +778,13 @@ Foam::forces::forces
     initialised_(false)
 {
     // Check if the available mesh is an fvMesh otherise deactivate
-    if (isA<fvMesh>(obr_))
+    if (setActive<fvMesh>())
     {
         if (readFields)
         {
             read(dict);
-            Info<< endl;
+            if (log_) Info << endl;
         }
-    }
-    else
-    {
-        active_ = false;
-        WarningIn
-        (
-            "Foam::forces::forces"
-            "("
-                "const word&, "
-                "const objectRegistry&, "
-                "const dictionary&, "
-                "const bool"
-            ")"
-        )   << "No fvMesh available, deactivating " << name_
-            << endl;
     }
 }
 
@@ -818,9 +802,9 @@ Foam::forces::forces
     const coordinateSystem& coordSys
 )
 :
+    functionObjectState(obr, name),
     functionObjectFile(obr, name),
     obr_(obr),
-    active_(true),
     log_(true),
     force_(3),
     moment_(3),
@@ -1040,6 +1024,14 @@ void Foam::forces::execute()
         if (log_) Info << endl;
     }
 
+    // write state/results information
+    setResult("normalForce", sum(force_[0]));
+    setResult("tangentialForce", sum(force_[1]));
+    setResult("porousForce", sum(force_[2]));
+
+    setResult("normalMoment", sum(moment_[0]));
+    setResult("tangentialMoment", sum(moment_[1]));
+    setResult("porousMoment", sum(moment_[2]));
 }
 
 
