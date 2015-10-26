2de68a9ae7010a550252c2079c09f41e01dfaa79
mattijs <mattijs>
2015-10-26 14:37:17 +0000

    BUG: snappyHexMesh: minThickness > 1 caused truncation of layers
    The start of the layer addition loop does a synchronisation of the wanted
    displacement. This also does a truncation of the displacement if it is <
    minThickness. At the first iteration the displacement was initialised to
    vector::one which might trigger the truncation logic (and then disable
    extrusion altogether). Instead we now initialise the displacement to
    vector::GREAT before entering the synchronisation.

--- src/mesh/autoMesh/autoHexMesh/autoHexMeshDriver/autoLayerDriver.C
+++ src/mesh/autoMesh/autoHexMesh/autoHexMeshDriver/autoLayerDriver.C
@@ -3068,8 +3068,10 @@ void Foam::autoLayerDriver::addLayers
                 )
             );
 
-            // Displacement for all pp.localPoints.
-            vectorField patchDisp(pp().nPoints(), vector::one);
+            // Displacement for all pp.localPoints. Set to large value to
+            // avoid truncation in syncPatchDisplacement because of
+            // minThickness.
+            vectorField patchDisp(pp().nPoints(), vector(GREAT, GREAT, GREAT));
             labelList patchNLayers(pp().nPoints(), 0);
             label nIdealTotAddedCells = 0;
             List<extrudeMode> extrudeStatus(pp().nPoints(), EXTRUDE);
@@ -3086,6 +3088,8 @@ void Foam::autoLayerDriver::addLayers
                 nIdealTotAddedCells
             );
             // Make sure displacement is equal on both sides of coupled patches.
+            // Note that we explicitly disable the minThickness truncation
+            // of the patchDisp here.
             syncPatchDisplacement
             (
                 pp,
@@ -3297,8 +3301,9 @@ void Foam::autoLayerDriver::addLayers
     // Point-wise extrusion data
     // ~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    // Displacement for all pp.localPoints.
-    vectorField patchDisp(pp().nPoints(), vector::one);
+    // Displacement for all pp.localPoints. Set to large value so does
+    // not trigger the minThickness truncation (see syncPatchDisplacement below)
+    vectorField patchDisp(pp().nPoints(), vector(GREAT, GREAT, GREAT));
 
     // Number of layers for all pp.localPoints. Note: only valid if
     // extrudeStatus = EXTRUDE.
@@ -3533,6 +3538,9 @@ void Foam::autoLayerDriver::addLayers
 
 
             // Make sure displacement is equal on both sides of coupled patches.
+            // Note that this also does the patchDisp < minThickness truncation
+            // so for the first pass make sure the patchDisp is larger than
+            // that.
             syncPatchDisplacement
             (
                 pp,
