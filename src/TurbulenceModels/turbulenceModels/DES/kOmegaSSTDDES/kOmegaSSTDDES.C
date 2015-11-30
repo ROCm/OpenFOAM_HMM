43f91a1415d1d698d9649a6d6df733fd4e36477b
Andrew Heather <a.heather@opencfd.co.uk>
2015-11-30 16:18:17 +0000

    CONTRIBUTION: Turbulence - new kOmegaSST DES, DDES and IDDES model variants
    - Initial code supplied by CFD Software E+F GmbH
    - Refactored and integrated into the new templated Turbulence structure
      by OpenCFD
    
    References:
    
    - kOmegaSSTDES model:
      Strelets, M. (2001)
      Detached Eddy Simulation of Massively Separated Flows,
      39th AIAA Aerospace Sciences Meeting and Exhibit, Reno, NV
    
    - kOmegaSSTDDES model:
      Gritskevich, M.S., Garbaruk, A.V., Schuetze, J., Menter, F.R. (2011)
      Development of DDES and IDDES  Formulations for the k-omega
      Shear Stress Transport Model, Flow, Turbulence and Combustion,
      pp. 1-19
    
    - kOmegaSSTIDDES model:
      Gritskevich, M.S., Garbaruk, A.V., Schuetze, J., Menter, F.R. (2011)
      Development of DDES and IDDES Formulations for the k-omega
      Shear Stress Transport Model, Flow, Turbulence and Combustion,
      pp. 1-19

---
+++ src/TurbulenceModels/turbulenceModels/DES/kOmegaSSTDDES/kOmegaSSTDDES.C
@@ -0,0 +1,173 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "kOmegaSSTDDES.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace LESModels
+{
+
+// * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+tmp<volScalarField> kOmegaSSTDDES<BasicTurbulenceModel>::rd
+(
+    const volScalarField& nur,
+    const volScalarField& magGradU
+) const
+{
+    tmp<volScalarField> tr
+    (
+        min
+        (
+            nur
+           /(
+                max
+                (
+                    magGradU,
+                    dimensionedScalar("SMALL", magGradU.dimensions(), SMALL)
+                )
+                *sqr(this->kappa_*this->y_)
+            ),
+            scalar(10)
+        )
+    );
+    tr().boundaryField() == 0.0;
+
+    return tr;
+}
+
+
+template<class BasicTurbulenceModel>
+tmp<volScalarField> kOmegaSSTDDES<BasicTurbulenceModel>::fd
+(
+    const volScalarField& magGradU
+) const
+{
+    return 1 - tanh(pow(cd1_*rd(this->nuEff(), magGradU), cd2_));
+}
+
+
+// * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+tmp<volScalarField> kOmegaSSTDDES<BasicTurbulenceModel>::dTilda
+(
+    const volScalarField& magGradU,
+    const volScalarField& CDES
+) const
+{
+    const volScalarField& k = this->k_;
+    const volScalarField& omega = this->omega_;
+
+    const volScalarField lRAS(sqrt(k)/(this->betaStar_*omega));
+    const volScalarField lLES(CDES*this->delta());
+    const dimensionedScalar d0("SMALL", dimLength, SMALL);
+
+    return max(lRAS - fd(magGradU)*max(lRAS - lLES, d0), d0);
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+kOmegaSSTDDES<BasicTurbulenceModel>::kOmegaSSTDDES
+(
+    const alphaField& alpha,
+    const rhoField& rho,
+    const volVectorField& U,
+    const surfaceScalarField& alphaRhoPhi,
+    const surfaceScalarField& phi,
+    const transportModel& transport,
+    const word& propertiesName,
+    const word& type
+)
+:
+    kOmegaSSTDES<BasicTurbulenceModel>
+    (
+        alpha,
+        rho,
+        U,
+        alphaRhoPhi,
+        phi,
+        transport,
+        propertiesName,
+        type
+    ),
+
+    cd1_
+    (
+        dimensioned<scalar>::lookupOrAddToDict
+        (
+            "cd1",
+            this->coeffDict_,
+            20
+        )
+    ),
+    cd2_
+    (
+        dimensioned<scalar>::lookupOrAddToDict
+        (
+            "cd2",
+            this->coeffDict_,
+            3
+        )
+    )
+{
+    if (type == typeName)
+    {
+        this->printCoeffs(type);
+    }
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class BasicTurbulenceModel>
+bool kOmegaSSTDDES<BasicTurbulenceModel>::read()
+{
+    if (kOmegaSSTDES<BasicTurbulenceModel>::read())
+    {
+        cd1_.readIfPresent(this->coeffDict());
+        cd2_.readIfPresent(this->coeffDict());
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace LESModels
+} // End namespace Foam
+
+// ************************************************************************* //
