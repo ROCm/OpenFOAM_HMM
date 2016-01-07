3568e3a9a6260019027859e8960db37ce07be3e6
mattijs <mattijs>
2016-01-07 08:48:27 +0000

    ENH: objectRegistry: revert to printing on master only

--- src/OpenFOAM/db/regIOobject/regIOobjectRead.C
+++ src/OpenFOAM/db/regIOobject/regIOobjectRead.C
@@ -288,7 +288,7 @@ bool Foam::regIOobject::readIfModified()
         if (modified())
         {
             const fileName& fName = time().getFile(watchIndex_);
-            Pout<< "regIOobject::readIfModified() : " << nl
+            Info<< "regIOobject::readIfModified() : " << nl
                 << "    Re-reading object " << name()
                 << " from file " << fName << endl;
             return read();
