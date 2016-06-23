4d95d6e8d9432559a42d9bb3fab27539b096a512
Andrew Heather <a.heather@opencfd.co.uk>
2016-06-23 14:53:50 +0100

    BUG: Continuation of #155 - further fix for accessing data object

--- src/postProcessing/functionObjects/graphics/runTimePostProcessing/functionObjectLine.C
+++ src/postProcessing/functionObjects/graphics/runTimePostProcessing/functionObjectLine.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2015 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2015 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2015-2016 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -110,7 +110,7 @@ void Foam::functionObjectLine::addGeometryToScene
 
         vtkSmartPointer<vtkPolyDataMapper> mapper =
             vtkSmartPointer<vtkPolyDataMapper>::New();
-        setField(position, fieldName_, mapper, renderer);
+        setField(position, fieldName_, mapper, renderer, lines->GetOutput());
 
         actor_->SetMapper(mapper);
 
