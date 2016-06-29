ROCm/OpenFOAM_HMM

commit ef9a14b288d7d0c516ebc7a62f6823fa41c30d7f

ENH: ensightFile: make interface consistent with Ostream::writeKeyword
Test with #122

src/conversion/ensight/file/ensightFile.C
@@ -278,7 +278,7 @@ Foam::Ostream& Foam::ensightFile::writeUndef()
 }
 
 
-Foam::Ostream& Foam::ensightFile::writeKeyword(const string& key)
+Foam::Ostream& Foam::ensightFile::writeKeyword(const keyType& key)
 {
     if (allowUndef_)
     {

src/conversion/ensight/file/ensightGeoFile.C
@@ -72,7 +72,7 @@ Foam::ensightGeoFile::~ensightGeoFile()
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
-Foam::Ostream& Foam::ensightGeoFile::writeKeyword(const string& key)
+Foam::Ostream& Foam::ensightGeoFile::writeKeyword(const keyType& key)
 {
     write(key); newline();
 

src/conversion/ensight/file/ensightFile.H
@@ -147,7 +147,7 @@ public:
         virtual Ostream& write(const char* buf, std::streamsize count);
 
         //- Write element keyword with trailing newline, optionally with undef
-        virtual Ostream& writeKeyword(const string& key);
+        virtual Ostream& writeKeyword(const keyType&);
 
         //- Write "C Binary" for binary files (eg, geometry/measured)
         Ostream& writeBinaryHeader();

src/conversion/ensight/file/ensightGeoFile.H
@@ -88,7 +88,7 @@ public:
     // Output
 
         //- Write keyword with trailing newline
-        virtual Ostream& writeKeyword(const string& key);
+        virtual Ostream& writeKeyword(const keyType& key);
 };
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //

commit c133f98dda9699ac2e46920b3e9c52ad5f010ffd

BUG: corrupt keywords in ensight binary
- regression introduced by ef9a14b288d7

src/conversion/ensight/file/ensightFile.C
@@ -282,14 +282,15 @@ Foam::Ostream& Foam::ensightFile::writeKeyword(const keyType& key)
 {
     if (allowUndef_)
     {
-        write(string(key + " undef"));
+        write(string(static_cast<const string&>(key) + " undef"));
         newline();
         write(undefValue_);
         newline();
     }
     else
     {
-        write(key);
+        // ensure we get ensightFile::write(const string&)
+        write(static_cast<const string&>(key));
         newline();
     }
     return *this;

src/conversion/ensight/file/ensightGeoFile.C
@@ -74,7 +74,8 @@ Foam::ensightGeoFile::~ensightGeoFile()
 
 Foam::Ostream& Foam::ensightGeoFile::writeKeyword(const keyType& key)
 {
-    write(key); newline();
+    // ensure we get ensightFile::write(const string&)
+    write(static_cast<const string&>(key)); newline();
 
     return *this;
 }