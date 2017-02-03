ROCm/OpenFOAM_HMM

commit a7195288321096a736fa2a7e770377957d70eb70

ENH: use STL reader from surfMesh library for triSurface as well (issue #294)
- initial step in reducing duplicate IO for triSurface.

- write STL triangle using common core routines from fileFormats

src/triSurface/triSurface/interfaces/STL/readSTL.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2017 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -23,50 +23,68 @@ License
 
 \*---------------------------------------------------------------------------*/
 
+#include "STLReader.H"
+#include "mergePoints.H"
 #include "triSurface.H"
-#include "IFstream.H"
-
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
-namespace Foam
-{
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
-bool triSurface::readSTL(const fileName& STLfileName)
+bool Foam::triSurface::readSTL(const fileName& STLfileName, bool forceBinary)
 {
-    token firstToken;
-    {
-        IFstream str(STLfileName);
+    // Read in the values
+    fileFormats::STLReader reader
+    (
+        STLfileName,
+        (
+            forceBinary
+          ? fileFormats::STLCore::BINARY
+          : fileFormats::STLCore::UNKNOWN
+        )
+    );
 
-        if (!str.good())
-        {
-            return false;
-        }
-        firstToken = token(str);
+    // Stitch points
+    labelList pointMap;
+    label nUniquePoints = mergePoints
+    (
+        reader.points(),
+        (
+            // With the merge distance depending on the input format
+            (reader.stlFormat() == fileFormats::STLCore::BINARY ? 10 : 100)
+          * SMALL
+        ),
+        false,                  // verbose
+        pointMap                // old to new point map
+    );
+
+    const pointField& readpts = reader.points();
+    const labelList&  zoneIds = reader.zoneIds();
+
+    pointField& pointLst = storedPoints();
+    List<Face>& faceLst  = storedFaces();
+
+    // Sizing
+    pointLst.setSize(nUniquePoints);
+    faceLst.setSize(zoneIds.size());
+
+    // Assign points
+    forAll(readpts, pointi)
+    {
+        pointLst[pointMap[pointi]] = readpts[pointi];
     }
 
-    if (firstToken.isWord())
+    // Assign triangles
+    label pointi = 0;
+    forAll(faceLst, i)
     {
-        word firstWord(firstToken.wordToken());
+        Face& f = faceLst[i];
 
-        for (size_t i = 0; i < firstWord.size(); i++)
-        {
-            firstWord[i] = std::toupper(firstWord[i]);
-        }
-
-        if (firstWord == "SOLID" || firstWord(5) == "SOLID")
-        {
-            return readSTLASCII(STLfileName);
-        }
+        f[0] = pointMap[pointi++];
+        f[1] = pointMap[pointi++];
+        f[2] = pointMap[pointi++];
+        f.region() = zoneIds[i];
     }
 
-    return readSTLBINARY(STLfileName);
+    return true;
 }
-
-
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
-} // End namespace Foam
 
 // ************************************************************************* //

src/fileFormats/stl/STLCore.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2016 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2016-2017 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -52,7 +52,7 @@ bool Foam::fileFormats::STLCore::isBinaryName
     const STLFormat& format
 )
 {
-    return (format == DETECT ? (filename.ext() == "stlb") : format == BINARY);
+    return (format == UNKNOWN ? (filename.ext() == "stlb") : format == BINARY);
 }
 
 

src/fileFormats/stl/STLCore.H
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2016 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2016-2017 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -61,7 +61,7 @@ public:
         {
             ASCII,  //!< ASCII
             BINARY, //!< BINARY
-            DETECT  //!< Detect based on (input) content or (output) extension
+            UNKNOWN //!< Detect based on (input) content or (output) extension
         };
 
 

src/fileFormats/stl/STLReader.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2016 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2016-2017 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -37,6 +37,7 @@ bool Foam::fileFormats::STLReader::readBINARY
 )
 {
     sorted_ = true;
+    format_ = UNKNOWN;
 
     label nTris = 0;
     autoPtr<istream> streamPtr = readBinaryHeader(filename, nTris);
@@ -123,6 +124,7 @@ bool Foam::fileFormats::STLReader::readBINARY
     names_.clear();
     sizes_.transfer(dynSizes);
 
+    format_ = BINARY;
     return true;
 }
 
@@ -133,7 +135,7 @@ bool Foam::fileFormats::STLReader::readFile
     const STLFormat& format
 )
 {
-    if (format == DETECT ? detectBinaryHeader(filename) : format == BINARY)
+    if (format == UNKNOWN ? detectBinaryHeader(filename) : format == BINARY)
     {
         return readBINARY(filename);
     }
@@ -155,10 +157,11 @@ Foam::fileFormats::STLReader::STLReader
     points_(),
     zoneIds_(),
     names_(),
-    sizes_()
+    sizes_(),
+    format_(STLCore::UNKNOWN)
 {
     // Auto-detect ASCII/BINARY format
-    readFile(filename, STLCore::DETECT);
+    readFile(filename, STLCore::UNKNOWN);
 }
 
 
@@ -172,7 +175,8 @@ Foam::fileFormats::STLReader::STLReader
     points_(),
     zoneIds_(),
     names_(),
-    sizes_()
+    sizes_(),
+    format_(STLCore::UNKNOWN)
 {
     // Manually specified ASCII/BINARY format
     readFile(filename, format);
@@ -183,6 +187,19 @@ Foam::fileFormats::STLReader::STLReader
 
 Foam::fileFormats::STLReader::~STLReader()
 {}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+void Foam::fileFormats::STLReader::clear()
+{
+    sorted_ = true;
+    points_.clear();
+    zoneIds_.clear();
+    names_.clear();
+    sizes_.clear();
+    format_ = UNKNOWN;
+}
 
 
 // ************************************************************************* //

src/fileFormats/stl/STLReader.H
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2016 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2016-2017 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -25,7 +25,7 @@ Class
     Foam::fileFormats::STLReader
 
 Description
-    Internal class used by the STLsurfaceFormat
+    Internal class used by the STLsurfaceFormat and triSurface.
 
 SourceFiles
     STLReader.C
@@ -72,17 +72,20 @@ class STLReader
         //- The solid count, in the order of their first appearance
         List<label> sizes_;
 
+        //- The STL format used
+        STLFormat format_;
+
 
     // Private Member Functions
 
         //- Read ASCII
-        bool readASCII(const fileName&);
+        bool readASCII(const fileName& filename);
 
         //- Read BINARY
-        bool readBINARY(const fileName&);
+        bool readBINARY(const fileName& filename);
 
         //- Read ASCII or BINARY
-        bool readFile(const fileName&, const STLFormat&);
+        bool readFile(const fileName& filename, const STLFormat& format);
 
 
         //- Disallow default bitwise copy construct
@@ -96,12 +99,13 @@ public:
 
     // Constructors
 
-        //- Read from file, filling in the information
-        STLReader(const fileName&);
+        //- Read from file, filling in the information.
+        //  Auto-detect ASCII/BINARY format.
+        STLReader(const fileName& filename);
 
         //- Read from file, filling in the information.
-        //  Manually selected choice of ascii/binary/detect.
-        STLReader(const fileName&, const STLFormat&);
+        //  Manually selected choice of ASCII/BINARY/UNKNOWN(detect) formats.
+        STLReader(const fileName& filename, const STLFormat& format);
 
 
     //- Destructor
@@ -110,44 +114,43 @@ public:
 
     // Member Functions
 
-        //- File read was already sorted
-        bool sorted() const
+        //- Flush all values
+        void clear();
+
+        //- File read was already sorted?
+        inline bool sorted() const
         {
             return sorted_;
         }
 
-        //- Flush all values
-        void clear()
-        {
-            sorted_ = true;
-            points_.clear();
-            zoneIds_.clear();
-            names_.clear();
-            sizes_.clear();
-        }
-
         //- Return full access to the points
-        pointField& points()
+        inline pointField& points()
         {
             return points_;
         }
 
         //- Return full access to the zoneIds
-        List<label>& zoneIds()
+        inline List<label>& zoneIds()
         {
             return zoneIds_;
         }
 
         //- The list of solid names in the order of their first appearance
-        List<word>& names()
+        inline List<word>& names()
         {
             return names_;
         }
 
         //- The list of solid sizes in the order of their first appearance
-        List<label>& sizes()
+        inline List<label>& sizes()
         {
             return sizes_;
+        }
+
+        //- The STL format used (ASCII or BINARY)
+        inline enum STLFormat stlFormat() const
+        {
+            return format_;
         }
 };
 

src/fileFormats/stl/STLReaderASCII.L
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2016 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2016-2017 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -97,13 +97,13 @@ public:
 
     // Access
 
-        //- Do all the solid groups appear in order
+        //- Do all the solid groups appear in order?
         inline bool sorted() const
         {
             return sorted_;
         }
 
-        //- A list of points corresponding to a pointField
+        //- A list of unstitched triangle points
         inline DynamicList<point>& points()
         {
             return points_;
@@ -116,13 +116,13 @@ public:
             return facets_;
         }
 
-        //- Names
+        //- Solid names in the order of their appearance.
         inline DynamicList<word>& names()
         {
             return names_;
         }
 
-        //- Sizes
+        //- Solid sizes in the order of their appearance.
         inline DynamicList<label>& sizes()
         {
             return sizes_;
@@ -396,6 +396,8 @@ bool Foam::fileFormats::STLReader::readASCII
     const fileName& filename
 )
 {
+    format_ = UNKNOWN;
+
     IFstream is(filename);
     if (!is)
     {
@@ -417,6 +419,7 @@ bool Foam::fileFormats::STLReader::readASCII
     names_.transfer(lexer.names());
     sizes_.transfer(lexer.sizes());
 
+    format_ = ASCII;
     return true;
 }
 

src/fileFormats/stl/STLpoint.H
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2016 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2016-2017 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -25,7 +25,7 @@ Class
     Foam::STLpoint
 
 Description
-    A vertex point representation for STL files.
+    A vertex point or facet normal representation for STL files.
 
 \*---------------------------------------------------------------------------*/
 
@@ -62,6 +62,12 @@ public:
         inline STLpoint(float x, float y, float z)
         :
             Vector<float>(x, y, z)
+        {}
+
+        //- Construct from components
+        inline STLpoint(double x, double y, double z)
+        :
+            Vector<float>(float(x), float(y), float(z))
         {}
 
         //- Construct from point

src/fileFormats/stl/STLtriangle.H
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2016 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2016-2017 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -46,7 +46,7 @@ namespace Foam
 
 // Forward declaration of friend functions and operators
 class STLtriangle;
-Ostream& operator<<(Ostream&, const STLtriangle&);
+Ostream& operator<<(Ostream& os, const STLtriangle& tri);
 
 /*---------------------------------------------------------------------------*\
                          Class STLtriangle Declaration
@@ -54,18 +54,20 @@ Ostream& operator<<(Ostream&, const STLtriangle&);
 
 class STLtriangle
 {
+    // Typedefs
+
+    //- Attribute is 16-bit
+    typedef unsigned short STLattrib;
+
     // Private data
+    // NB: The order of the members (1 normal, 3 points, 1 attribute) is
+    // essential when reading/writing binary content.
 
-        //- Attribute is 16-bit
-        typedef unsigned short STLattrib;
+        //- The face normal and the three points defining the triangle.
+        //  Some programs may write zero or other junk for the normal.
+        STLpoint normal_, a_, b_, c_;
 
-        //- The face normal, many programs write zero or other junk
-        STLpoint normal_;
-
-        //- The three points defining the triangle
-        STLpoint a_, b_, c_;
-
-        //- The attribute information could for colour or solid id, etc
+        //- The attribute information could be for 'color' or solid id, etc
         STLattrib attrib_;
 
 
@@ -87,7 +89,7 @@ public:
         );
 
         //- Construct from istream (read binary)
-        inline STLtriangle(istream&);
+        inline STLtriangle(std::istream& is);
 
 
     // Member Functions
@@ -104,13 +106,13 @@ public:
         // Read
 
             //- Read from istream (binary)
-            inline void read(istream&);
+            inline void read(std::istream& is);
 
 
         // Write
 
             //- Write to ostream (binary)
-            inline void write(ostream&) const;
+            inline void write(std::ostream& os) const;
 
             //- Write to Ostream (ASCII)
             inline Ostream& print(Ostream& os) const;
@@ -138,7 +140,8 @@ public:
 
     // Ostream operator
 
-        inline friend Ostream& operator<<(Ostream&, const STLtriangle&);
+        //- Print triangle contents
+        inline friend Ostream& operator<<(Ostream& os, const STLtriangle& tri);
 };
 
 

commit ec94c078e3322379747473b24567bfeafcf720bc

BUG: STL patch names not being passed through
- regression introduced by a7195288321096a7

STYLE: add meaningful solid names for some stl files

src/triSurface/triSurface/interfaces/STL/readSTL.C
@@ -84,6 +84,20 @@ bool Foam::triSurface::readSTL(const fileName& STLfileName, bool forceBinary)
         f.region() = zoneIds[i];
     }
 
+    // Set patch names (and sizes)
+    // - there is likely a more efficient means of doing this
+    if (reader.stlFormat() == fileFormats::STLCore::ASCII)
+    {
+        const List<word>& names = reader.names();
+
+        patches_.setSize(names.size());
+        forAll(names, namei)
+        {
+            patches_[namei].name() = names[namei];
+        }
+        setDefaultPatches();
+    }
+
     return true;
 }
 

tutorials/incompressible/porousSimpleFoam/straightDuctImplicit/constant/triSurface/porosity.stl
@@ -1,4 +1,4 @@
-solid ascii
+solid porosity
  facet normal 0 -1 0
   outer loop
    vertex 0 0 -0.025
@@ -33599,4 +33599,4 @@ solid ascii
    vertex 0.1 0.0475 0.025
   endloop
  endfacet
-endsolid
+endsolid porosity

tutorials/incompressible/porousSimpleFoam/straightDuctImplicit/constant/triSurface/porosity_inflated.stl
@@ -1,4 +1,4 @@
-solid ascii
+solid porosity_inflated
  facet normal 0 -1 0
   outer loop
    vertex 0 -0.005 -0.031
@@ -33599,4 +33599,4 @@ solid ascii
    vertex 0.1 0.052 0.029
   endloop
  endfacet
-endsolid
+endsolid porosity_inflated