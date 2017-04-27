88a03de238bbdd75bfe4f2d09d15311318e45909
Andrew Heather <a.heather@opencfd.co.uk>
2017-04-27 15:24:52 +0100

    ENH: Random numbers - updated dependent code from change to cachedRandom class

--- src/regionModels/surfaceFilmModels/submodels/kinematic/injectionModel/drippingInjection/drippingInjection.C
+++ src/regionModels/surfaceFilmModels/submodels/kinematic/injectionModel/drippingInjection/drippingInjection.C
@@ -57,7 +57,7 @@ drippingInjection::drippingInjection
     injectionModel(type(), owner, dict),
     deltaStable_(readScalar(coeffDict_.lookup("deltaStable"))),
     particlesPerParcel_(readScalar(coeffDict_.lookup("particlesPerParcel"))),
-    rndGen_(label(0), -1),
+    rndGen_(),
     parcelDistribution_
     (
         distributionModels::distributionModel::New
