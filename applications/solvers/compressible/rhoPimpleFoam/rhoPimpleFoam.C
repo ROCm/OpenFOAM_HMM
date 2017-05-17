39476bde1c8feefcd4e37c55663bca8fa43e422d
Henry Weller <http://cfd.direct>
2017-05-17 17:05:43 +0100

    GIT: Resolve conflict associated with cherry-pick of Foundation commit 79ff91350
    79ff91350 - rhoPimpleFoam: Improved support for compressible liquids
    (2017-05-17 17:05:43 +0100) <Henry Weller>

--- applications/solvers/compressible/rhoPimpleFoam/rhoPimpleFoam.C
+++ applications/solvers/compressible/rhoPimpleFoam/rhoPimpleFoam.C
@@ -122,6 +122,8 @@ int main(int argc, char *argv[])
             }
         }
 
+        rho = thermo.rho();
+
         runTime.write();
 
         Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
--- applications/solvers/lagrangian/coalChemistryFoam/coalChemistryFoam.C
+++ applications/solvers/lagrangian/coalChemistryFoam/coalChemistryFoam.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2017 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -42,6 +42,7 @@ Description
 #include "radiationModel.H"
 #include "SLGThermo.H"
 #include "pimpleControl.H"
+#include "pressureControl.H"
 #include "localEulerDdtScheme.H"
 #include "fvcSmooth.H"
 
