a6ef8b902736dd99fcc845795c73f369706e28d5
Andrew Heather <a.heather@opencfd.co.uk>
2017-06-20 14:36:15 +0100

    INT: Integration of isoAdvector and supporting material
    Community contribution from Johan Roenby, DHI
    
    IsoAdvector is a geometric Volume-of-Fluid method for advection of a
    sharp interface between two incompressible fluids. It works on both
    structured and unstructured meshes with no requirements on cell shapes.
    IsoAdvector is as an alternative choice for the interface compression
    treatment with the MULES limiter implemented in the interFoam family
    of solvers.
    
    The isoAdvector concept and code was developed at DHI and was funded
    by a Sapere Aude postdoc grant to Johan Roenby from The Danish Council
    for Independent Research | Technology and Production Sciences (Grant-ID:
    DFF - 1337-00118B - FTP).
    Co-funding is also provided by the GTS grant to DHI from the Danish
    Agency for Science, Technology and Innovation.
    
    The ideas behind and performance of the isoAdvector scheme is
    documented in:
    
        Roenby J, Bredmose H, Jasak H. 2016 A computational method for sharp
        interface  advection. R. Soc. open sci. 3: 160405.
        [http://dx.doi.org/10.1098/rsos.160405](http://dx.doi.org/10.1098/rsos.160405)
    
    Videos showing isoAdvector's performance with a number of standard
    test cases can be found in this youtube channel:
    
        https://www.youtube.com/channel/UCt6Idpv4C8TTgz1iUX0prAA
    
    Project contributors:
    
    * Johan Roenby <jro@dhigroup.com> (Inventor and main developer)
    * Hrvoje Jasak <hrvoje.jasak@fsb.hr> (Consistent treatment of
      boundary faces including processor boundaries, parallelisation,
      code clean up
    * Henrik Bredmose <hbre@dtu.dk> (Assisted in the conceptual
      development)
    * Vuko Vukcevic <vuko.vukcevic@fsb.hr> (Code review, profiling,
      porting to foam-extend, bug fixing, testing)
    * Tomislav Maric <tomislav@sourceflux.de> (Source file
      rearrangement)
    * Andy Heather <a.heather@opencfd.co.uk> (Integration into OpenFOAM
      for v1706 release)
    
    See the integration repository below to see the full set of changes
    implemented for release into OpenFOAM v1706
    
        https://develop.openfoam.com/Community/Integration-isoAdvector

---
+++ src/finiteVolume/fvMatrices/solvers/isoAdvection/isoAdvection/isoAdvectionTemplates.C
@@ -0,0 +1,111 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2016-2017 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+                isoAdvector | Copyright (C) 2016-2017 DHI
+-------------------------------------------------------------------------------
+
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "isoAdvection.H"
+
+// ************************************************************************* //
+
+template<typename Type>
+Type Foam::isoAdvection::faceValue
+(
+    const GeometricField<Type, fvsPatchField, surfaceMesh>& f,
+    const label facei
+) const
+{
+    if (mesh_.isInternalFace(facei))
+    {
+        return f.primitiveField()[facei];
+    }
+    else
+    {
+        const polyBoundaryMesh& pbm = mesh_.boundaryMesh();
+
+        // Boundary face. Find out which face of which patch
+        const label patchi = pbm.patchID()[facei - mesh_.nInternalFaces()];
+
+        if (patchi < 0 || patchi >= pbm.size())
+        {
+            FatalErrorInFunction
+                << "Cannot find patch for face " << facei
+                << abort(FatalError);
+        }
+
+        // Handle empty patches
+        const polyPatch& pp = pbm[patchi];
+        if (isA<emptyPolyPatch>(pp) || pp.empty())
+        {
+            return pTraits<Type>::zero;
+        }
+
+        const label patchFacei = pp.whichFace(facei);
+        return f.boundaryField()[patchi][patchFacei];
+    }
+}
+
+
+template<typename Type>
+void Foam::isoAdvection::setFaceValue
+(
+    GeometricField<Type, fvsPatchField, surfaceMesh>& f,
+    const label facei,
+    const Type& value
+) const
+{
+    if (mesh_.isInternalFace(facei))
+    {
+        f.primitiveFieldRef()[facei] = value;
+    }
+    else
+    {
+        const polyBoundaryMesh& pbm = mesh_.boundaryMesh();
+
+        // Boundary face. Find out which face of which patch
+        const label patchi = pbm.patchID()[facei - mesh_.nInternalFaces()];
+
+        if (patchi < 0 || patchi >= pbm.size())
+        {
+            FatalErrorInFunction
+                << "Cannot find patch for face " << facei
+                << abort(FatalError);
+        }
+
+        // Handle empty patches
+        const polyPatch& pp = pbm[patchi];
+        if (isA<emptyPolyPatch>(pp) || pp.empty())
+        {
+            return;
+        }
+
+        const label patchFacei = pp.whichFace(facei);
+
+        f.boundaryFieldRef()[patchi][patchFacei] = value;
+    }
+}
+
+
+// ************************************************************************* //
