56bfc75949a87840bd459d3f11d7c38385c9fc04
Henry Weller <http://cfd.direct>
2017-06-22 14:32:18 +0100

    Rationalize the "pos" function
    "pos" now returns 1 if the argument is greater than 0, otherwise it returns 0.
    This is consistent with the common mathematical definition of the "pos" function:
    
    https://en.wikipedia.org/wiki/Sign_(mathematics)
    
    However the previous implementation in which 1 was also returned for a 0
    argument is useful in many situations so the "pos0" has been added which returns
    1 if the argument is greater or equal to 0.  Additionally the "neg0" has been
    added which returns 1 if if the argument is less than or equal to 0.

--- src/finiteVolume/fields/fvPatchFields/derived/inletOutlet/inletOutletFvPatchField.C
+++ src/finiteVolume/fields/fvPatchFields/derived/inletOutlet/inletOutletFvPatchField.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2017 OpenFOAM Foundation
      \\/     M anipulation  | Copyright (C) 2017 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
@@ -128,7 +128,7 @@ void Foam::inletOutletFvPatchField<Type>::updateCoeffs()
             phiName_
         );
 
-    this->valueFraction() = 1.0 - pos(phip);
+    this->valueFraction() = 1.0 - pos0(phip);
 
     mixedFvPatchField<Type>::updateCoeffs();
 }
