3df71d18d044fb77c857bca8e6f290cbe5091120
Henry Weller <http://cfd.direct>
2017-06-22 14:42:36 +0100

    compressibleInterFoam: Improved mass conservation
    using the continuity error correction formulation developed for
    twoPhaseEulerFoam and reactingEulerFoam.

--- applications/solvers/multiphase/interFoam/interDyMFoam/interDyMFoam.C
+++ applications/solvers/multiphase/interFoam/interDyMFoam/interDyMFoam.C
@@ -132,7 +132,7 @@ int main(int argc, char *argv[])
                     // if the mesh topology changed
                     if (mesh.topoChanging())
                     {
-                        talphaPhiCorr0.clear();
+                        talphaPhi1Corr0.clear();
                     }
 
                     gh = (g & mesh.C()) - ghRef;
