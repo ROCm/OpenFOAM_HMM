929ed3ca02b71272da82115c316a323f96c1ef9c
Andrew Heather <a.heather@opencfd.co.uk>
2017-12-21 09:03:19 +0000

    ENH: New deferred correction scheme
    The characteristics of the base scheme are recovered by applying an
    explicit correction to the upwind scheme weights.
    
    Usage
        Example of the \c deferredCorrection scheme applied to the \c linear
        scheme:
        \verbatim
        divSchemes
        {
            .
            .
            div(phi,U)      Gauss deferredCorrection linear;
            .
            .
        }
        \endverbatim
    
    Based on a generalised form of a deferred correction linear scheme
    supplied by CFD Software E+F GmbH

---
+++ src/finiteVolume/interpolation/surfaceInterpolation/schemes/deferredCorrection/deferredCorrection.C
@@ -0,0 +1,71 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2017 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "deferredCorrection.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+template<class Type>
+Foam::tmp<Foam::GeometricField<Type, Foam::fvsPatchField, Foam::surfaceMesh>>
+Foam::deferredCorrection<Type>::correction
+(
+    const GeometricField<Type, fvPatchField, volMesh>& vf
+) const
+{
+    tmp<GeometricField<Type, fvsPatchField, surfaceMesh>> tsfCorr
+    (
+        new GeometricField<Type, fvsPatchField, surfaceMesh>
+        (
+            "deferredCorrection::correction(" + vf.name() + ')',
+            tbaseScheme_().interpolate(vf)
+        )
+    );
+    GeometricField<Type, fvsPatchField, surfaceMesh>& sfCorr = tsfCorr.ref();
+
+    // Interpolate using the upwind weights to avoid circular reference to
+    // [this] explicit correction
+    sfCorr -= upwind<Type>::interpolate(vf, upwind<Type>::weights());
+/*
+    auto& sfCorrBf = sfCorr.boundaryFieldRef();
+    for (auto& pf : sfCorrBf)
+    {
+        if (!pf.coupled())
+        {
+            pf = pTraits<Type>::zero;
+        }
+    }
+*/
+    return tsfCorr;
+}
+
+
+namespace Foam
+{
+    //makeSurfaceInterpolationScheme(deferredCorrection)
+    makeSurfaceInterpolationTypeScheme(deferredCorrection, scalar)
+    makeSurfaceInterpolationTypeScheme(deferredCorrection, vector)
+}
+
+// ************************************************************************* //
