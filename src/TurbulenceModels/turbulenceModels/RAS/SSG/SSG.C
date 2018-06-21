2f83248827ec6d95533ebd9a72d406cce8f8396f
sergio <sergio>
2018-06-21 16:12:41 -0700

    ENH: Bounding k using kMin in SSG RANS model

--- src/TurbulenceModels/turbulenceModels/RAS/SSG/SSG.C
+++ src/TurbulenceModels/turbulenceModels/RAS/SSG/SSG.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2015-2016 OpenFOAM Foundation
-     \\/     M anipulation  |
+     \\/     M anipulation  | Copyright (C) 2018 OpenCFD Ltd
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -379,6 +379,7 @@ void SSG<BasicTurbulenceModel>::correct()
     this->boundNormalStress(R);
 
     k_ = 0.5*tr(R);
+    bound(k_, this->kMin_);
 
     correctNut();
 
