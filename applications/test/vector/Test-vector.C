71ab8a473d5ce64b2aab41bf89dac37f0e6ead53
Mark Olesen <Mark.Olesen@esi-group.com>
2018-07-13 11:58:47 +0200

    ENH: add normalise methods for vectors and normalised function
    - the vector normalise() method modifies the object inplace,
      the normalised function returns a copy.
    
          vector vec1(1,2,3);
          vec1.normalise();
      vs
          vector vec1(1,2,3);
          vec1 /= mag(vec1) + VSMALL;
    
      For const usage, can use either of these
    
          const vector vec2a(normalised(vector(1,2,3)));
          const vector vec2b(vector(1,2,3).normalise());

--- applications/test/vector/Test-vector.C
+++ applications/test/vector/Test-vector.C
@@ -1,37 +1,97 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2018 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    Test-vector
+
+Description
+    Some simple tests for vector
+
+\*---------------------------------------------------------------------------*/
+
 #include "vector.H"
 #include "IOstreams.H"
 
 using namespace Foam;
 
-int main()
+void printInfo(const vector& vec)
 {
-    Info<< vector::zero << endl
-        << vector::one << endl
-        << vector::dim << endl
-        << vector::rank << endl;
+    Info<< "vector : " << vec << nl
+        << "magSqr : " << magSqr(vec) << nl;
 
-    vector d(0.5, 0.5, 0.5);
-    d /= mag(d);
+    Info<< "component"
+        << " max:" << cmptMax(vec)
+        << " sum:" << cmptSum(vec)
+        << " prod:" << cmptProduct(vec)
+        << " mag:" << cmptMag(vec)
+        << nl << nl;
+}
 
-    vector dSmall = (1e-100)*d;
-    dSmall /= mag(dSmall);
 
-    Info<< (dSmall - d) << endl;
+void doTest(vector& vec1, vector& vec2)
+{
+    Info<<"normalised(vector1): " << normalised(vec1) << nl;
+    Info<<"vector1: " << vec1 << nl;
+    vec1.normalise();
+    Info<<"normalised: " << vec1 << nl;
 
-    d *= 4.0;
+    vector vecsmall((1e-100 * vec1));
 
-    Info<< d << endl;
+    Info<<"small: " << vecsmall << nl;
+    Info<<"small normalised: " << normalised(vecsmall) << nl;
+    Info<<"small diff: " << (vecsmall.normalise() - vec1) << nl;
 
-    Info<< d + d << endl;
+    vec1 *= 4.0;
 
-    Info<< magSqr(d) << endl;
+    Info<< "scalar mult: " << vec1 << nl;
+    Info<< "addition   : " << (vec1 + vec1) << nl;
 
-    vector d2(0.5, 0.51, -0.5);
-    Info<< cmptMax(d2) << " "
-        << cmptSum(d2) << " "
-        << cmptProduct(d2) << " "
-        << cmptMag(d2)
-        << endl;
-    Info<< min(d, d2) << endl;
+    printInfo(vec1);
+    printInfo(vec2);
+
+    Info<< "min of " << vec1 << " and " << vec2 << " = "
+        << min(vec1, vec2) << nl << nl;
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+//  Main program:
+
+int main(int argc, char *argv[])
+{
+    Info<<"normalised: " << vector(1,2,3).normalise() << nl;
+    Info<<"normalised: " << vector(VSMALL,VSMALL,VSMALL).normalise() << nl;
+    Info<<"normalised: " <<
+        vector(ROOTVSMALL,ROOTVSMALL,ROOTVSMALL).normalise() << nl;
+
+    {
+        vector vec1(0.5, 0.5, 0.5);
+        vector vec2(0.5, 0.51, -0.5);
+
+        doTest(vec1, vec2);
+    }
+
     return 0;
 }
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
