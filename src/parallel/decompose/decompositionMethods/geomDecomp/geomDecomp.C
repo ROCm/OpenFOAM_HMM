3bb5cef5bfff444c9c410e6f8be987272c214969
mattijs <mattijs>
2018-07-18 09:29:28 +0100

    ENH: decompsition: check for mesh dimensions. Fixes #937.

--- src/parallel/decompose/decompositionMethods/geomDecomp/geomDecomp.C
+++ src/parallel/decompose/decompositionMethods/geomDecomp/geomDecomp.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2011-2017 OpenFOAM Foundation
-     \\/     M anipulation  |
+     \\/     M anipulation  | Copyright (C) 2018 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -55,6 +55,25 @@ void Foam::geomDecomp::readCoeffs()
         a*d - a2*d,  a*a2 + d2,  -2*a*d,
         a*d2 + a2,   a*d - a2*d,  d2 - a2
     );
+}
+
+
+void Foam::geomDecomp::checkDecompositionDirections
+(
+    const Vector<label>& meshDirs
+) const
+{
+    for (direction dir = 0; dir < Vector<label>::nComponents; dir++)
+    {
+        if (n_[dir] > 1 && meshDirs[dir] == -1)
+        {
+            FatalErrorInFunction << "Trying to decompose a 1/2D mesh"
+                << " into " << n_[dir]
+                << " parts in direction "
+                << Vector<label>::componentNames[dir]
+                << exit(FatalError);
+        }
+    }
 }
 
 
