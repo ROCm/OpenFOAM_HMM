edd93728e242d91ac973699665cc3f8e22303b7a
Mark Olesen <Mark.Olesen@esi-group.com>
2018-10-09 08:36:28 +0200

    Partial revert "ENH: add GeometricField constructor for movable internal field"
    Compilation problems on older compilers
    
    This reverts commit baf3e931de2ab59efbf3d005146689359b022e1f.

--- src/OpenFOAM/fields/GeometricFields/GeometricField/GeometricField.C
+++ src/OpenFOAM/fields/GeometricFields/GeometricField/GeometricField.C
@@ -332,52 +332,6 @@ Foam::GeometricField<Type, PatchField, GeoMesh>::GeometricField
 (
     const IOobject& io,
     const Mesh& mesh,
-    const dimensionSet& ds,
-    Field<Type>&& iField,
-    const PtrList<PatchField<Type>>& ptfl
-)
-:
-    Internal(io, mesh, ds, std::move(iField)),
-    timeIndex_(this->time().timeIndex()),
-    field0Ptr_(nullptr),
-    fieldPrevIterPtr_(nullptr),
-    boundaryField_(mesh.boundary(), *this, ptfl)
-{
-    DebugInFunction
-        << "Move construct from components" << nl << this->info() << endl;
-
-    readIfPresent();
-}
-
-
-template<class Type, template<class> class PatchField, class GeoMesh>
-Foam::GeometricField<Type, PatchField, GeoMesh>::GeometricField
-(
-    const IOobject& io,
-    const Mesh& mesh,
-    const dimensionSet& ds,
-    List<Type>&& iField,
-    const PtrList<PatchField<Type>>& ptfl
-)
-:
-    Internal(io, mesh, ds, std::move(iField)),
-    timeIndex_(this->time().timeIndex()),
-    field0Ptr_(nullptr),
-    fieldPrevIterPtr_(nullptr),
-    boundaryField_(mesh.boundary(), *this, ptfl)
-{
-    DebugInFunction
-        << "Move construct from components" << nl << this->info() << endl;
-
-    readIfPresent();
-}
-
-
-template<class Type, template<class> class PatchField, class GeoMesh>
-Foam::GeometricField<Type, PatchField, GeoMesh>::GeometricField
-(
-    const IOobject& io,
-    const Mesh& mesh,
     const bool readOldTime
 )
 :
