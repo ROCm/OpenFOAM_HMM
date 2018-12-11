a93a1abec755b5afa30a62674819a3d722704e7c
Mark Olesen <Mark.Olesen@esi-group.com>
2018-12-11 18:50:19 +0100

    COMP: define Scotch and Zoltan include dirs before mpi directories
    - the local third-party headers are to be searched before any MPI
      include directories. This stops the situation where an mpi-specific
      version of ptscotch.h is installed into the MPI directories.
    
      This "system" version may well have a different SCOTCH_Num size
      (32 bit vs 64 bit) from the third-party library etc.

--- src/parallel/decompose/metisDecomp/metisDecomp.C
+++ src/parallel/decompose/metisDecomp/metisDecomp.C
@@ -27,12 +27,23 @@ License
 #include "addToRunTimeSelectionTable.H"
 #include "Time.H"
 
-extern "C"
-{
-    #define OMPI_SKIP_MPICXX
-    #include "metis.h"
-}
+// Probably not needed...
+#define MPICH_SKIP_MPICXX
+#define OMPI_SKIP_MPICXX
 
+#include "metis.h"
+
+// Provide a clear error message if we have a size mismatch
+//
+// Metis has an 'idx_t' type, but the IDXTYPEWIDTH define is perhaps
+// more future-proof?
+#ifdef IDXTYPEWIDTH
+static_assert
+(
+    sizeof(Foam::label) == (IDXTYPEWIDTH/8),
+    "sizeof(Foam::label) == (IDXTYPEWIDTH/8), check your metis headers"
+);
+#endif
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
