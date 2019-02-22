gratienj/openfoam

commit c508da8f374470f86189d3b66710a4eb5877be42

ENH: for-range, forAllIters() ... in lagrangian/intermediate/
- reduced clutter when iterating over containers

src/lagrangian/intermediate/parcels/Templates/ReactingParcel/ReactingParcelIO.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2008-2011, 2016 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2008-2011, 2016-2019 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
                             | Copyright (C) 2011-2017 OpenFOAM Foundation
@@ -109,10 +109,11 @@ void Foam::ReactingParcel<ParcelType>::readFields
     c.checkFieldIOobject(c, mass0);
 
     label i = 0;
-    forAllIter(typename Cloud<ReactingParcel<ParcelType>>, c, iter)
+    for (ReactingParcel<ParcelType>& p : c)
     {
-        ReactingParcel<ParcelType>& p = iter();
-        p.mass0_ = mass0[i++];
+        p.mass0_ = mass0[i];
+
+        ++i;
     }
 
     // Get names and sizes for each Y...
@@ -126,9 +127,8 @@ void Foam::ReactingParcel<ParcelType>::readFields
 
 
     // Set storage for each Y... for each parcel
-    forAllIter(typename Cloud<ReactingParcel<ParcelType>>, c, iter)
+    for (ReactingParcel<ParcelType>& p : c)
     {
-        ReactingParcel<ParcelType>& p = iter();
         p.Y_.setSize(nPhases, 0.0);
     }
 
@@ -146,10 +146,11 @@ void Foam::ReactingParcel<ParcelType>::readFields
         );
 
         label i = 0;
-        forAllIter(typename Cloud<ReactingParcel<ParcelType>>, c, iter)
+        for (ReactingParcel<ParcelType>& p : c)
         {
-            ReactingParcel<ParcelType>& p = iter();
-            p.Y_[j] = Y[i++];
+            p.Y_[j] = Y[i];
+
+            ++i;
         }
     }
 }
@@ -179,10 +180,11 @@ void Foam::ReactingParcel<ParcelType>::writeFields
         IOField<scalar> mass0(c.fieldIOobject("mass0", IOobject::NO_READ), np);
 
         label i = 0;
-        forAllConstIter(typename Cloud<ReactingParcel<ParcelType>>, c, iter)
+        for (const ReactingParcel<ParcelType>& p : c)
         {
-            const ReactingParcel<ParcelType>& p = iter();
             mass0[i++] = p.mass0_;
+
+            ++i;
         }
         mass0.write(np > 0);
 
@@ -205,16 +207,13 @@ void Foam::ReactingParcel<ParcelType>::writeFields
                 ),
                 np
             );
+
             label i = 0;
-            forAllConstIter
-            (
-                typename Cloud<ReactingParcel<ParcelType>>,
-                c,
-                iter
-            )
+            for (const ReactingParcel<ParcelType>& p : c)
             {
-                const ReactingParcel<ParcelType>& p = iter();
-                Y[i++] = p.Y()[j];
+                Y[i] = p.Y()[j];
+
+                ++i;
             }
 
             Y.write(np > 0);
@@ -253,10 +252,11 @@ void Foam::ReactingParcel<ParcelType>::writeObjects
         IOField<scalar>& mass0(cloud::createIOField<scalar>("mass0", np, obr));
 
         label i = 0;
-        forAllConstIter(typename Cloud<ReactingParcel<ParcelType>>, c, iter)
+        for (const ReactingParcel<ParcelType>& p : c)
         {
-            const ReactingParcel<ParcelType>& p = iter();
-            mass0[i++] = p.mass0_;
+            mass0[i] = p.mass0_;
+
+            ++i;
         }
 
         // Write the composition fractions
@@ -276,15 +276,11 @@ void Foam::ReactingParcel<ParcelType>::writeObjects
             );
 
             label i = 0;
-            forAllConstIter
-            (
-                typename Cloud<ReactingParcel<ParcelType>>,
-                c,
-                iter
-            )
+            for (const ReactingParcel<ParcelType>& p : c)
             {
-                const ReactingParcel<ParcelType>& p = iter();
-                Y[i++] = p.Y()[j];
+                Y[i] = p.Y()[j];
+
+                ++i;
             }
         }
     }

src/lagrangian/intermediate/clouds/Templates/CollidingCloud/CollidingCloudI.H
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2008-2011 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2008-2011, 2019 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
                             | Copyright (C) 2011-2016 OpenFOAM Foundation
@@ -65,10 +65,8 @@ Foam::CollidingCloud<CloudType>::rotationalKineticEnergyOfSystem() const
 {
     scalar rotationalKineticEnergy = 0.0;
 
-    forAllConstIter(typename CollidingCloud<CloudType>, *this, iter)
+    for (const parcelType& p : *this)
     {
-        const parcelType& p = iter();
-
         rotationalKineticEnergy +=
             p.nParticle()*0.5*p.momentOfInertia()*(p.omega() & p.omega());
     }

src/lagrangian/intermediate/clouds/Templates/KinematicCloud/KinematicCloud.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2008-2011, 2016 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2008-2011, 2016-2019 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
                             | Copyright (C) 2011-2017 OpenFOAM Foundation
@@ -155,14 +155,14 @@ void Foam::KinematicCloud<CloudType>::buildCellOccupancy()
 
     List<DynamicList<parcelType*>>& cellOccupancy = cellOccupancyPtr_();
 
-    forAll(cellOccupancy, cO)
+    for (auto& list : cellOccupancy)
     {
-        cellOccupancy[cO].clear();
+        list.clear();
     }
 
-    forAllIter(typename KinematicCloud<CloudType>, *this, iter)
+    for (parcelType& p : *this)
     {
-        cellOccupancy[iter().cell()].append(&iter());
+        cellOccupancy[p.cell()].append(&p);
     }
 }
 

src/lagrangian/intermediate/clouds/Templates/KinematicCloud/KinematicCloudI.H
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2008-2011 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2008-2011, 2019 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
                             | Copyright (C) 2011-2017 OpenFOAM Foundation
@@ -273,10 +273,9 @@ template<class CloudType>
 inline Foam::scalar Foam::KinematicCloud<CloudType>::massInSystem() const
 {
     scalar sysMass = 0.0;
-    forAllConstIter(typename KinematicCloud<CloudType>, *this, iter)
+    for (const parcelType& p : *this)
     {
-         const parcelType& p = iter();
-         sysMass += p.nParticle()*p.mass();
+        sysMass += p.nParticle()*p.mass();
     }
 
     return sysMass;
@@ -289,10 +288,8 @@ Foam::KinematicCloud<CloudType>::linearMomentumOfSystem() const
 {
     vector linearMomentum(Zero);
 
-    forAllConstIter(typename KinematicCloud<CloudType>, *this, iter)
+    for (const parcelType& p : *this)
     {
-        const parcelType& p = iter();
-
         linearMomentum += p.nParticle()*p.mass()*p.U();
     }
 
@@ -306,10 +303,8 @@ Foam::KinematicCloud<CloudType>::linearKineticEnergyOfSystem() const
 {
     scalar linearKineticEnergy = 0.0;
 
-    forAllConstIter(typename KinematicCloud<CloudType>, *this, iter)
+    for (const parcelType& p : *this)
     {
-        const parcelType& p = iter();
-
         linearKineticEnergy += p.nParticle()*0.5*p.mass()*(p.U() & p.U());
     }
 
@@ -326,9 +321,8 @@ inline Foam::scalar Foam::KinematicCloud<CloudType>::Dij
 {
     scalar si = 0.0;
     scalar sj = 0.0;
-    forAllConstIter(typename KinematicCloud<CloudType>, *this, iter)
+    for (const parcelType& p : *this)
     {
-        const parcelType& p = iter();
         si += p.nParticle()*pow(p.d(), i);
         sj += p.nParticle()*pow(p.d(), j);
     }
@@ -345,9 +339,8 @@ template<class CloudType>
 inline Foam::scalar Foam::KinematicCloud<CloudType>::Dmax() const
 {
     scalar d = -GREAT;
-    forAllConstIter(typename KinematicCloud<CloudType>, *this, iter)
+    for (const parcelType& p : *this)
     {
-        const parcelType& p = iter();
         d = max(d, p.d());
     }
 
@@ -477,9 +470,8 @@ Foam::KinematicCloud<CloudType>::vDotSweep() const
     );
 
     volScalarField& vDotSweep = tvDotSweep.ref();
-    forAllConstIter(typename KinematicCloud<CloudType>, *this, iter)
+    for (const parcelType& p : *this)
     {
-        const parcelType& p = iter();
         const label celli = p.cell();
 
         vDotSweep[celli] += p.nParticle()*p.areaP()*mag(p.U() - U_[celli]);
@@ -516,9 +508,8 @@ Foam::KinematicCloud<CloudType>::theta() const
     );
 
     volScalarField& theta = ttheta.ref();
-    forAllConstIter(typename KinematicCloud<CloudType>, *this, iter)
+    for (const parcelType& p : *this)
     {
-        const parcelType& p = iter();
         const label celli = p.cell();
 
         theta[celli] += p.nParticle()*p.volume();
@@ -554,9 +545,8 @@ Foam::KinematicCloud<CloudType>::alpha() const
     );
 
     scalarField& alpha = talpha.ref().primitiveFieldRef();
-    forAllConstIter(typename KinematicCloud<CloudType>, *this, iter)
+    for (const parcelType& p : *this)
     {
-        const parcelType& p = iter();
         const label celli = p.cell();
 
         alpha[celli] += p.nParticle()*p.mass();
@@ -591,9 +581,8 @@ Foam::KinematicCloud<CloudType>::rhoEff() const
     );
 
     scalarField& rhoEff = trhoEff.ref().primitiveFieldRef();
-    forAllConstIter(typename KinematicCloud<CloudType>, *this, iter)
+    for (const parcelType& p : *this)
     {
-        const parcelType& p = iter();
         const label celli = p.cell();
 
         rhoEff[celli] += p.nParticle()*p.mass();

src/lagrangian/intermediate/clouds/Templates/ThermoCloud/ThermoCloudI.H
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2008-2011 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2008-2011, 2019 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
                             | Copyright (C) 2011-2017 OpenFOAM Foundation
@@ -374,48 +374,42 @@ Foam::ThermoCloud<CloudType>::sigmap() const
 template<class CloudType>
 inline Foam::scalar Foam::ThermoCloud<CloudType>::Tmax() const
 {
-    scalar T = -GREAT;
-    scalar n = 0;
-    forAllConstIter(typename ThermoCloud<CloudType>, *this, iter)
+    scalar val = -GREAT;
+    bool nonEmpty = false;
+
+    for (const parcelType& p : *this)
     {
-        const parcelType& p = iter();
-        T = max(T, p.T());
-        n++;
+        val = max(val, p.T());
+        nonEmpty = true;
     }
 
-    reduce(T, maxOp<scalar>());
-    reduce(n, sumOp<label>());
-
-    if (n > 0)
+    if (returnReduce(nonEmpty, orOp<bool>()))
     {
-        return T;
+        return returnReduce(val, maxOp<scalar>());
     }
 
-    return 0.0;
+    return 0;
 }
 
 
 template<class CloudType>
 inline Foam::scalar Foam::ThermoCloud<CloudType>::Tmin() const
 {
-    scalar T = GREAT;
-    scalar n = 0;
-    forAllConstIter(typename ThermoCloud<CloudType>, *this, iter)
+    scalar val = GREAT;
+    bool nonEmpty = false;
+
+    for (const parcelType& p : *this)
     {
-        const parcelType& p = iter();
-        T = min(T, p.T());
-        n++;
+        val = min(val, p.T());
+        nonEmpty = true;
     }
 
-    reduce(T, minOp<scalar>());
-    reduce(n, sumOp<label>());
-
-    if (n > 0)
+    if (returnReduce(nonEmpty, orOp<bool>()))
     {
-        return T;
+        return returnReduce(val, minOp<scalar>());
     }
 
-    return 0.0;
+    return 0;
 }
 
 

src/lagrangian/intermediate/parcels/Templates/CollidingParcel/CollidingParcelIO.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2011 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2011-2011, 2019 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
                             | Copyright (C) 2011-2017 OpenFOAM Foundation
@@ -164,10 +164,8 @@ void Foam::CollidingParcel<ParcelType>::readFields(CloudType& c)
 
     label i = 0;
 
-    forAllIter(typename CloudType, c, iter)
+    for (CollidingParcel<ParcelType>& p : c)
     {
-        CollidingParcel<ParcelType>& p = iter();
-
         p.f_ = f[i];
         p.angularMomentum_ = angularMomentum[i];
         p.torque_ = torque[i];
@@ -183,7 +181,7 @@ void Foam::CollidingParcel<ParcelType>::readFields(CloudType& c)
             collisionRecordsWallData[i]
         );
 
-        i++;
+        ++i;
     }
 }
 
@@ -245,11 +243,8 @@ void Foam::CollidingParcel<ParcelType>::writeFields(const CloudType& c)
     );
 
     label i = 0;
-
-    forAllConstIter(typename CloudType, c, iter)
+    for (const CollidingParcel<ParcelType>& p : c)
     {
-        const CollidingParcel<ParcelType>& p = iter();
-
         f[i] = p.f();
         angularMomentum[i] = p.angularMomentum();
         torque[i] = p.torque();
@@ -264,7 +259,7 @@ void Foam::CollidingParcel<ParcelType>::writeFields(const CloudType& c)
         collisionRecordsWallPRel[i] = p.collisionRecords().wallPRel();
         collisionRecordsWallData[i] = p.collisionRecords().wallData();
 
-        i++;
+        ++i;
     }
 
     const bool valid = (np > 0);
@@ -303,15 +298,13 @@ void Foam::CollidingParcel<ParcelType>::writeObjects
     IOField<vector>& torque(cloud::createIOField<vector>("torque", np, obr));
 
     label i = 0;
-    forAllConstIter(typename CloudType, c, iter)
+    for (const CollidingParcel<ParcelType>& p : c)
     {
-        const CollidingParcel<ParcelType>& p = iter();
-
         f[i] = p.f();
         angularMomentum[i] = p.angularMomentum();
         torque[i] = p.torque();
 
-        i++;
+        ++i;
     }
 }
 

src/lagrangian/intermediate/parcels/Templates/KinematicParcel/KinematicParcelIO.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2008-2011, 2016 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2008-2011, 2016-2019 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
                             | Copyright (C) 2011-2017 OpenFOAM Foundation
@@ -176,10 +176,8 @@ void Foam::KinematicParcel<ParcelType>::readFields(CloudType& c)
 
     label i = 0;
 
-    forAllIter(typename CloudType, c, iter)
+    for (KinematicParcel<ParcelType>& p : c)
     {
-        KinematicParcel<ParcelType>& p = iter();
-
         p.active_ = active[i];
         p.typeId_ = typeId[i];
         p.nParticle_ = nParticle[i];
@@ -191,7 +189,7 @@ void Foam::KinematicParcel<ParcelType>::readFields(CloudType& c)
         p.tTurb_ = tTurb[i];
         p.UTurb_ = UTurb[i];
 
-        i++;
+        ++i;
     }
 }
 
@@ -221,10 +219,8 @@ void Foam::KinematicParcel<ParcelType>::writeFields(const CloudType& c)
 
     label i = 0;
 
-    forAllConstIter(typename CloudType, c, iter)
+    for (const KinematicParcel<ParcelType>& p : c)
     {
-        const KinematicParcel<ParcelType>& p = iter();
-
         active[i] = p.active();
         typeId[i] = p.typeId();
         nParticle[i] = p.nParticle();
@@ -236,7 +232,7 @@ void Foam::KinematicParcel<ParcelType>::writeFields(const CloudType& c)
         tTurb[i] = p.tTurb();
         UTurb[i] = p.UTurb();
 
-        i++;
+        ++i;
     }
 
     const bool valid = np > 0;
@@ -282,10 +278,8 @@ void Foam::KinematicParcel<ParcelType>::writeObjects
 
     label i = 0;
 
-    forAllConstIter(typename CloudType, c, iter)
+    for (const KinematicParcel<ParcelType>& p : c)
     {
-        const KinematicParcel<ParcelType>& p = iter();
-
         active[i] = p.active();
         typeId[i] = p.typeId();
         nParticle[i] = p.nParticle();
@@ -297,7 +291,7 @@ void Foam::KinematicParcel<ParcelType>::writeObjects
         tTurb[i] = p.tTurb();
         UTurb[i] = p.UTurb();
 
-        i++;
+        ++i;
     }
 }
 

src/lagrangian/intermediate/parcels/Templates/MPPICParcel/MPPICParcelIO.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2016 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2016-2019 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
                             | Copyright (C) 2013-2017 OpenFOAM Foundation
@@ -92,14 +92,11 @@ void Foam::MPPICParcel<ParcelType>::readFields(CloudType& c)
     c.checkFieldIOobject(c, UCorrect);
 
     label i = 0;
-
-    forAllIter(typename CloudType, c, iter)
+    for (MPPICParcel<ParcelType>& p : c)
     {
-        MPPICParcel<ParcelType>& p = iter();
-
         p.UCorrect_ = UCorrect[i];
 
-        i++;
+        ++i;
     }
 }
 
@@ -117,13 +114,11 @@ void Foam::MPPICParcel<ParcelType>::writeFields(const CloudType& c)
 
     label i = 0;
 
-    forAllConstIter(typename CloudType, c, iter)
+    for (const MPPICParcel<ParcelType>& p : c)
     {
-        const MPPICParcel<ParcelType>& p = iter();
-
         UCorrect[i] = p.UCorrect();
 
-        i++;
+        ++i;
     }
 
     UCorrect.write(np > 0);
@@ -147,13 +142,11 @@ void Foam::MPPICParcel<ParcelType>::writeObjects
 
     label i = 0;
 
-    forAllConstIter(typename CloudType, c, iter)
+    for (const MPPICParcel<ParcelType>& p : c)
     {
-        const MPPICParcel<ParcelType>& p = iter();
-
         UCorrect[i] = p.UCorrect();
 
-        i++;
+        ++i;
     }
 }
 

ROCm/OpenFOAM_HMM

commit 683817bcab9b6c1305f7821737d51fed2681dd99

BUG: incorrect increment introduced by c508da8f3744

src/lagrangian/intermediate/parcels/Templates/ReactingParcel/ReactingParcelIO.C
@@ -182,7 +182,7 @@ void Foam::ReactingParcel<ParcelType>::writeFields
         label i = 0;
         for (const ReactingParcel<ParcelType>& p : c)
         {
-            mass0[i++] = p.mass0_;
+            mass0[i] = p.mass0_;
 
             ++i;
         }