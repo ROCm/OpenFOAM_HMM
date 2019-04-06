d4eb17a9ff5fff5d7879c37bf59d063fbfbd68ed
Mark Olesen <Mark.Olesen@esi-group.com>
2019-04-06 15:07:53 +0200

    ENH: new sliceRange class
    - this is somewhat like labelRange, but with a stride.
      Can be used to define slices (of lists, fields, ..) or as a range specifier
      for a for-loop. For example,
    
          for (label i : sliceRange(0, 10, 3))
          {
              ...
          }

---
+++ applications/test/sliceRange/Test-sliceRange.C
@@ -0,0 +1,136 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2019 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Test slice range
+\*---------------------------------------------------------------------------*/
+
+#include "argList.H"
+#include "labelList.H"
+#include "FixedList.H"
+#include "sliceRange.H"
+
+using namespace Foam;
+
+
+typedef FixedList<label, 3> sliceCoeffs;
+
+void printInfo(const sliceCoeffs& coeffs)
+{
+    sliceRange range(coeffs);
+
+    Info<< nl
+        << "coeffs: " << coeffs << nl
+        << "range: " << range << nl
+        << "first: " << range.first() << nl
+        << "*begin  " << *range.begin() << nl
+        << "last:  " << range.last() << nl
+        << "*end    " << *range.end() << nl
+        << "values: " << flatOutput(range.labels()) << nl;
+
+    Info<< "for  :";
+    for (const label val : range)
+    {
+        Info<< ' ' << val;
+    }
+    Info<< nl;
+
+    if (range.empty())
+    {
+        Info<< "empty"<< nl;
+        return;
+    }
+
+    Info<< "mid-point [" << (range.size()/2) << "] = "
+        << range[range.size()/2] << nl;
+
+    const auto endIter = range.cend();
+
+    for (const label i : {-1, (range.size()/2), range.size()})
+    {
+        const auto iter = range.at(i);
+
+        Info<< "at(" << i << ") = " << *iter;
+
+        if (iter == endIter)
+        {
+            Info<< " (out-of-range)";
+        }
+
+        Info<< nl;
+    }
+
+    // Copy construct
+    sliceRange range2(range);
+
+    // Copy assign
+    range2 = range;
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+//  Main program:
+
+int main(int argc, char *argv[])
+{
+    argList::noBanner();
+    argList::noParallel();
+    argList::noFunctionObjects();
+
+    argList args(argc, argv, false, true);
+
+
+    for
+    (
+        sliceCoeffs coeffs :
+        {
+            sliceCoeffs{25, 8, 2},
+            sliceCoeffs{15, 5, 3},
+            sliceCoeffs{15, -5, 2},
+        }
+    )
+    {
+        printInfo(coeffs);
+    }
+
+
+    // Generator
+    {
+        sliceRange range(25, 8, 3);
+
+        Info<< "Generator for " << range << nl;
+
+        auto gen = range.generator();
+
+        for (label i=0; i < 10; ++i)
+        {
+            Info<< "    " << gen() << nl;
+        }
+    }
+
+    Info<< "\nEnd\n" << endl;
+
+    return 0;
+}
+
+// ************************************************************************* //
