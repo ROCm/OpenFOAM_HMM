cbc3953f2628626e18cb3929659fdc6613b2c5db
mattijs <mattijs>
2019-04-10 10:32:18 +0100

    Revert "BUG: snappyHexMesh: attraction distance mapping. Fixes #941."
    This reverts commit 708e6509901005b2805dc1d67266d9e12dd56323.

--- src/mesh/snappyHexMesh/snappyHexMeshDriver/snappySnapDriverFeature.C
+++ src/mesh/snappyHexMesh/snappyHexMeshDriver/snappySnapDriverFeature.C
@@ -308,7 +308,7 @@ void Foam::snappySnapDriver::calcNearestFace
         (
             labelList(1, zoneSurfi),
             fc,
-            sqr(scalarField(faceSnapDist, ppFaces)),// sqr of attract dist
+            sqr(faceSnapDist),// sqr of attract dist
             hitSurface,
             hitInfo,
             hitRegion,
@@ -384,7 +384,7 @@ void Foam::snappySnapDriver::calcNearestFace
     (
         unzonedSurfaces,
         fc,
-        sqr(scalarField(faceSnapDist, ppFaces)),// sqr of attract dist
+        sqr(faceSnapDist),// sqr of attract dist
         hitSurface,
         hitInfo,
         hitRegion,
