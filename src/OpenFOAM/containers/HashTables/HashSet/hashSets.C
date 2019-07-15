4862cb4603500ba2d3147a0c89fe9de0a42ad009
Mark Olesen <Mark.Olesen@esi-group.com>
2019-07-15 08:36:05 +0200

    GIT: add forgotten file (commit 4ee65d12c4ae2999)

---
+++ src/OpenFOAM/containers/HashTables/HashSet/hashSets.C
@@ -0,0 +1,72 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2019 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "HashSet.H"
+#include "MinMax.H"
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+Foam::label Foam::min(const labelHashSet& set, label minValue)
+{
+    for (const label val : set)
+    {
+        if (minValue > val)
+        {
+            minValue = val;
+        }
+    }
+
+    return minValue;
+}
+
+
+Foam::label Foam::max(const labelHashSet& set, label maxValue)
+{
+    for (const label val : set)
+    {
+        if (maxValue < val)
+        {
+            maxValue = val;
+        }
+    }
+
+    return maxValue;
+}
+
+
+Foam::MinMax<Foam::label> Foam::minMax(const labelHashSet& set)
+{
+    MinMax<label> result;
+
+    for (const label val : set)
+    {
+        result += val;
+    }
+
+    return result;
+}
+
+
+// ************************************************************************* //
