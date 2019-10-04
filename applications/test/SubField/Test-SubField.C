61e95b84714071c42f5daecca231e0c3d2c89d0f
Mark Olesen <Mark.Olesen@esi-group.com>
2019-10-04 14:21:18 +0200

    ENH: improvements to SubList and SubField
    - SubField and SubList assign from zero
    - SubField +=, -=, *=, /= operators
    
    - SubList construct from UList (as per SubField)
    
      Note: constructing an anonymous SubField or SubList with a single
      parameter should use '{} instead of '()' to avoid compiler
      ambiguities.

---
+++ applications/test/SubField/Test-SubField.C
@@ -0,0 +1,94 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2019 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    Test-SubField
+
+Description
+    Simple tests on SubList, SubField
+
+\*---------------------------------------------------------------------------*/
+
+#include "OSspecific.H"
+#include "argList.H"
+
+#include "scalarField.H"
+#include "SubField.H"
+#include "labelRange.H"
+#include <numeric>
+
+using namespace Foam;
+
+template<class T>
+void print(const UList<T>& list)
+{
+    Info<< flatOutput(list) << nl;
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+//  Main program:
+
+int main(int argc, char *argv[])
+{
+    argList::noParallel();
+    argList::noFunctionObjects();
+
+    {
+        List<scalar> ident(25);
+        std::iota(ident.begin(), ident.end(), 0);
+
+        print(ident);
+
+        SubList<scalar>(ident, 10) = -10;
+        print(ident);
+
+        SubField<scalar>(ident, 10) = 10;
+        print(ident);
+
+        SubField<scalar>(ident, 10) += 10;
+        print(ident);
+
+        SubField<scalar>{ident, 10, 10} *= 5;
+        print(ident);
+
+
+        // NOTE: Need {} instead of ()
+        // SubList<scalar>(ident) = 100;
+
+        // GCC
+        // error: conflicting declaration 'Foam::SubList<double> ident'
+
+        // CLANG
+        // warning: parentheses were disambiguated as redundant parentheses
+        // around declaration of variable named 'ident' [-Wvexing-parse]
+
+        SubList<scalar>{ident} = 100;
+        print(ident);
+    }
+
+    Info << "\nEnd\n";
+    return 0;
+}
+
+// ************************************************************************* //
