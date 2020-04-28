50055b3d00bed203acfad1a7d148a51f2f64d9ca
Mark Olesen <Mark.Olesen@esi-group.com>
2020-04-28 15:00:07 +0200

    ENH: add IOobject::scopedName() static method
    - preliminary to further changes (#1675)

--- src/OpenFOAM/db/IOobject/IOobject.C
+++ src/OpenFOAM/db/IOobject/IOobject.C
@@ -6,7 +6,7 @@
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
     Copyright (C) 2011-2017 OpenFOAM Foundation
-    Copyright (C) 2016-2019 OpenCFD Ltd.
+    Copyright (C) 2016-2020 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -36,6 +36,17 @@ namespace Foam
 {
     defineTypeNameAndDebug(IOobject, 0);
 }
+
+char Foam::IOobject::scopeSeparator
+(
+    #ifdef _WIN32
+    // Windows: using ':' for scoping conflicts with d:/path etc
+    Foam::debug::infoSwitch("scopeSeparator", '_')
+    #else
+    Foam::debug::infoSwitch("scopeSeparator", ':')
+    #endif
+);
+
 
 const Foam::Enum
 <
