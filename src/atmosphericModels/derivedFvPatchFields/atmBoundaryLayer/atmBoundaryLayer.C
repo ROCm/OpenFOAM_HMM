bb2ae5c90d2aeb7af2f53eef52adefc5b6c95595
Mark Olesen <Mark.Olesen@esi-group.com>
2020-06-05 17:01:27 +0200

    GIT: remove merge/rebase artifacts

--- src/atmosphericModels/derivedFvPatchFields/atmBoundaryLayer/atmBoundaryLayer.C
+++ src/atmosphericModels/derivedFvPatchFields/atmBoundaryLayer/atmBoundaryLayer.C
@@ -74,8 +74,6 @@ atmBoundaryLayer::atmBoundaryLayer
     patch_(pp),
     flowDir_(TimeFunction1<vector>(time, "flowDir", dict)),
     zDir_(TimeFunction1<vector>(time, "zDir", dict)),
-    kappa_(dict.getOrDefault<scalar>("kappa", 0.41)),
-    Cmu_(dict.getOrDefault<scalar>("Cmu", 0.09)),
     Uref_(TimeFunction1<scalar>(time, "Uref", dict)),
     Zref_(TimeFunction1<scalar>(time, "Zref", dict)),
     z0_(PatchFunction1<scalar>::New(pp, "z0", dict)),
