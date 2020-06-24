vsujeesh/openfoam

ENH: pimpleControl - added ddtCorr flag

src/finiteVolume/cfdTools/general/solutionControl/pimpleControl/pimpleControl.C
@@ -50,6 +50,7 @@ bool Foam::pimpleControl::read()
     SIMPLErho_ = pimpleDict.lookupOrDefault("SIMPLErho", false);
     turbOnFinalIterOnly_ =
         pimpleDict.lookupOrDefault("turbOnFinalIterOnly", true);
+    ddtCorr_ = pimpleDict.lookupOrDefault("ddtCorr", true);
 
     return true;
 }
@@ -153,6 +154,7 @@ Foam::pimpleControl::pimpleControl
     corrPISO_(0),
     SIMPLErho_(false),
     turbOnFinalIterOnly_(true),
+    ddtCorr_(true),
     converged_(false)
 {
     read();

ROCm/OpenFOAM_HMM

src/finiteVolume/cfdTools/general/solutionControl/pimpleControl/pimpleControl.C
@@ -52,6 +52,7 @@ bool Foam::pimpleControl::read()
         pimpleDict.getOrDefault("turbOnFinalIterOnly", true);
     finalOnLastPimpleIterOnly_ =
         pimpleDict.getOrDefault("finalOnLastPimpleIterOnly", false);
+    ddtCorr_ = pimpleDict.getOrDefault("ddtCorr", true);
 
     return true;
 }
@@ -155,8 +156,9 @@ Foam::pimpleControl::pimpleControl
     corrPISO_(0),
     SIMPLErho_(false),
     turbOnFinalIterOnly_(true),
-    converged_(false),
-    finalOnLastPimpleIterOnly_(false)
+    finalOnLastPimpleIterOnly_(false),
+    ddtCorr_(true),
+    converged_(false)
 {
     read();
 