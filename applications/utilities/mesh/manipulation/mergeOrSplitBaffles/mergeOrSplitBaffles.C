ROCm/OpenFOAM_HMM

commit f721b5344f51ba8e79991febf037d863268e91d6

ENH: report dictionary name actually used (for -dict option)

applications/utilities/mesh/manipulation/mergeOrSplitBaffles/mergeOrSplitBaffles.C
@@ -6,7 +6,7 @@
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
     Copyright (C) 2011-2016 OpenFOAM Foundation
-    Copyright (C) 2016-2018 OpenCFD Ltd.
+    Copyright (C) 2016-2020 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -286,7 +286,12 @@ int main(int argc, char *argv[])
 
     #include "addOverwriteOption.H"
     #include "addRegionOption.H"
-    #include "addDictOption.H"
+    argList::addBoolOption
+    (
+        "dict",
+        "file",
+        "Specify a dictionary to read actions from"
+    );
     argList::addBoolOption
     (
         "detectOnly",
@@ -330,7 +335,7 @@ int main(int argc, char *argv[])
         const word dictName;
         #include "setSystemMeshDictionaryIO.H"
 
-        Info<< "Reading " << dictName << "\n" << endl;
+        Info<< "Reading " << dictIO.name() << nl << endl;
         IOdictionary dict(dictIO);
 
         if (dict.found("detect"))

applications/test/PDRblockMesh/Test-PDRblockMesh.C
@@ -5,7 +5,7 @@
     \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2019 OpenCFD Ltd.
+    Copyright (C) 2019-2020 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -42,12 +42,7 @@ int main(int argc, char *argv[])
 {
     argList::noParallel();
     argList::noFunctionObjects();
-    argList::addOption
-    (
-        "dict",
-        "file",
-        "Alternative dictionary for the PDRblockMesh description"
-    );
+    argList::addOption("dict", "file", "Alternative PDRblockMeshDict");
 
     #include "setRootCase.H"
     #include "createTime.H"
@@ -56,7 +51,7 @@ int main(int argc, char *argv[])
 
     #include "setSystemRunTimeDictionaryIO.H"
 
-    Info<< "Reading " << dictName << nl << endl;
+    Info<< "Reading " << dictIO.name() << nl << endl;
 
     IOdictionary meshDict(dictIO);
 

applications/test/surfaceIntersection/Test-surfaceIntersection.C
@@ -116,11 +116,16 @@ int main(int argc, char *argv[])
         "geometry scaling factor"
     );
 
-    #include "addDictOption.H"
+    argList::addOption
+    (
+        "dict",
+        "file",
+        "Dictionary of intersect options"
+    );
 
     argList::addNote
     (
-        "test intersect of two surfaces. Writes obj file"
+        "Test intersect of two surfaces. Writes obj file"
     );
     argList::noParallel();
     argList::noFunctionObjects();

applications/utilities/mesh/advanced/collapseEdges/collapseEdges.C
@@ -6,6 +6,7 @@
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
     Copyright (C) 2011-2016 OpenFOAM Foundation
+    Copyright (C) 2020 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -84,7 +85,7 @@ int main(int argc, char *argv[])
         "Collapse faces that are in the supplied face set"
     );
 
-    argList::addOption("dict", "file", "Use alternative collapseDict");
+    argList::addOption("dict", "file", "Alternative collapseDict");
 
     #include "addOverwriteOption.H"
 
@@ -102,7 +103,7 @@ int main(int argc, char *argv[])
     const word dictName("collapseDict");
     #include "setSystemMeshDictionaryIO.H"
 
-    Info<< "Reading " << dictName << nl << endl;
+    Info<< "Reading " << dictIO.name() << nl << endl;
 
     IOdictionary collapseDict(dictIO);
 

applications/utilities/mesh/advanced/modifyMesh/modifyMesh.C
@@ -339,7 +339,7 @@ int main(int argc, char *argv[])
         "For example, moving points, splitting/collapsing edges etc."
     );
     #include "addOverwriteOption.H"
-    argList::addOption("dict", "file", "Use alternative modifyMeshDict");
+    argList::addOption("dict", "file", "Alternative modifyMeshDict");
 
     argList::noFunctionObjects();  // Never use function objects
 

applications/utilities/mesh/generation/PDRblockMesh/PDRblockMesh.C
@@ -5,7 +5,7 @@
     \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2019 OpenCFD Ltd.
+    Copyright (C) 2019-2020 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -95,12 +95,7 @@ int main(int argc, char *argv[])
         "noClean",
         "Do not remove any existing polyMesh/ directory or files"
     );
-    argList::addOption
-    (
-        "dict",
-        "file",
-        "Alternative dictionary for the PDRblockMesh description"
-    );
+    argList::addOption("dict", "file", "Alternative PDRblockMeshDict");
     argList::addOption
     (
         "time",

applications/utilities/mesh/generation/blockMesh/blockMesh.C
@@ -134,12 +134,7 @@ int main(int argc, char *argv[])
         "noClean",
         "Do not remove any existing polyMesh/ directory or files"
     );
-    argList::addOption
-    (
-        "dict",
-        "file",
-        "Alternative dictionary for the blockMesh description"
-    );
+    argList::addOption("dict", "file", "Alternative blockMeshDict");
     argList::addBoolOption
     (
         "sets",

applications/utilities/mesh/generation/extrude/extrudeMesh/extrudeMesh.C
@@ -218,7 +218,7 @@ int main(int argc, char *argv[])
     );
 
     #include "addRegionOption.H"
-    argList::addOption("dict", "file", "Use alternative extrudeMeshDict");
+    argList::addOption("dict", "file", "Alternative extrudeMeshDict");
     #include "setRootCase.H"
     #include "createTimeExtruded.H"
 

commit 6d965d699b2314034da0403adb913e6a5d446f7f

BUG: mergeOrSplitBaffles -dict option ignored (fixes #1759)
- regression from f721b5344f51

applications/utilities/mesh/manipulation/mergeOrSplitBaffles/mergeOrSplitBaffles.C
@@ -286,7 +286,7 @@ int main(int argc, char *argv[])
 
     #include "addOverwriteOption.H"
     #include "addRegionOption.H"
-    argList::addBoolOption
+    argList::addOption
     (
         "dict",
         "file",