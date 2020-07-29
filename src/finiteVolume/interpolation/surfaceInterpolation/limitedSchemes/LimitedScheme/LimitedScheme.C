28eae0573bda1dbb5c1fafb963bd2d918d718af1
sergio <s.ferraris@opencfd.co.uk>
2020-07-29 11:10:41 -0700

    BUG: The option to cache the limiter field was not working
    In fvSolution is possible to cache the limiter. This surface field
    is stored in the database to be read later.
    
    The internal tmp management of the field was not working properly.

--- src/finiteVolume/interpolation/surfaceInterpolation/limitedSchemes/LimitedScheme/LimitedScheme.C
+++ src/finiteVolume/interpolation/surfaceInterpolation/limitedSchemes/LimitedScheme/LimitedScheme.C
@@ -6,6 +6,7 @@
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
     Copyright (C) 2011-2016 OpenFOAM Foundation
+    Copyright (C) 2020 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -177,7 +178,11 @@ Foam::LimitedScheme<Type, Limiter, LimitFunc>::limiter
 
         calcLimiter(phi, limiterField);
 
-        return limiterField;
+        return tmp<surfaceScalarField>::New
+        (
+            limiterFieldName,
+            limiterField
+        );
     }
     else
     {
