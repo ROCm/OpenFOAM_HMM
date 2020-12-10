ZhangYanTJU/OpenFOAM-ESI

ENH: adding generalizedNewtonian to laminar turbulence model
The generalizedNewtonian viscocity models were ported from
the org version and added to the laminar turbulence framework.

This allows its use to compressible and incompressible solvers
through the turbulence dictionary under the laminar sub-dictionary.

The thermal laminar viscosity is taken from the thermo for solvers
that use thermo library or from the transportProperties dictionary
for incompressible solvers.

At the moment the option to include viscocity models through the
transportDict is still available.

The icoTabulated Eq of State was ported from the org version.

src/TurbulenceModels/turbulenceModels/laminar/generalizedNewtonian/generalizedNewtonianViscosityModels/HerschelBulkley/HerschelBulkley.C
@@ -1,0 +1,123 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2018-2020 OpenFOAM Foundation
+    Copyright (C) 2020 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "HerschelBulkley.H"
+#include "volFields.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace laminarModels
+{
+namespace generalizedNewtonianViscosityModels
+{
+    defineTypeNameAndDebug(HerschelBulkley, 0);
+
+    addToRunTimeSelectionTable
+    (
+        generalizedNewtonianViscosityModel,
+        HerschelBulkley,
+        dictionary
+    );
+}
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::laminarModels::generalizedNewtonianViscosityModels::HerschelBulkley::
+HerschelBulkley
+(
+    const dictionary& viscosityProperties
+)
+:
+    generalizedNewtonianViscosityModel(viscosityProperties),
+    HerschelBulkleyCoeffs_
+    (
+        viscosityProperties.optionalSubDict(typeName + "Coeffs")
+    ),
+    k_("k", dimViscosity, HerschelBulkleyCoeffs_),
+    n_("n", dimless, HerschelBulkleyCoeffs_),
+    tau0_("tau0", dimViscosity/dimTime, HerschelBulkleyCoeffs_)
+{
+    read(viscosityProperties);
+}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+bool Foam::laminarModels::generalizedNewtonianViscosityModels::
+HerschelBulkley::read
+(
+    const dictionary& viscosityProperties
+)
+{
+    generalizedNewtonianViscosityModel::read(viscosityProperties);
+
+    HerschelBulkleyCoeffs_ =
+        viscosityProperties.optionalSubDict(typeName + "Coeffs");
+
+    HerschelBulkleyCoeffs_.readEntry("k", k_);
+    HerschelBulkleyCoeffs_.readEntry("n", n_);
+    HerschelBulkleyCoeffs_.readEntry("tau0", tau0_);
+
+    return true;
+}
+
+
+Foam::tmp<Foam::volScalarField>
+Foam::laminarModels::generalizedNewtonianViscosityModels::HerschelBulkley::
+nu
+(
+    const volScalarField& nu0,
+    const volScalarField& strainRate
+) const
+{
+    dimensionedScalar tone("tone", dimTime, 1.0);
+    dimensionedScalar rtone("rtone", dimless/dimTime, 1.0);
+
+    return
+    (
+        min
+        (
+            nu0,
+            (tau0_ + k_*rtone*pow(tone*strainRate, n_))
+           /max
+            (
+                strainRate,
+                dimensionedScalar ("VSMALL", dimless/dimTime, VSMALL)
+            )
+        )
+    );
+}
+
+
+// ************************************************************************* //

ROCm/OpenFOAM_HMM

This allows use in compressible and incompressible solvers
The icoTabulated equation of state was ported from the org version.
STYLE: use 'model' instead of 'laminarModel' in tutorials

src/TurbulenceModels/turbulenceModels/laminar/generalizedNewtonian/generalizedNewtonianViscosityModels/HerschelBulkley/HerschelBulkley.C
@@ -1,0 +1,123 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2018-2020 OpenFOAM Foundation
+    Copyright (C) 2020 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "HerschelBulkley.H"
+#include "volFields.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace laminarModels
+{
+namespace generalizedNewtonianViscosityModels
+{
+    defineTypeNameAndDebug(HerschelBulkley, 0);
+
+    addToRunTimeSelectionTable
+    (
+        generalizedNewtonianViscosityModel,
+        HerschelBulkley,
+        dictionary
+    );
+}
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::laminarModels::generalizedNewtonianViscosityModels::HerschelBulkley::
+HerschelBulkley
+(
+    const dictionary& viscosityProperties
+)
+:
+    generalizedNewtonianViscosityModel(viscosityProperties),
+    HerschelBulkleyCoeffs_
+    (
+        viscosityProperties.optionalSubDict(typeName + "Coeffs")
+    ),
+    k_("k", dimViscosity, HerschelBulkleyCoeffs_),
+    n_("n", dimless, HerschelBulkleyCoeffs_),
+    tau0_("tau0", dimViscosity/dimTime, HerschelBulkleyCoeffs_)
+{
+    read(viscosityProperties);
+}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+bool Foam::laminarModels::generalizedNewtonianViscosityModels::
+HerschelBulkley::read
+(
+    const dictionary& viscosityProperties
+)
+{
+    generalizedNewtonianViscosityModel::read(viscosityProperties);
+
+    HerschelBulkleyCoeffs_ =
+        viscosityProperties.optionalSubDict(typeName + "Coeffs");
+
+    HerschelBulkleyCoeffs_.readEntry("k", k_);
+    HerschelBulkleyCoeffs_.readEntry("n", n_);
+    HerschelBulkleyCoeffs_.readEntry("tau0", tau0_);
+
+    return true;
+}
+
+
+Foam::tmp<Foam::volScalarField>
+Foam::laminarModels::generalizedNewtonianViscosityModels::HerschelBulkley::
+nu
+(
+    const volScalarField& nu0,
+    const volScalarField& strainRate
+) const
+{
+    const dimensionedScalar tone("tone", dimTime, 1);
+    const dimensionedScalar rtone("rtone", dimless/dimTime, 1);
+
+    return
+    (
+        min
+        (
+            nu0,
+            (tau0_ + k_*rtone*pow(tone*strainRate, n_))
+           /max
+            (
+                strainRate,
+                dimensionedScalar("SMALL", dimless/dimTime, VSMALL)
+            )
+        )
+    );
+}
+
+
+// ************************************************************************* //