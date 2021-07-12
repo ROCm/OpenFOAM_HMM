c1c1799051f1eab44b9ed4bb3f6dfb25285dd7d5
sergio <s.ferraris@opencfd.co.uk>
2021-07-12 14:22:08 -0700

    BUG: Caching alphaPhiUn for VoF for scalarTransport FOcin mixture. #2053

--- src/regionModels/pyrolysisModels/reactingOneDim/reactingOneDim.C
+++ src/regionModels/pyrolysisModels/reactingOneDim/reactingOneDim.C
@@ -6,7 +6,7 @@
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
     Copyright (C) 2011-2017 OpenFOAM Foundation
-    Copyright (C) 2016-2020 OpenCFD Ltd.
+    Copyright (C) 2016-2021 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -202,7 +202,6 @@ void reactingOneDim::updateFields()
         updateqr();
     }
 
-    //Note: Commented out as the sensible gas energy is included in energy eq.
     updatePhiGas();
 }
 
