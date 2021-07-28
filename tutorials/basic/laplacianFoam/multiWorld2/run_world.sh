9d26b25c460652156c6c497cc41e9c8d8ddc5d17
Mark Olesen <Mark.Olesen@esi-group.com>
2021-07-28 09:54:19 +0200

    TUT: multi-world tutorial setup with circular connectivity

---
+++ tutorials/basic/laplacianFoam/multiWorld2/run_world.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+world="${1:?specify world/case}"
+application="${2:-laplacianFoam}"
+
+if [ -z "$application" ] || ! command -v "$application" > /dev/null
+then
+    echo "No application: $application"
+    exit 2
+fi
+
+worldCase="$(echo "$world" | tr '[:upper:]' '[:lower:]')"
+# worldName="$(echo "$world" | tr '[:lower:]' '[:upper:]')"
+worldName="$worldCase"
+
+log="log.run_$worldCase"
+
+"$application" -case "$worldCase" -world "$worldName" > "$log" 2>&1
+
+# ----------------------------------------------------------------------------
