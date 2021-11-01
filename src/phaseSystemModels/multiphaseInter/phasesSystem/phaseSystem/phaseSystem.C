cad325f41f7caf139261cb23b1b092cebcc71b3a
Kutalmis Bercin <kutalmis.bercin@esi-group.com>
2021-11-01 10:05:44 +0000

    BUG: icoReactingMultiphaseInterFoam: enable phasesSystem surface tension models (Fixes #2228)

--- src/phaseSystemModels/multiphaseInter/phasesSystem/phaseSystem/phaseSystem.C
+++ src/phaseSystemModels/multiphaseInter/phasesSystem/phaseSystem/phaseSystem.C
@@ -5,7 +5,7 @@
     \\  /    A nd           | www.openfoam.com
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-    Copyright (C) 2017-2020 OpenCFD Ltd.
+    Copyright (C) 2017-2021 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -265,7 +265,6 @@ Foam::phaseSystem::phaseSystem
         generatePairsAndSubModels
         (
             "surfaceTension",
-            mesh_,
             surfaceTensionModels_
         );
     }
