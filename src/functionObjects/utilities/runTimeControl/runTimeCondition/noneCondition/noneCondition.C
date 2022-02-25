ZhangYanTJU/OpenFOAM-ESI

ENH: runTimeControl - added 'none' condition as a no-op

src/functionObjects/utilities/runTimeControl/runTimeCondition/noneCondition/noneCondition.C
@@ -1,0 +1,81 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2022 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "noneCondition.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fieldTypes.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace functionObjects
+{
+namespace runTimeControls
+{
+    defineTypeNameAndDebug(noneCondition, 0);
+    addToRunTimeSelectionTable(runTimeCondition, noneCondition, dictionary);
+
+}
+}
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::functionObjects::runTimeControls::noneCondition::noneCondition
+(
+    const word& name,
+    const objectRegistry& obr,
+    const dictionary& dict,
+    stateFunctionObject& state
+)
+:
+    runTimeCondition(name, obr, dict, state)
+{}
+
+
+// * * * * * * * * * * * * * * Public Member Functions * * * * * * * * * * * //
+
+bool Foam::functionObjects::runTimeControls::noneCondition::apply()
+{
+    return true;
+}
+
+
+void Foam::functionObjects::runTimeControls::noneCondition::write()
+{
+    // do nothing
+}
+
+
+void Foam::functionObjects::runTimeControls::noneCondition::reset()
+{
+    // do nothing
+}
+
+
+// ************************************************************************* //

ROCm/OpenFOAM_HMM

src/functionObjects/utilities/runTimeControl/runTimeCondition/noneCondition/noneCondition.C
@@ -1,0 +1,82 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2022 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "noneCondition.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fieldTypes.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace functionObjects
+{
+namespace runTimeControls
+{
+    defineTypeName(noneCondition, 0);
+    addToRunTimeSelectionTable(runTimeCondition, noneCondition, dictionary);
+
+}
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::functionObjects::runTimeControls::noneCondition::noneCondition
+(
+    const word& name,
+    const objectRegistry& obr,
+    const dictionary& dict,
+    stateFunctionObject& state
+)
+:
+    runTimeCondition(name, obr, dict, state)
+{}
+
+
+// * * * * * * * * * * * * * * Public Member Functions * * * * * * * * * * * //
+
+bool Foam::functionObjects::runTimeControls::noneCondition::apply()
+{
+    return true;
+}
+
+
+void Foam::functionObjects::runTimeControls::noneCondition::write()
+{
+    // do nothing
+}
+
+
+void Foam::functionObjects::runTimeControls::noneCondition::reset()
+{
+    // do nothing
+}
+
+
+// ************************************************************************* //