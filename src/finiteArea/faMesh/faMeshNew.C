730ce92b683ea8dbbecf354ed74d330b967e939f
Mark Olesen <Mark.Olesen@esi-group.com>
2022-03-09 15:58:14 +0100

    ENH: handle try-construct faMesh (#2399)
    - a try/catch approach is not really robust enough (or even possible)
      since read failures likely do not occur on all ranks simultaneously.
      This leads to situations where the master has thrown an exception
      (and thus exiting the current routine) while other ranks are still
      waiting to receive data and the program blocks completely.
    
      Since this primarily affects data conversion routines such as
      foamToEnsight etc, treat similarly to lagrangian: check for the
      existence of essential files before proceeding or not. This is
      wrapped into a TryNew factory method:
    
          autoPtr<faMesh> faMeshPtr(faMesh::TryNew(mesh));
          if (faMeshPtr) ...

---
+++ src/finiteArea/faMesh/faMeshNew.C
@@ -0,0 +1,157 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2022 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "faMesh.H"
+#include "polyMesh.H"
+#include "fileOperation.H"
+
+// * * * * * * * * * * * * * * * * Selectors * * * * * * * * * * * * * * * * //
+
+bool Foam::faMesh::hasSystemFiles(const polyMesh& pMesh)
+{
+    // Expect
+    // - system/faSchemes
+    // - system/faSolution
+
+    const fileOperation& fp = Foam::fileHandler();
+
+    bool looksValid = true;
+
+    // Global files: system/{faSchemes,faSolution}
+    for
+    (
+        const word& expect
+      : List<word>
+        ({
+            {"faSchemes"},
+            {"faSolution"}
+        })
+    )
+    {
+        fileName found
+        (
+            fp.filePath
+            (
+                true,  // global
+                IOobject
+                (
+                    expect,
+                    pMesh.time().system(),
+                    pMesh,
+                    IOobject::MUST_READ,
+                    IOobject::NO_WRITE,
+                    false
+                ),
+                expect // typeName (ununsed?)
+            )
+        );
+
+        if (found.empty())
+        {
+            looksValid = false;
+        }
+    }
+
+    Pstream::broadcast(looksValid);
+
+    return looksValid;
+}
+
+
+bool Foam::faMesh::hasFiles(const polyMesh& pMesh)
+{
+    // As well as system/{faSchemes,faSolution}
+    //
+    // expect these:
+    // - timeValue/faMesh/faBoundary
+    // - timeValue/instance/faMesh/faceLabels
+
+    bool looksValid = hasSystemFiles(pMesh);
+
+    if (looksValid)
+    {
+        const fileOperation& fp = Foam::fileHandler();
+
+        fileName subDir(pMesh.dbDir()/faMesh::meshSubDir);
+
+        for
+        (
+            const wordPair& expect
+          : List<wordPair>
+            ({
+                {"faBoundary", "faBoundaryMesh"},
+                {"faceLabels", "labelList"}
+            })
+        )
+        {
+            const word& dataFile = expect.first();
+            const word& dataClass = expect.second();
+
+            fileName found
+            (
+                fp.filePath
+                (
+                    false,  // non-global
+                    IOobject
+                    (
+                        dataFile,
+                        pMesh.time().findInstance(subDir, dataFile),
+                        faMesh::meshSubDir,
+                        pMesh,
+                        IOobject::MUST_READ,
+                        IOobject::NO_WRITE,
+                        false
+                    ),
+                    dataClass // typeName (ununsed?)
+                )
+            );
+
+            if (found.empty())
+            {
+                looksValid = false;
+            }
+        }
+
+        Pstream::broadcast(looksValid);
+    }
+
+    return looksValid;
+}
+
+
+Foam::autoPtr<Foam::faMesh> Foam::faMesh::TryNew(const polyMesh& pMesh)
+{
+    if (faMesh::hasFiles(pMesh))
+    {
+        return autoPtr<faMesh>::New(pMesh);
+    }
+
+    return nullptr;
+}
+
+
+// ************************************************************************* //
