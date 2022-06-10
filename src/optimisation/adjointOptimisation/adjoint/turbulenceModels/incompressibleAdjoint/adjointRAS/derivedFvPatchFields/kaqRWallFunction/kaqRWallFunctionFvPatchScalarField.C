jiban/openform

ENH: added the adjoint to the kOmega SST turbulence model
for incompressible flows. The typical convention of appending the primal
field name with 'a' to form the adjoint field is followed for the
adjoint turbulent kinetic energy (i.e. 'ka') but since this would produce
an ugly variable name for the adjoint to omega (i.e. omegaa), the latter
is abbreviated to 'wa'.

The work is based on

\verbatim
    Kavvadias, I., Papoutsis-Kiachagias, E.,
    Dimitrakopoulos, G., & Giannakoglou, K.  (2014).
    The continuous adjoint approach to the k–$omega$ SST turbulence model with
    applications in shape optimization
    Engineering Optimization, 47(11), 1523-1542.
    https://doi.org/10.1080/0305215X.2014.979816
\endverbatim

with changes in the discretisation of
a number of differential operators and the formulation of the adjoint to
the wall functions employed by the primal model.

Regarding the latter, the code assumes (and differentiates) the default
behaviour of nutkWallFunction (i.e. nutWallFunction::blendingType::STEPWISE)
and omegaWallFunction (i.e. omegaWallFunction::blendingType::BINOMIAL2).

Due to the availability of a number of terms required for the
formulation of the wall function for ka, the latter is implemented
within adjointkOmegaSST itself, with contributions from objective functions
implemented within kaqRWallFunction. Wall functions for wa are
implemented within waWallFunction.

The initial implementation of the above-mentioned reference was
performed by Dr. Ioannis Kavvadias

src/optimisation/adjointOptimisation/adjoint/turbulenceModels/incompressibleAdjoint/adjointRAS/derivedFvPatchFields/kaqRWallFunction/kaqRWallFunctionFvPatchScalarField.C
@@ -1,0 +1,175 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2014-2022 PCOpt/NTUA
+    Copyright (C) 2014-2022 FOSS GP
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "kaqRWallFunctionFvPatchScalarField.H"
+#include "fvPatchFieldMapper.H"
+#include "nutkWallFunctionFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::kaqRWallFunctionFvPatchScalarField::kaqRWallFunctionFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    kqRWallFunctionFvPatchField<scalar>(p, iF),
+    adjointScalarBoundaryCondition(p, iF, word::null)
+{}
+
+
+Foam::kaqRWallFunctionFvPatchScalarField::kaqRWallFunctionFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    kqRWallFunctionFvPatchField<scalar>(p, iF, dict),
+    adjointScalarBoundaryCondition(p, iF, dict.get<word>("solverName"))
+{}
+
+
+Foam::kaqRWallFunctionFvPatchScalarField::kaqRWallFunctionFvPatchScalarField
+(
+    const kaqRWallFunctionFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    kqRWallFunctionFvPatchField<scalar>(ptf, p, iF, mapper),
+    adjointScalarBoundaryCondition(p, iF, ptf.adjointSolverName_)
+{}
+
+
+Foam::kaqRWallFunctionFvPatchScalarField::kaqRWallFunctionFvPatchScalarField
+(
+    const kaqRWallFunctionFvPatchScalarField& tkqrwfpf
+)
+:
+    kqRWallFunctionFvPatchField<scalar>(tkqrwfpf),
+    adjointScalarBoundaryCondition(tkqrwfpf)
+{}
+
+
+Foam::kaqRWallFunctionFvPatchScalarField::kaqRWallFunctionFvPatchScalarField
+(
+    const kaqRWallFunctionFvPatchScalarField& tkqrwfpf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    kqRWallFunctionFvPatchField<scalar>(tkqrwfpf, iF),
+    adjointScalarBoundaryCondition(tkqrwfpf)
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::kaqRWallFunctionFvPatchScalarField::manipulateMatrix
+(
+    fvMatrix<scalar>& matrix
+)
+{
+    scalarField& source = matrix.source();
+    tmp<fvPatchScalarField> tnutWall(boundaryContrPtr_->turbulentDiffusivity());
+    fvPatchScalarField& nutWall = tnutWall.constCast();
+    if (isA<nutkWallFunctionFvPatchScalarField>(nutWall))
+    {
+        const label patchi(patch().index());
+        const scalarField& magSf = patch().magSf();
+        const turbulenceModel& turbModel = db().lookupObject<turbulenceModel>
+        (
+            IOobject::groupName
+            (
+                turbulenceModel::propertiesName,
+                internalField().group()
+            )
+        );
+        const scalarField& y = turbModel.y()[patchi];
+        const tmp<scalarField> tnuw = turbModel.nu(patchi);
+        const scalarField& nuw = tnuw();
+        const nutWallFunctionFvPatchScalarField& nutWF =
+            refCast<nutWallFunctionFvPatchScalarField>(nutWall);
+        const scalar Cmu = nutWF.Cmu();
+        const scalar kappa = nutWF.kappa();
+        const scalar E = nutWF.E();
+        const scalar yPlusLam = nutWF.yPlusLam();
+
+        const scalar Cmu25 = pow025(Cmu);
+
+        const labelList& faceCells = patch().faceCells();
+        const fvPatchVectorField& Uw = boundaryContrPtr_->Ub();
+        const scalarField magGradUw(mag(Uw.snGrad()));
+
+        tmp<scalarField> tdJdnut = boundaryContrPtr_->dJdnut();
+        const scalarField& dJdnut = tdJdnut();
+
+        const tmp<volScalarField> tk = turbModel.k();
+        const volScalarField& k = tk();
+        forAll(dJdnut, facei)
+        {
+            const label celli = faceCells[facei];
+
+            const scalar sqrtkCell(sqrt(k[celli]));
+            const scalar yPlus = Cmu25*y[facei]*sqrtkCell/nuw[facei];
+            if (yPlusLam < yPlus)
+            {
+                const scalar logEyPlus = log(E*yPlus);
+                const scalar dnut_dyPlus =
+                        nuw[facei]*kappa*(logEyPlus - 1)/sqr(logEyPlus);
+                const scalar dyPlus_dk =
+                    Cmu25*y[facei]/(2*nuw[facei]*sqrtkCell);
+                const scalar dnut_dk = dnut_dyPlus*dyPlus_dk;
+                source[celli] -= dJdnut[facei]*dnut_dk*magSf[facei];
+            }
+        }
+    }
+}
+
+
+void Foam::kaqRWallFunctionFvPatchScalarField::write(Ostream& os) const
+{
+    kqRWallFunctionFvPatchField<scalar>::write(os);
+    os.writeEntry("solverName", adjointSolverName_);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    makePatchTypeField
+    (
+        fvPatchScalarField,
+        kaqRWallFunctionFvPatchScalarField
+    );
+}
+
+
+// ************************************************************************* //

ROCm/OpenFOAM_HMM

src/optimisation/adjointOptimisation/adjoint/turbulenceModels/incompressibleAdjoint/adjointRAS/derivedFvPatchFields/kaqRWallFunction/kaqRWallFunctionFvPatchScalarField.C
@@ -1,0 +1,176 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2014-2022 PCOpt/NTUA
+    Copyright (C) 2014-2022 FOSS GP
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "kaqRWallFunctionFvPatchScalarField.H"
+#include "fvPatchFieldMapper.H"
+#include "nutkWallFunctionFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::kaqRWallFunctionFvPatchScalarField::kaqRWallFunctionFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    kqRWallFunctionFvPatchField<scalar>(p, iF),
+    adjointScalarBoundaryCondition(p, iF, word::null)
+{}
+
+
+Foam::kaqRWallFunctionFvPatchScalarField::kaqRWallFunctionFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    kqRWallFunctionFvPatchField<scalar>(p, iF, dict),
+    adjointScalarBoundaryCondition(p, iF, dict.get<word>("solverName"))
+{}
+
+
+Foam::kaqRWallFunctionFvPatchScalarField::kaqRWallFunctionFvPatchScalarField
+(
+    const kaqRWallFunctionFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    kqRWallFunctionFvPatchField<scalar>(ptf, p, iF, mapper),
+    adjointScalarBoundaryCondition(p, iF, ptf.adjointSolverName_)
+{}
+
+
+Foam::kaqRWallFunctionFvPatchScalarField::kaqRWallFunctionFvPatchScalarField
+(
+    const kaqRWallFunctionFvPatchScalarField& tkqrwfpf
+)
+:
+    kqRWallFunctionFvPatchField<scalar>(tkqrwfpf),
+    adjointScalarBoundaryCondition(tkqrwfpf)
+{}
+
+
+Foam::kaqRWallFunctionFvPatchScalarField::kaqRWallFunctionFvPatchScalarField
+(
+    const kaqRWallFunctionFvPatchScalarField& tkqrwfpf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    kqRWallFunctionFvPatchField<scalar>(tkqrwfpf, iF),
+    adjointScalarBoundaryCondition(tkqrwfpf)
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::kaqRWallFunctionFvPatchScalarField::manipulateMatrix
+(
+    fvMatrix<scalar>& matrix
+)
+{
+    scalarField& source = matrix.source();
+    tmp<fvPatchScalarField> tnutWall(boundaryContrPtr_->turbulentDiffusivity());
+    fvPatchScalarField& nutWall = tnutWall.constCast();
+    if (isA<nutkWallFunctionFvPatchScalarField>(nutWall))
+    {
+        const label patchi(patch().index());
+        const scalarField& magSf = patch().magSf();
+        const turbulenceModel& turbModel = db().lookupObject<turbulenceModel>
+        (
+            IOobject::groupName
+            (
+                turbulenceModel::propertiesName,
+                internalField().group()
+            )
+        );
+        const scalarField& y = turbModel.y()[patchi];
+        const tmp<scalarField> tnuw = turbModel.nu(patchi);
+        const scalarField& nuw = tnuw();
+        const nutWallFunctionFvPatchScalarField& nutWF =
+            refCast<nutWallFunctionFvPatchScalarField>(nutWall);
+        const wallFunctionCoefficients& wallCoeffs = nutWF.wallCoeffs();
+        const scalar Cmu = wallCoeffs.Cmu();
+        const scalar kappa = wallCoeffs.kappa();
+        const scalar E = wallCoeffs.E();
+        const scalar yPlusLam = wallCoeffs.yPlusLam();
+
+        const scalar Cmu25 = pow025(Cmu);
+
+        const labelList& faceCells = patch().faceCells();
+        const fvPatchVectorField& Uw = boundaryContrPtr_->Ub();
+        const scalarField magGradUw(mag(Uw.snGrad()));
+
+        tmp<scalarField> tdJdnut = boundaryContrPtr_->dJdnut();
+        const scalarField& dJdnut = tdJdnut();
+
+        const tmp<volScalarField> tk = turbModel.k();
+        const volScalarField& k = tk();
+        forAll(dJdnut, facei)
+        {
+            const label celli = faceCells[facei];
+
+            const scalar sqrtkCell(sqrt(k[celli]));
+            const scalar yPlus = Cmu25*y[facei]*sqrtkCell/nuw[facei];
+            if (yPlusLam < yPlus)
+            {
+                const scalar logEyPlus = log(E*yPlus);
+                const scalar dnut_dyPlus =
+                        nuw[facei]*kappa*(logEyPlus - 1)/sqr(logEyPlus);
+                const scalar dyPlus_dk =
+                    Cmu25*y[facei]/(2*nuw[facei]*sqrtkCell);
+                const scalar dnut_dk = dnut_dyPlus*dyPlus_dk;
+                source[celli] -= dJdnut[facei]*dnut_dk*magSf[facei];
+            }
+        }
+    }
+}
+
+
+void Foam::kaqRWallFunctionFvPatchScalarField::write(Ostream& os) const
+{
+    kqRWallFunctionFvPatchField<scalar>::write(os);
+    os.writeEntry("solverName", adjointSolverName_);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    makePatchTypeField
+    (
+        fvPatchScalarField,
+        kaqRWallFunctionFvPatchScalarField
+    );
+}
+
+
+// ************************************************************************* //