16e3c11bffc86c12e5579ca5d81c269688205106
leopold-grinberg <71348444+leopold-grinberg@users.noreply.github.com>
2022-10-25 21:39:28 -0400

    Update MEMO_SETUP.sh
    generalizing PATH to ROCM/AFAR and MPI installations

--- scripts/MEMO_SETUP.sh
+++ scripts/MEMO_SETUP.sh
@@ -1,25 +1,21 @@
 export HSA_XNACK=1
 export OMP_NUM_THREADS=2
 
+export ROCM4FOAM=/global/software/rocm-afar001-732
+export MPI4FOAM=/global/software/openmpi/rocm-afar001/openmpi-5.0.0rc2
+
+
+export PATH=${MPI4FOAM}/build/bin:${MPI4FOAM}/build/lib:$PATH
+export LIBRARY_PATH=${MPI4FOAM}/build/lib:$LIBRARY_PATH
+
+export LD_LIBRARY_PATH=${ROCM4FOAM}/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=${ROCM4FOAM}/lib:$LIBRARY_PATH
+
+#for profiler
+export PATH=${ROCM4FOAM}/bin:$PATH
+
 source etc/bashrc
 
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/work/lgrinber/rocm-afar001-460/llvm/lib
-export PATH=/work/lgrinber/rocm-afar001-460/llvm/bin:$PATH
-
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/work/lgrinber/rocm-afar001-432/llvm/lib
-#export PATH=/work/lgrinber/rocm-afar001-432/llvm/bin:$PATH
-#export PATH=/work/lgrinber/MPI_CLANG/openmpi-5.0.0rc2/build/bin:$PATH
-
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/work/lgrinber/OPENMP_COMPILER_UVM/aomp_15.0-1/lib
-#export PATH=/work/lgrinber/OPENMP_COMPILER_UVM/aomp_15.0-1/bin:/work/lgrinber/OPENMP_COMPILER_UVM/aomp_15.0-1/include:$PATH
-#export PATH=/work/lgrinber/MPI_CLANG/openmpi-5.0.0rc2/build/bin:$PATH
-
-
-export PATH=$PATH:/work/lgrinber/MPI_CLANG/openmpi-5.0.0rc2/build/bin:/work/lgrinber/MPI_CLANG/openmpi-5.0.0rc2/build/lib
-export LIBRARY_PATH=/work/lgrinber/MPI_CLANG/openmpi-5.0.0rc2/build/lib:$LIBRARY_PATH
-
-#if fails to execute try:
-#export LD_LIBRARY_PATH=/opt/rocm-5.0.2/hsa/lib:$LD_LIBRARY_PATH
 
 
