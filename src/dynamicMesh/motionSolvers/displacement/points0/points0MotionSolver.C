18216a463953548a46e235bfe436ba7748333c83
mattijs <mattijs>
2022-11-08 12:38:44 +0000

    BUG: zoneMotion: supply optional coeffs dict. Fixes #2630

--- src/dynamicMesh/motionSolvers/displacement/points0/points0MotionSolver.C
+++ src/dynamicMesh/motionSolvers/displacement/points0/points0MotionSolver.C
@@ -6,7 +6,7 @@
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
     Copyright (C) 2012-2017 OpenFOAM Foundation
-    Copyright (C) 2015-2021 OpenCFD Ltd.
+    Copyright (C) 2015-2022 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -84,7 +84,7 @@ Foam::points0MotionSolver::points0MotionSolver
 )
 :
     motionSolver(mesh, dict, type),
-    zoneMotion(dict, mesh),
+    zoneMotion(coeffDict(), mesh),
     points0_(points0IO(mesh))
 {
     if
@@ -128,7 +128,7 @@ Foam::points0MotionSolver::points0MotionSolver
 )
 :
     motionSolver(mesh, dict, type),
-    zoneMotion(dict, mesh),
+    zoneMotion(coeffDict(), mesh),
     points0_(points0)
 {
     if (points0_.size() != mesh.nPoints())
