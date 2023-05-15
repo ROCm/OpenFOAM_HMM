d92583f4b6c69e8608a3952be6a1f8b9e89c5afa
mattijs <mattijs>
2023-05-15 16:17:27 +0100

    BUG: extrudePatchMesh: disable walking across non-manifold. See #2780
    This code can be removed if nInternalEdges excludes non-manifold ones

--- src/dynamicMesh/extrudePatchMesh/extrudePatchMesh.C
+++ src/dynamicMesh/extrudePatchMesh/extrudePatchMesh.C
@@ -6,7 +6,7 @@
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
     Copyright (C) 2011-2016 OpenFOAM Foundation
-    Copyright (C) 2020-2022 OpenCFD Ltd.
+    Copyright (C) 2020-2023 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -163,6 +163,12 @@ void Foam::extrudePatchMesh::extrudeMesh(polyPatchList& regionPatches)
         {
             if (columnCells)
             {
+                nonManifoldEdge.set(edgeI);
+            }
+            else if (extrudedPatch_.edgeFaces()[edgeI].size() > 2)
+            {
+                // TBD: issue #2780 : non-manifold edges get seen as internal
+                // This bit of code can be removed once #2780 is solved.
                 nonManifoldEdge.set(edgeI);
             }
         }
