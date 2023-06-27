ROCm/OpenFOAM_HMM

commit fb69a54bc309f5893e2bb49de4741749b8d51c0e

ENH: avoid prior communication when using mapDistribute
- in most cases can simply construct mapDistribute with the sendMap
  and have it take care of communication and addressing for the
  corresponding constructMap.

  This removes code duplication, which in some cases was also using
  much less efficient mechanisms (eg, combineReduce on list of
  lists, or an allGatherList on the send sizes etc) and also
  reduces the number of places where Pstream::exchange/exchangeSizes
  is being called.

ENH: reduce communication in turbulentDFSEMInlet

- was doing an allGatherList to populate a mapDistribute.
  Now simply use PstreamBuffers mechanisms directly.

src/sampling/meshToMesh/meshToMeshParallelOps.C
@@ -6,7 +6,7 @@
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
     Copyright (C) 2012-2017 OpenFOAM Foundation
-    Copyright (C) 2015-2022 OpenCFD Ltd.
+    Copyright (C) 2015-2023 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -232,42 +232,7 @@ Foam::autoPtr<Foam::mapDistribute> Foam::meshToMesh::calcProcMap
                 }
             }
 
-
-            // send over how many tgt cells I need to receive from each
-            // processor
-            labelListList sendSizes(Pstream::nProcs());
-            sendSizes[Pstream::myProcNo()].resize(Pstream::nProcs());
-            forAll(sendMap, proci)
-            {
-                sendSizes[Pstream::myProcNo()][proci] = sendMap[proci].size();
-            }
-            Pstream::allGatherList(sendSizes);
-
-
-            // determine order of receiving
-            labelListList constructMap(Pstream::nProcs());
-
-            label segmentI = 0;
-            forAll(constructMap, proci)
-            {
-                // what I need to receive is what other processor is sending
-                // to me
-                label nRecv = sendSizes[proci][Pstream::myProcNo()];
-                constructMap[proci].setSize(nRecv);
-
-                for (label i = 0; i < nRecv; i++)
-                {
-                    constructMap[proci][i] = segmentI++;
-                }
-            }
-
-            return autoPtr<mapDistribute>::New
-            (
-                segmentI,       // size after construction
-                std::move(sendMap),
-                std::move(constructMap)
-            );
-
+            return autoPtr<mapDistribute>::New(std::move(sendMap));
             break;
         }
     }

applications/test/parallel/Test-parallel.C
@@ -67,54 +67,24 @@ void testMapDistribute()
     labelList nSend(Pstream::nProcs(), Zero);
     forAll(complexData, i)
     {
-        const label procI = complexData[i].first();
-        nSend[procI]++;
+        const label proci = complexData[i].first();
+        nSend[proci]++;
     }
 
     // Collect items to be sent
     labelListList sendMap(Pstream::nProcs());
-    forAll(sendMap, procI)
+    forAll(sendMap, proci)
     {
-        sendMap[procI].setSize(nSend[procI]);
+        sendMap[proci].resize_nocopy(nSend[proci]);
+        nSend[proci] = 0;
     }
-    nSend = 0;
     forAll(complexData, i)
     {
-        const label procI = complexData[i].first();
-        sendMap[procI][nSend[procI]++] = i;
+        const label proci = complexData[i].first();
+        sendMap[proci][nSend[proci]++] = i;
     }
 
-    // Sync how many to send
-    labelList nRecv;
-    Pstream::exchangeSizes(sendMap, nRecv);
-
-    // Collect items to be received
-    labelListList recvMap(Pstream::nProcs());
-    forAll(recvMap, procI)
-    {
-        recvMap[procI].setSize(nRecv[procI]);
-    }
-
-    label constructSize = 0;
-    // Construct with my own elements first
-    forAll(recvMap[Pstream::myProcNo()], i)
-    {
-        recvMap[Pstream::myProcNo()][i] = constructSize++;
-    }
-    // Construct from other processors
-    forAll(recvMap, procI)
-    {
-        if (procI != Pstream::myProcNo())
-        {
-            forAll(recvMap[procI], i)
-            {
-                recvMap[procI][i] = constructSize++;
-            }
-        }
-    }
-
-    // Construct distribute map (destructively)
-    mapDistribute map(constructSize, std::move(sendMap), std::move(recvMap));
+    mapDistribute map(std::move(sendMap));
 
     // Distribute complexData
     map.distribute(complexData);

applications/utilities/mesh/generation/foamyMesh/conformalVoronoiMesh/DelaunayMesh/DistributedDelaunayMesh.C
@@ -54,7 +54,6 @@ Foam::DistributedDelaunayMesh<Triangulation>::buildMap
     forAll(toProc, i)
     {
         label proci = toProc[i];
-
         nSend[proci]++;
     }
 
@@ -64,8 +63,7 @@ Foam::DistributedDelaunayMesh<Triangulation>::buildMap
 
     forAll(nSend, proci)
     {
-        sendMap[proci].setSize(nSend[proci]);
-
+        sendMap[proci].resize_nocopy(nSend[proci]);
         nSend[proci] = 0;
     }
 
@@ -73,49 +71,10 @@ Foam::DistributedDelaunayMesh<Triangulation>::buildMap
     forAll(toProc, i)
     {
         label proci = toProc[i];
-
         sendMap[proci][nSend[proci]++] = i;
     }
 
-    // 4. Send over how many I need to receive
-    labelList recvSizes;
-    Pstream::exchangeSizes(sendMap, recvSizes);
-
-
-    // Determine receive map
-    // ~~~~~~~~~~~~~~~~~~~~~
-
-    labelListList constructMap(Pstream::nProcs());
-
-    // Local transfers first
-    constructMap[Pstream::myProcNo()] = identity
-    (
-        sendMap[Pstream::myProcNo()].size()
-    );
-
-    label constructSize = constructMap[Pstream::myProcNo()].size();
-
-    forAll(constructMap, proci)
-    {
-        if (proci != Pstream::myProcNo())
-        {
-            label nRecv = recvSizes[proci];
-
-            constructMap[proci].setSize(nRecv);
-
-            for (label i = 0; i < nRecv; i++)
-            {
-                constructMap[proci][i] = constructSize++;
-            }
-        }
-    }
-
-    return autoPtr<mapDistribute>::New
-    (
-        constructSize,
-        std::move(sendMap),
-        std::move(constructMap)
-    );
+    return autoPtr<mapDistribute>::New(std::move(sendMap));
 }
 
 

applications/utilities/mesh/generation/foamyMesh/conformalVoronoiMesh/backgroundMeshDecomposition/backgroundMeshDecomposition.C
@@ -59,7 +59,6 @@ Foam::autoPtr<Foam::mapDistribute> Foam::backgroundMeshDecomposition::buildMap
     forAll(toProc, i)
     {
         label proci = toProc[i];
-
         nSend[proci]++;
     }
 
@@ -69,8 +68,7 @@ Foam::autoPtr<Foam::mapDistribute> Foam::backgroundMeshDecomposition::buildMap
 
     forAll(nSend, proci)
     {
-        sendMap[proci].setSize(nSend[proci]);
-
+        sendMap[proci].resize_nocopy(nSend[proci]);
         nSend[proci] = 0;
     }
 
@@ -78,49 +76,10 @@ Foam::autoPtr<Foam::mapDistribute> Foam::backgroundMeshDecomposition::buildMap
     forAll(toProc, i)
     {
         label proci = toProc[i];
-
         sendMap[proci][nSend[proci]++] = i;
     }
 
-    // 4. Send over how many I need to receive
-    labelList recvSizes;
-    Pstream::exchangeSizes(sendMap, recvSizes);
-
-
-    // Determine receive map
-    // ~~~~~~~~~~~~~~~~~~~~~
-
-    labelListList constructMap(Pstream::nProcs());
-
-    // Local transfers first
-    constructMap[Pstream::myProcNo()] = identity
-    (
-        sendMap[Pstream::myProcNo()].size()
-    );
-
-    label constructSize = constructMap[Pstream::myProcNo()].size();
-
-    forAll(constructMap, proci)
-    {
-        if (proci != Pstream::myProcNo())
-        {
-            label nRecv = recvSizes[proci];
-
-            constructMap[proci].setSize(nRecv);
-
-            for (label i = 0; i < nRecv; i++)
-            {
-                constructMap[proci][i] = constructSize++;
-            }
-        }
-    }
-
-    return autoPtr<mapDistribute>::New
-    (
-        constructSize,
-        std::move(sendMap),
-        std::move(constructMap)
-    );
+    return autoPtr<mapDistribute>::New(std::move(sendMap));
 }
 
 

applications/utilities/parallelProcessing/redistributePar/parLagrangianDistributor.C
@@ -146,7 +146,6 @@ Foam::parLagrangianDistributor::distributeLagrangianPositions
 
     labelListList subMap;
 
-
     // Allocate transfer buffers
     PstreamBuffers pBufs(Pstream::commsTypes::nonBlocking);
 
@@ -283,32 +282,23 @@ Foam::parLagrangianDistributor::distributeLagrangianPositions
         lpi.rename(cloudName);
     }
 
-    // Work the send indices (subMap) into a mapDistributeBase
-    labelListList sizes(Pstream::nProcs());
-    labelList& nsTransPs = sizes[Pstream::myProcNo()];
-    nsTransPs.setSize(Pstream::nProcs());
-    forAll(subMap, sendProcI)
-    {
-        nsTransPs[sendProcI] = subMap[sendProcI].size();
-    }
-    // Send sizes across. Note: blocks.
-    Pstream::combineReduce(sizes, Pstream::listEq());
+    // Until now (FEB-2023) we have always used processor ordering for the
+    // construct map (whereas mapDistribute has local transfers first),
+    // so we'll stick with that for now, but can likely just use the subMap
+    // directly with mapDistribute and have it determine the constructMap.
 
-    labelListList constructMap(Pstream::nProcs());
+    labelList recvSizes;
+    Pstream::exchangeSizes(subMap, recvSizes);
+
     label constructSize = 0;
-    forAll(constructMap, procI)
+    labelListList constructMap(Pstream::nProcs());
+
+    forAll(constructMap, proci)
     {
-        const label nRecv = sizes[procI][UPstream::myProcNo()];
-
-        labelList& map = constructMap[procI];
-
-        map.setSize(nRecv);
-        forAll(map, i)
-        {
-            map[i] = constructSize++;
-        }
+        const label len = recvSizes[proci];
+        constructMap[proci] = identity(len, constructSize);
+        constructSize += len;
     }
-
 
     return autoPtr<mapDistributeBase>::New
     (

src/finiteVolume/fields/fvPatchFields/derived/turbulentDFSEMInlet/turbulentDFSEMInletFvPatchVectorField.C
@@ -450,13 +450,16 @@ void Foam::turbulentDFSEMInletFvPatchVectorField::convectEddies
         }
     }
 
-    reduce(nRecycled, sumOp<label>());
-
-    if (debug && nRecycled > 0)
+    if (debug)
     {
-        Info<< "Patch: " << patch().patch().name()
-            << " recycled " << nRecycled << " eddies"
-            << endl;
+        reduce(nRecycled, sumOp<label>());
+
+        if (nRecycled)
+        {
+            Info<< "Patch: " << patch().patch().name()
+                << " recycled " << nRecycled << " eddies"
+                << endl;
+        }
     }
 }
 
@@ -492,11 +495,11 @@ void Foam::turbulentDFSEMInletFvPatchVectorField::calcOverlappingProcEddies
     Pstream::allGatherList(patchBBs);
 
     // Per processor indices into all segments to send
-    List<DynamicList<label>> dynSendMap(Pstream::nProcs());
+    List<DynamicList<label>> sendMap(UPstream::nProcs());
 
+    // Collect overlapping eddies
     forAll(eddies_, i)
     {
-        // Collect overlapping eddies
         const eddy& e = eddies_[i];
 
         // Eddy bounds
@@ -505,91 +508,41 @@ void Foam::turbulentDFSEMInletFvPatchVectorField::calcOverlappingProcEddies
         ebb.min() += x;
         ebb.max() += x;
 
-        forAll(patchBBs, procI)
+        forAll(patchBBs, proci)
         {
             // Not including intersection with local patch
-            if (procI != Pstream::myProcNo())
+            if (proci != Pstream::myProcNo())
             {
-                if (ebb.overlaps(patchBBs[procI]))
+                if (ebb.overlaps(patchBBs[proci]))
                 {
-                    dynSendMap[procI].append(i);
+                    sendMap[proci].push_back(i);
                 }
             }
         }
     }
 
-    labelListList sendMap(Pstream::nProcs());
-    forAll(sendMap, procI)
+
+    PstreamBuffers pBufs(UPstream::commsTypes::nonBlocking);
+
+    forAll(sendMap, proci)
     {
-        sendMap[procI].transfer(dynSendMap[procI]);
-    }
-
-    // Send the number of eddies for local processors to receive
-    labelListList sendSizes(Pstream::nProcs());
-    sendSizes[Pstream::myProcNo()].setSize(Pstream::nProcs());
-    forAll(sendMap, procI)
-    {
-        sendSizes[Pstream::myProcNo()][procI] = sendMap[procI].size();
-    }
-    Pstream::allGatherList(sendSizes);
-
-    // Determine order of receiving
-    labelListList constructMap(Pstream::nProcs());
-
-    // Local segment first
-    constructMap[Pstream::myProcNo()] = identity
-    (
-        sendMap[Pstream::myProcNo()].size()
-    );
-
-    label segmentI = constructMap[Pstream::myProcNo()].size();
-    forAll(constructMap, procI)
-    {
-        if (procI != Pstream::myProcNo())
+        if (proci != Pstream::myProcNo() && !sendMap[proci].empty())
         {
-            // What I need to receive is what other processor is sending to me
-            const label nRecv = sendSizes[procI][Pstream::myProcNo()];
-            constructMap[procI].setSize(nRecv);
+            UOPstream os(proci, pBufs);
 
-            for (label i = 0; i < nRecv; ++i)
-            {
-                constructMap[procI][i] = segmentI++;
-            }
+            os << UIndirectList<eddy>(eddies_, sendMap[proci]);
         }
     }
 
-    mapDistribute map(segmentI, std::move(sendMap), std::move(constructMap));
-
-    PstreamBuffers pBufs(Pstream::commsTypes::nonBlocking);
-
-    for (const int domain : Pstream::allProcs())
-    {
-        const labelList& sendElems = map.subMap()[domain];
-
-        if (domain != Pstream::myProcNo() && sendElems.size())
-        {
-            List<eddy> subEddies(UIndirectList<eddy>(eddies_, sendElems));
-
-            UOPstream toDomain(domain, pBufs);
-
-            toDomain<< subEddies;
-        }
-    }
-
-    // Start receiving
     pBufs.finishedSends();
 
-    // Consume
-    for (const int domain : Pstream::allProcs())
+    for (const int proci : pBufs.allProcs())
     {
-        const labelList& recvElems = map.constructMap()[domain];
-
-        if (domain != Pstream::myProcNo() && recvElems.size())
+        if (proci != Pstream::myProcNo() && pBufs.recvDataCount(proci))
         {
-            UIPstream str(domain, pBufs);
-            {
-                str >> overlappingEddies[domain];
-            }
+            UIPstream is(proci, pBufs);
+
+            is >> overlappingEddies[proci];
         }
     }
 
@@ -1001,10 +954,8 @@ void Foam::turbulentDFSEMInletFvPatchVectorField::updateCoeffs()
             List<List<eddy>> overlappingEddies(Pstream::nProcs());
             calcOverlappingProcEddies(overlappingEddies);
 
-            forAll(overlappingEddies, procI)
+            for (const List<eddy>& eddies : overlappingEddies)
             {
-                const List<eddy>& eddies = overlappingEddies[procI];
-
                 if (eddies.size())
                 {
                     forAll(U, faceI)

src/lagrangian/basic/InteractionLists/InteractionLists.C
@@ -822,8 +822,7 @@ void Foam::InteractionLists<ParticleType>::buildMap
 
     forAll(nSend, proci)
     {
-        sendMap[proci].setSize(nSend[proci]);
-
+        sendMap[proci].resize_nocopy(nSend[proci]);
         nSend[proci] = 0;
     }
 
@@ -831,52 +830,10 @@ void Foam::InteractionLists<ParticleType>::buildMap
     forAll(toProc, i)
     {
         label proci = toProc[i];
-
         sendMap[proci][nSend[proci]++] = i;
     }
 
-    // 4. Send over how many I need to receive
-    labelList recvSizes;
-    Pstream::exchangeSizes(sendMap, recvSizes);
-
-
-    // Determine receive map
-    // ~~~~~~~~~~~~~~~~~~~~~
-
-    labelListList constructMap(Pstream::nProcs());
-
-    // Local transfers first
-    constructMap[Pstream::myProcNo()] = identity
-    (
-        sendMap[Pstream::myProcNo()].size()
-    );
-
-    label constructSize = constructMap[Pstream::myProcNo()].size();
-
-    forAll(constructMap, proci)
-    {
-        if (proci != Pstream::myProcNo())
-        {
-            const label nRecv = recvSizes[proci];
-
-            constructMap[proci].setSize(nRecv);
-
-            for (label i = 0; i < nRecv; i++)
-            {
-                constructMap[proci][i] = constructSize++;
-            }
-        }
-    }
-
-    mapPtr.reset
-    (
-        new mapDistribute
-        (
-            constructSize,
-            std::move(sendMap),
-            std::move(constructMap)
-        )
-    );
+    mapPtr.reset(new mapDistribute(std::move(sendMap)));
 }
 
 

src/meshTools/AMIInterpolation/AMIInterpolation/advancingFrontAMI/advancingFrontAMIParallelOps.C
@@ -344,48 +344,7 @@ Foam::autoPtr<Foam::mapDistribute> Foam::advancingFrontAMI::calcProcMap
         }
     }
 
-
-    // Send over how many faces I need to receive
-    labelListList sendSizes(Pstream::nProcs());
-    sendSizes[Pstream::myProcNo()].setSize(Pstream::nProcs());
-    forAll(sendMap, proci)
-    {
-        sendSizes[Pstream::myProcNo()][proci] = sendMap[proci].size();
-    }
-    Pstream::allGatherList(sendSizes);
-
-
-    // Determine order of receiving
-    labelListList constructMap(Pstream::nProcs());
-
-    // My local segment first
-    constructMap[Pstream::myProcNo()] = identity
-    (
-        sendMap[Pstream::myProcNo()].size()
-    );
-
-    label segmentI = constructMap[Pstream::myProcNo()].size();
-    forAll(constructMap, proci)
-    {
-        if (proci != Pstream::myProcNo())
-        {
-            // What I need to receive is what other processor is sending to me
-            label nRecv = sendSizes[proci][Pstream::myProcNo()];
-            constructMap[proci].setSize(nRecv);
-
-            for (label i = 0; i < nRecv; ++i)
-            {
-                constructMap[proci][i] = segmentI++;
-            }
-        }
-    }
-
-    return autoPtr<mapDistribute>::New
-    (
-        segmentI, // size after construction
-        std::move(sendMap),
-        std::move(constructMap)
-    );
+    return autoPtr<mapDistribute>::New(std::move(sendMap));
 }
 
 

commit 7b20e888a80fee608f01e7dc13ce21eb42b4ffe7

REGRESSION: inconsistent constructMap order in meshToMesh::calcProcMap
- commit fb69a54bc309 accidentally changed the constructMap compact
  order from linear ordering to local elements first order. Seems to
  interact poorly with other bookkeeping so doing a partial revert,
  but still replacing the old allGatherList with exchangeSizes.

Note:
   the processorLOD method does actually use a constructMap with local
   elements first ordering, so some inconsistency may still exist
   there

src/sampling/meshToMesh/meshToMeshParallelOps.C
@@ -212,27 +212,46 @@ Foam::autoPtr<Foam::mapDistribute> Foam::meshToMesh::calcProcMap
                     }
                 }
 
-                // convert dynamicList to labelList
-                forAll(sendMap, proci)
-                {
-                    sendMap[proci].transfer(dynSendMap[proci]);
-                }
-
-                // debug printing
                 if (debug)
                 {
                     Pout<< "Of my " << tgt.nCells()
                         << " target cells I need to send to:" << nl
                         << "\tproc\tcells" << endl;
-                    forAll(sendMap, proci)
+                    forAll(dynSendMap, proci)
                     {
                         Pout<< '\t' << proci << '\t'
-                            << sendMap[proci].size() << endl;
+                            << dynSendMap[proci].size() << endl;
                     }
+                }
+
+                // Convert DynamicList -> List
+                forAll(sendMap, proci)
+                {
+                    sendMap[proci].transfer(dynSendMap[proci]);
                 }
             }
 
-            return autoPtr<mapDistribute>::New(std::move(sendMap));
+
+            labelList recvSizes;
+            Pstream::exchangeSizes(sendMap, recvSizes, UPstream::worldComm);
+
+            // Uses linear receive order
+            labelListList constructMap(UPstream::nProcs());
+
+            label constructSize = 0;
+            forAll(constructMap, proci)
+            {
+                const label len = recvSizes[proci];
+                constructMap[proci] = identity(len, constructSize);
+                constructSize += len;
+            }
+
+            return autoPtr<mapDistribute>::New
+            (
+                constructSize,
+                std::move(sendMap),
+                std::move(constructMap)
+            );
             break;
         }
     }