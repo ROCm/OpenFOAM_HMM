2e9af747c63e480a409aad7211dfecee404a3044
Suyash Tandon <suyash.tandon@amd.com>
2023-09-28 22:45:04 -0500

    cleaned scripts/MEMO_SETUP.sh and removed stale paths

--- scripts/MEMO_SETUP.sh
+++ scripts/MEMO_SETUP.sh
@@ -4,38 +4,26 @@ export HSA_XNACK=1
 export OMP_NUM_THREADS=1
 export OMPX_DISABLE_USM_MAPS=1
 export GPU_MAX_HW_QUEUES=4
-#export HSA_FORCE_STORESC1=0 
 
-#export ROCM4FOAM=${HOME}/OPENMP_COMPILER_UVM/rocm-5.5.0
-#export ROCM4FOAM=/opt/rocm-5.7.0-1207
-export ROCM4FOAM=/opt/rocm-5.7.0-1330
-#export ROCM4FOAM=/home/rlieberm/rocm-5.7.0-12374
-#export MPI4FOAM=${THERA_OMPI_DIR}
+#set ROCm and MPI paths
+export ROCM4FOAM=${ROCM_PATH:-/opt/rocm}
+export MPI4FOAM=${MPI_PATH:-/opt/ompi}
 
-#sh5/MI300A
-export MPI4FOAM=/opt/ompi
-
-#sh5/MI300A
-#export MPI4FOAM=${HOME}/openmpi-5.0.0rc12
-
+#set OMPI compilers
 export OMPI_CXX=clang++
 export OMPI_CC=clang
 
-export PATH=${MPI4FOAM}/bin:${MPI4FOAM}/lib:${MPI4FOAM}/include::$PATH
+#add OMPI and ROCm to PATH and LD_LIBRARY_PATH
+export PATH=${MPI4FOAM}/bin:$PATH
+export LD_LIBRARY_PATH=${MPI4FOAM}/lib:$LD_LIBRARY_PATH
 export LIBRARY_PATH=${MPI4FOAM}/lib:$LIBRARY_PATH
 
-export PATH=${ROCM4FOAM}/llvm/bin:$PATH
+export PATH=${ROCM4FOAM}/bin:${ROCM4FOAM}/llvm/bin:$PATH
 export LD_LIBRARY_PATH=${ROCM4FOAM}/llvm/lib:${ROCM4FOAM}/lib:$LD_LIBRARY_PATH
 export LIBRARY_PATH=${ROCM4FOAM}/llvm/lib:${ROCM4FOAM}/lib:$LIBRARY_PATH
 
-
-
-#for profiler
-export PATH=${ROCM4FOAM}/bin:$PATH
-
-
-
-source etc/bashrc
+#source OpenFOAM environment
+source ./etc/bashrc
 
 
 
